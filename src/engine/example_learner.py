89c573e4148a4006b52452b68464712c4eae1c44
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-09-17 09:07:50 +0200

    Fix engine source files

--- src/engine/example_learner.py
+++ src/engine/example_learner.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from python.engine.learners import Learner
+from engine.learners import Learner
 
 
 class ExampleLearner(Learner):
