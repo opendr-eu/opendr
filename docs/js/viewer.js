75c805b20c6d0581910327d568f37b7e94c303d4
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-09-21 15:25:00 +0200

    Add missing function definitions

--- docs/js/viewer.js
+++ docs/js/viewer.js
@@ -1,6 +1,4 @@
 /* eslint no-extend-native: ["error", { "exceptions": ["String"] }] */
-/* global getGETQueryValue */
-/* global getGETQueriesMatchingRegularExpression */
 /* global setup */
 /* global showdown */
 /* global hljs */
@@ -21,6 +19,32 @@ if (typeof String.prototype.endsWith !== 'function') {
   String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
   };
+}
+
+function getGETQueryValue(variableName, defaultValue) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (let i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (pair[0] === variableName)
+      return pair[1];
+  }
+  return defaultValue;
+}
+
+function getGETQueriesMatchingRegularExpression(pattern) {
+  var values = {};
+  var query = window.location.search.substring(1);
+  if (query === '')
+    return values;
+  var vars = query.split('&');
+  var regex = new RegExp(pattern);
+  for (let i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (regex.test(pair[0]))
+      values[pair[0].toLowerCase()] = pair[1].toLowerCase();
+  }
+  return values;
 }
 
 function isInternetExplorer() {
