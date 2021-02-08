638fb1e709fe1c1f324d44bb2768134b7477d88f
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2021-02-08 20:39:47 +0100

    Add initial simple method to install tools dependencies based on INI files

---
+++ install/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+       echo "This script must be run as root"
+       exit 1
+fi
+
+python linux_runtime_dependencies.py
+pip install -r python_dependencies.txt
---
+++ install/linux_runtime_dependencies.py
@@ -0,0 +1,28 @@
+
+import os
+from pip._internal import main as pip
+from configparser import ConfigParser
+
+dependency_file = "python_dependencies.txt"
+
+def read_ini(path):
+    parser = ConfigParser()
+    parser.read(path)
+    python_dependencies = parser.get('runtime', 'python')
+    if python_dependencies:
+        for package in python_dependencies.split():
+             f = open(dependency_file, "a")
+             f.write(package + '\n')
+
+
+# Clear dependencies
+open(dependency_file, 'w').close()
+# Extract generic dependencies
+read_ini('runtime_dependencies.ini')
+# Loop through tools and extract dependencies
+opendr_home = os.environ.get('OPENDR_HOME') 
+for subdir, dirs, files in os.walk(opendr_home + '/src'):
+    for filename in files:
+        if filename == 'dependencies.ini':
+            read_ini(subdir + os.sep + filename)    
+
