ed61d83b139a1209cd07385c179e984f0895dc31
Illia Oleksiienko <io@ece.au.dk>
2021-03-11 15:30:04 +0000

    Fix c style errors

--- src/perception/object_detection_3d/voxel_object_detection_3d/second_detector/core/cc/nms/nms_cpu_only.cc
+++ src/perception/object_detection_3d/voxel_object_detection_3d/second_detector/core/cc/nms/nms_cpu_only.cc
@@ -1,10 +1,6 @@
 #include "nms_cpu.h"
 PYBIND11_MODULE(nms, m) {
   m.doc() = "non_max_suppression";
-//   m.def("non_max_suppression", &non_max_suppression<double>, py::return_value_policy::reference_internal, "bbox iou",
-//         "boxes"_a = 1, "keep_out"_a = 2, "nms_overlap_thresh"_a = 3, "device_id"_a = 4);
-//   m.def("non_max_suppression", &non_max_suppression<float>, py::return_value_policy::reference_internal, "bbox iou",
-//         "boxes"_a = 1, "keep_out"_a = 2, "nms_overlap_thresh"_a = 3, "device_id"_a = 4);
   m.def("non_max_suppression_cpu", &non_max_suppression_cpu<double>, py::return_value_policy::reference_internal, "bbox iou",
         "boxes"_a = 1, "order"_a = 2, "nms_overlap_thresh"_a = 3, "eps"_a = 4);
   m.def("non_max_suppression_cpu", &non_max_suppression_cpu<float>, py::return_value_policy::reference_internal, "bbox iou",
