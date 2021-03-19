f8dda76e0dfe58ff3c901b3618dd6678dba73b10
Joonas Roos <joonas.roos@hotmail.com>
2021-03-19 14:19:59 +0200

    Add target class for speech commands

--- src/engine/target.py
+++ src/engine/target.py
@@ -84,3 +84,20 @@ class Pose(Target):
         for name, kpt in zip(Pose.kpt_names, self.data.tolist()):
             out_string += name + ": " + str(kpt) + "\n"
         return out_string
+
+
+class SpeechCommand(Target):
+    """
+    This target is used for speech command recognition. Contains the predicted class or ground truth
+    and optionally the prediction confidence.
+    """
+    def __init__(self, prediction, confidence=None):
+        super().__init__()
+        self.data = prediction
+        self.confidence = confidence
+
+    def __str__(self):
+        if self.confidence is not None:
+            return f"Class {self.data} speech command with confidence {self.confidence}"
+        else:
+            return f"Class {self.data} speech command"
