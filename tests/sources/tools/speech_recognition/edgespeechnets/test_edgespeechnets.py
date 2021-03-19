fdd56d53690e47b8a7be03d988eead44f4f53719
Joonas Roos <joonas.roos@hotmail.com>
2021-03-19 14:20:43 +0200

    Change the output type of infer

--- tests/sources/tools/speech_recognition/edgespeechnets/test_edgespeechnets.py
+++ tests/sources/tools/speech_recognition/edgespeechnets/test_edgespeechnets.py
@@ -22,6 +22,7 @@ import torch as t
 
 from perception.speech_recognition.edgespeechnets.edgespeechnets_learner import EdgesSpeechNetsLearner
 from engine.datasets import DatasetIterator
+from engine.target import SpeechCommand
 
 TEST_BATCH_SIZE = 2
 TEST_EPOCHS = 1
@@ -73,12 +74,13 @@ class EdgeSpeechNetsTest(unittest.TestCase):
     def test_infer_batch(self):
         batch = np.ones((TEST_INFER_LENGTH, TEST_SIGNAL_LENGTH))
         results = self.learner.infer(batch)
-        self.assertTrue(results.shape == (TEST_INFER_LENGTH,))
+        self.assertTrue(len(results) == TEST_INFER_LENGTH)
+        self.assertTrue(all([isinstance(x, SpeechCommand) for x in results]))
 
     def test_infer_pure_signal(self):
         signal = np.ones(TEST_SIGNAL_LENGTH)
-        results = self.learner.infer(signal)
-        self.assertTrue(results.shape == (1,))
+        result = self.learner.infer(signal)
+        self.assertTrue(isinstance(result, SpeechCommand))
 
     def test_reset(self):
         weights_before_reset = list(self.learner.model.parameters())[0].clone()
