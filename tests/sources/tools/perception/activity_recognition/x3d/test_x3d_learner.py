a8897c2383141477c984a326a666bd03337c419e
LukasHedegaard <lh@eng.au.dk>
2021-03-29 15:09:47 +0200

    Add optimize and onnx functionality

--- tests/sources/tools/perception/activity_recognition/x3d/test_x3d_learner.py
+++ tests/sources/tools/perception/activity_recognition/x3d/test_x3d_learner.py
@@ -12,13 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-import torch
-# import shutil
-# Temporary for debugging. TODO: Remove
+# # Temporary for debugging. TODO: Remove
 # import sys
 # if "/Users/au478108/Projects/opendr_internal/src" not in sys.path:
 #     sys.path.append("/Users/au478108/Projects/opendr_internal/src")
+import shutil
+import torch
+import unittest
 
 from perception.activity_recognition.x3d.x3d_learner import X3DLearner
 from perception.activity_recognition.datasets.kinetics import KineticsDataset
@@ -45,17 +45,18 @@ class TestX3DLearner(unittest.TestCase):
             device="cpu", temp_path=str(cls.temp_dir), iters=1, batch_size=2, backbone=_BACKBONE, num_workers=0,
         )
 
-    # @classmethod
-    # def tearDownClass(cls):
-    #     try:
-    #         shutil.rmtree(str(cls.temp_dir))
-    #     except OSError as e:
-    #         logger.error(f"Caught error while cleaning up {e.filename}: {e.strerror}")
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            shutil.rmtree(str(".cache"))
+            shutil.rmtree(str(cls.temp_dir))
+        except OSError as e:
+            logger.error(f"Caught error while cleaning up {e.filename}: {e.strerror}")
 
     def test_downloaded(self):
         assert Path(self.temp_dir) / "weights" / "x3d_s.pyth"
 
-    def xtest_save_and_load(self):
+    def test_save_and_load(self):
         assert self.learner.model is not None
         self.learner.save(self.temp_dir)
         # Make changes to check subsequent load
@@ -67,7 +68,7 @@ class TestX3DLearner(unittest.TestCase):
         )
         assert self.learner.batch_size == 2
 
-    def xtest_fit(self):
+    def test_fit(self):
         train_ds = KineticsDataset(path=_DATASET_PATH, frames_per_clip=4, split="train")
         val_ds = KineticsDataset(path=_DATASET_PATH, frames_per_clip=4, split="val")
 
@@ -84,14 +85,14 @@ class TestX3DLearner(unittest.TestCase):
         # Check that parameters changed
         assert not torch.equal(m, list(self.learner.model.parameters())[0])
 
-    def xtest_eval(self):
+    def test_eval(self):
         test_ds = KineticsDataset(path=_DATASET_PATH, frames_per_clip=4, split="test")
 
         self.learner.load_model_weights(self.temp_dir / "weights" / f"x3d_{_BACKBONE}.pyth")
         results = self.learner.eval(test_ds, steps=2)
 
         assert results["accuracy"] > 0.2  # Most likely ≈ 60%
-        assert results["results"] < 20  # Most likely ≈ 6.0
+        assert results["loss"] < 20  # Most likely ≈ 6.0
 
     def test_infer(self):
         ds = KineticsDataset(path=_DATASET_PATH, frames_per_clip=4, split="test")
@@ -104,30 +105,32 @@ class TestX3DLearner(unittest.TestCase):
         # Results is a batch with each item summing to 1.0
         assert torch.all(torch.sum(results, dim=1) == torch.ones((2, 1)))
 
+    # Redundant test: Same code is executed internally in `test_optimize`
     # def test_save_load_onnx(self):
-    #     self.pose_estimator.model = None
-    #     self.pose_estimator.ort_session = None
-    #     self.pose_estimator.init_model()
-    #     self.pose_estimator.optimize()
-    #     self.pose_estimator.save(os.path.join(self.temp_dir, "testModel"))
-    #     self.pose_estimator.model = None
-    #     self.pose_estimator.load(os.path.join(self.temp_dir, "testModel"))
-    #     self.assertIsNotNone(
-    #         self.pose_estimator.ort_session,
-    #         "ort_session is None after loading onnx model.",
-    #     )
-    #     # Cleanup
-    #     rmfile(os.path.join(self.temp_dir, "onnx_model_temp.onnx"))
-    #     rmdir(os.path.join(self.temp_dir, "testModel"))
+    #     self.learner.load_model_weights(self.temp_dir / "weights" / f"x3d_{_BACKBONE}.pyth")
+    #     path = self.temp_dir / f"x3d_{_BACKBONE}.pyth"
+    #     # Save
+    #     if path.exists():
+    #         path.unlink()
+    #     assert not path.exists()
+    #     self.learner.save_onnx(path)
+    #     assert path.exists()
+    #     # Load
+    #     assert getattr(self.learner, "ort_session", None) == None
+    #     self.learner.load_onnx(path)
+    #     assert getattr(self.learner, "ort_session", None) != None
+    #     # Clean up
+    #     self.learner.ort_session = None
 
-    # def test_optimize(self):
-    #     self.pose_estimator.model = None
-    #     self.pose_estimator.ort_session = None
-    #     self.pose_estimator.load(os.path.join(self.temp_dir, "trainedModel"))
-    #     self.pose_estimator.optimize()
-    #     self.assertIsNotNone(self.pose_estimator.ort_session)
-    #     # Cleanup
-    #     rmfile(os.path.join(self.temp_dir, "onnx_model_temp.onnx"))
+    def test_optimize(self):
+        self.learner.ort_session = None
+        self.learner.load_model_weights(self.temp_dir / "weights" / f"x3d_{_BACKBONE}.pyth")
+        self.learner.optimize()
+
+        assert self.learner.ort_session is not None
+
+        # Clean up
+        self.learner.ort_session = None
 
 
 if __name__ == "__main__":
