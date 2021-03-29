b5f46ca20b2e0053053f4f9c3e2ae82362a69072
LukasHedegaard <lh@eng.au.dk>
2021-03-29 15:09:31 +0200

    Make Swish compatible with ONNX

--- src/perception/activity_recognition/x3d/modules/operators.py
+++ src/perception/activity_recognition/x3d/modules/operators.py
@@ -13,23 +13,24 @@ class Swish(nn.Module):
         super(Swish, self).__init__()
 
     def forward(self, x):
-        return SwishEfficient.apply(x)
+        return x * torch.sigmoid(x)
+        # return SwishEfficient.apply(x)
 
 
-class SwishEfficient(torch.autograd.Function):
-    """Swish activation function: x * sigmoid(x)."""
+# class SwishEfficient(torch.autograd.Function):
+#     """Swish activation function: x * sigmoid(x)."""
 
-    @staticmethod
-    def forward(ctx, x):
-        result = x * torch.sigmoid(x)
-        ctx.save_for_backward(x)
-        return result
+#     @staticmethod
+#     def forward(ctx, x):
+#         result = x * torch.sigmoid(x)
+#         ctx.save_for_backward(x)
+#         return result
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        x = ctx.saved_variables[0]
-        sigmoid_x = torch.sigmoid(x)
-        return grad_output * (sigmoid_x * (1 + x * (1 - sigmoid_x)))
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         x = ctx.saved_variables[0]
+#         sigmoid_x = torch.sigmoid(x)
+#         return grad_output * (sigmoid_x * (1 + x * (1 - sigmoid_x)))
 
 
 class SE(nn.Module):
