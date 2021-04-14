fb4e88c24c09769108064f9aee295032d155e5f7
Daniel Honerkamp <daniel.honerkamp@gmail.com>
2021-04-14 13:56:26 +0200

    python dependencies

--- src/control/mobile_manipulation/mobileRL/envs/__init__.py
+++ src/control/mobile_manipulation/mobileRL/envs/__init__.py
@@ -1,7 +1,5 @@
-from .tasks import RestrictedWsTask, RndStartRndGoalsTask, SimpleObstacleTask, HouseExpoTask, AdversarialTask, \
-    HierarchiecalAdversarial
+from .tasks import RestrictedWsTask, RndStartRndGoalsTask
 from .tasks_chained import PickNPlaceChainedTask, DoorChainedTask, DrawerChainedTask
 
-_all_tasks = [RndStartRndGoalsTask, RestrictedWsTask, SimpleObstacleTask, AdversarialTask, HouseExpoTask,
-              PickNPlaceChainedTask, DoorChainedTask, DrawerChainedTask, HierarchiecalAdversarial]
+_all_tasks = [RndStartRndGoalsTask, RestrictedWsTask, PickNPlaceChainedTask, DoorChainedTask, DrawerChainedTask]
 ALL_TASKS = {task.taskname().lower(): task for task in _all_tasks}
