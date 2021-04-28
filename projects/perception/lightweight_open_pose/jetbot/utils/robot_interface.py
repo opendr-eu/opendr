66c098ad1d4c268c4a6ba62ca4e6b1cc7d73b459
Nikolaos Passalis <passalis@csd.auth.gr>
2021-04-28 10:40:41 +0300

    Style changes

--- projects/perception/lightweight_open_pose/jetbot/utils/robot_interface.py
+++ projects/perception/lightweight_open_pose/jetbot/utils/robot_interface.py
@@ -1,3 +1,17 @@
+# Copyright 2020-2021 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import time
 
@@ -58,7 +72,7 @@ class PoseRobot:
         @rtype:
         """
         if self.robot_interface == 'webots':
-            self.motor_left.setPosition(self.left_motor_position_sensor.getValue() )
+            self.motor_left.setPosition(self.left_motor_position_sensor.getValue())
             self.motor_right.setPosition(self.right_motor_position_sensor.getValue())
         elif self.robot_interface == 'jetbot':
             self.robot.stop()
