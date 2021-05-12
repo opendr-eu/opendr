3cce58c7a0091e2428a09141ddc530c3f6ffc2b4
Dat Tran <viebboy@gmail.com>
2021-05-12 21:48:05 +0300

    update test_license.py to ignore files taken from others

--- tests/test_license.py
+++ tests/test_license.py
@@ -84,11 +84,13 @@ class TestLicense(unittest.TestCase):
             'src/opendr/perception/face_recognition/algorithm',
             'src/opendr/perception/activity_recognition/x3d/algorithm',
             'src/opendr/perception/object_tracking_2d/fair_mot/algorithm'
+            'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/backbones'
         ]
 
         skippedFilePaths = [
             'src/opendr/perception/activity_recognition/datasets/utils/decoder.py',
-            'projects/perception/lightweight_open_pose/jetbot/utils/pid.py'
+            'projects/perception/lightweight_open_pose/jetbot/utils/pid.py',
+            'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/trainers.py',
         ]
 
         skippedDirectories = [
