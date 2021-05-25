bbd9112d86b281aa9914985bb052c28c0bc023fd
ekakalet <63847549+ekakalet@users.noreply.github.com>
2021-05-25 12:52:14 +0300

    add OPENDR classes

---
+++ src/simulation/synthetic-muti-view-facial-image-generation/testSytheticDataGeneration.py
@@ -0,0 +1,67 @@
+# Copyright 1996-2020 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# MIT License
+#
+# Copyright (c) 2019 Jian Zhao
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import sys
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+
+sys.path.insert(0, parent_dir_path)
+
+import numpy as np
+import matplotlib.pyplot as mpplot
+import matplotlib.image as mpimg
+import main
+import inference
+import test_multipose 
+from SyntheticDataGeneration import MultiviewDataGenerationLearner
+import argparse
+import face_alignment
+from utils.ddfa import ToTensorGjz, NormalizeGjz, str2bool 
+import torch
+import torchvision.transforms as transforms
+import mobilenet_v1
+import numpy as np
+import cv2
+import face_alignment
+from utils.ddfa import ToTensorGjz, NormalizeGjz, str2bool
+from tqdm import tqdm
+from shutil import copyfile
+from os import path
+
+
+synthetic=MultiviewDataGenerationLearner()
+synthetic.eval()
