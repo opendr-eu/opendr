c94cdf44988ca273221465521b5408532dc781f3
jelledouwe <j.d.luijkx@tudelft.nl>
2021-06-07 11:32:49 +0200

    Added webots example

--- src/opendr/control/gym_tools/eager/README.md
+++ src/opendr/control/gym_tools/eager/README.md
@@ -7,7 +7,6 @@ This folder contains the Engine Agnostic Gym Environment for Robotics (EAGER) to
 If you have not done this yet, first install the OpenDR toolkit dependencies.
 The following command creates a virtual environment, so if you are working from another virtual environment, you probably first want to deactivate your current virtual environment.
 Go to the root of the OpenDR toolkit and run
-
 ```
 source bin/setup.bash
 ```
@@ -15,16 +14,24 @@ Then run:
 ```
 make install_runtime_dependencies
 ```
-Now the EAGER toolkit can be installed:
+Now the EAGER toolkit can be installed (mind the source command):
 ```
 source $OPENDR_HOME/src/opendr/control/gym_tools/eager/install_eager.sh
 ```
-You can check if installation was succesful by running:
+You can check if installation was succesful by running one of the examples:
+```
+roslaunch opendr_example example_action_processing.launch
+```
+or
 ```
 roslaunch opendr_example example_multi_robot.launch
 ```
+or
+```
+roslaunch opendr_example example_switch_engine.launch
+```
 Note that the toolkit can now be used from the terminal in which the install script is run.
-For usage in another terminal either run the installation script there too or run:
+For usage in another terminal either run the installation script there too (with source) or run:
 ```
 source $OPENDR_HOME/lib/ur_ws/devel/setup.bash
 source $OPENR_HOME/lib/eager_ws/devel/setup.bash
---
+++ src/opendr/control/gym_tools/eager/eager/opendr_example/scripts/example_switch_engine.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+# ROS packages required
+import rospy
+from eager_core.eager_env import BaseEagerEnv
+from eager_core.objects import Object
+from eager_core.wrappers.flatten import Flatten
+from eager_bridge_webots.webots_engine import WebotsEngine
+from eager_bridge_pybullet.pybullet_engine import PyBulletEngine
+from eager_process_safe_actions.safe_actions_processor import SafeActionsProcessor
+
+from gym import spaces
+import numpy as np
+import pybullet_data
+from stable_baselines3 import PPO
+
+
+class MyEnv(BaseEagerEnv):
+
+    def __init__(self, engine, name="my_env"):
+        super().__init__(engine, name=name)
+
+        self.STEPS_PER_ROLLOUT = 100
+        self.steps = 0
+
+        # Create ur5e object
+        self.ur5e = Object.create('ur5e1', 'eager_robot_ur5e', 'ur5e')
+
+        # Add preprocessing so that commanded actions are safe
+        process_args = SafeActionsProcessor(moveit_package='ur5_e_moveit_config',
+                                            urdf_path='$(find ur_e_description)/urdf/ur5e_robot.urdf.xacro',
+                                            joint_names=['shoulder_pan_joint',
+                                                         'shoulder_lift_joint',
+                                                         'elbow_joint',
+                                                         'wrist_1_joint',
+                                                         'wrist_2_joint',
+                                                         'wrist_3_joint'],
+                                            group_name='manipulator',
+                                            duration=0.1,
+                                            object_frame='base_link',
+                                            checks_per_rad=15,
+                                            vel_limit=3.0,
+                                            robot_type='ur5e')
+        self.ur5e.actuators['joints'].add_preprocess(
+            launch_path='$(find eager_process_safe_actions)/launch/safe_actions.launch',
+            launch_args=process_args.__dict__,
+            observations_from_objects=[self.ur5e],
+            action_space=spaces.Box(low=-np.pi, high=np.pi, shape=(6,)))
+
+        # Initialize all the services of the robots
+        self._init_nodes([self.ur5e])
+
+        # Define the spaces
+        self.observation_space = self.ur5e.observation_space
+        self.action_space = self.ur5e.action_space
+
+    def step(self, action):
+        # Set actions before stepping
+        self.ur5e.set_action(action)
+
+        # Step the environment
+        self._step()
+        self.steps += 1
+
+        # Get observations
+        obs = self.ur5e.get_obs()
+
+        return obs, self._get_reward(obs), self._is_done(obs), self.ur5e.get_state()
+
+    def reset(self) -> object:
+        self.steps = 0
+
+        # Set desired reset state
+        reset_states = dict()
+        reset_states['joint_pos'] = np.array([0, -np.pi / 2, 0, 0, 0, 0], dtype='float32')
+        reset_states['joint_vel'] = np.array([0, 0, 0, 0, 0, 0], dtype='float32')
+        self.ur5e.reset(states=reset_states)
+
+        # Reset the environment
+        self._reset()
+
+        # Get new observations
+        return self.ur5e.get_obs()
+
+    def render(self, mode, **kwargs):
+        return None
+
+    def _get_reward(self, obs):
+        # Quadratic reward - move to goal position [0, -np.pi/2, 0, 0, 0, 0]
+        return -((obs['joint_sensors'] - np.array([0, -np.pi / 2, 0, 0, 0, 0], dtype='float32')) ** 2).sum()
+
+    def _is_done(self, obs):
+        return self.steps >= self.STEPS_PER_ROLLOUT
+
+
+if __name__ == '__main__':
+
+    rospy.init_node('example_safe_actions', anonymous=True, log_level=rospy.WARN)
+
+    # Define the engine
+    engine = WebotsEngine(world='$(find opendr_example)/worlds/ur5e.wbt')
+    # engine = PyBulletEngine(world='%s/%s.urdf' % (pybullet_data.getDataPath(), 'plane'), no_gui='false')
+
+    # Create environment
+    env = MyEnv(engine, name="my_env")
+    env = Flatten(env)
+
+    env.seed(42)
+
+    rospy.loginfo("Training starts")
+
+    obs = env.reset()
+    for i in range(1000):
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
+
+    model = PPO('MlpPolicy', env, verbose=1)
+
+    model.learn(total_timesteps=100000)
+
+    env.close()
