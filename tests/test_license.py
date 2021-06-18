23faa0938dad402bef61074322344f6951e07e77
Negar <negar.heidari@eng.au.dk>
2021-06-18 10:27:43 +0200

    landmark-based-facial-expression-recognition learner, algorithms and documents are added.

--- tests/test_license.py
+++ tests/test_license.py
@@ -92,6 +92,8 @@ class TestLicense(unittest.TestCase):
             'src/opendr/perception/activity_recognition/datasets/utils/decoder.py',
             'projects/perception/lightweight_open_pose/jetbot/utils/pid.py',
             'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/trainers.py',
+            'src/opendr/perception/facial_exressoin_recognition/landmark_based_facial_expression_recognition/algorithm'
+            '/datasets/landmark_extractor.py',
         ]
 
         skippedDirectories = [
