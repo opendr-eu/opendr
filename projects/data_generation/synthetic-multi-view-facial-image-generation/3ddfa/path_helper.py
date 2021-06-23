bd8fcb4aed80c863a7ff9bffe2614ab3fdf3afcb
Nikolaos Passalis <passalis@csd.auth.gr>
2021-06-24 01:34:27 +0300

    Added path_helper files

---
+++ projects/data_generation/synthetic-multi-view-facial-image-generation/3ddfa/path_helper.py
@@ -0,0 +1,5 @@
+import os
+import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
