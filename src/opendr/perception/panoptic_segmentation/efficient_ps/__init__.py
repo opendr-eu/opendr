be02e3f55dbfb87c226ae653278cbd3600b2107e
Niclas Vödisch <voedisch@cs.uni-freiburg.de>
2021-06-23 11:10:33 +0200

    Move files

--- src/opendr/perception/panoptic_segmentation/efficient_ps/__init__.py
+++ src/opendr/perception/panoptic_segmentation/efficient_ps/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa
-from opendr.perception.panoptic_segmentation.efficient_ps.src.opendr_interface.efficient_ps_learner import EfficientPsLearner
+from opendr.perception.panoptic_segmentation.efficient_ps.efficient_ps_learner import EfficientPsLearner
