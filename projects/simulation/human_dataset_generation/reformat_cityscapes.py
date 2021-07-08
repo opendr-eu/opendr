180b62ebbf57bccf3dee223c291a1e2dbecb5aa4
charsyme <63857415+charsyme@users.noreply.github.com>
2021-07-08 11:07:24 +0300

    Update reformat_cityscapes.py

--- projects/simulation/human_dataset_generation/reformat_cityscapes.py
+++ projects/simulation/human_dataset_generation/reformat_cityscapes.py
@@ -19,7 +19,7 @@ import shutil
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-data_dir', type=str, default='./background_images/CityScapes/in')
+    parser.add_argument('-data_dir', type=str, default='./background_images/Cityscapes/in')
     opt = parser.parse_args()
 
     all_dir = os.path.join(opt.data_dir, 'all')
