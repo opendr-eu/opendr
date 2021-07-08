290bff581340fe69a1a4a1e4516c153a80f0fec4
charsyme <63857415+charsyme@users.noreply.github.com>
2021-07-08 11:10:02 +0300

    Update download_models.sh

--- projects/simulation/human_dataset_generation/download_models.sh
+++ projects/simulation/human_dataset_generation/download_models.sh
@@ -6,8 +6,8 @@ tar -xzvf human_models.tar.gz
 
 rm human_models.tar.gz
 
-mkdir -p ./background_images/CityScapes/in
-mkdir -p ./background_images/CityScapes/out
+mkdir -p ./background_images/Cityscapes/in
+mkdir -p ./background_images/Cityscapes/out
 
 wget --load-cookies /tmp/cookies.txt "ftp://opendrdata.csd.auth.gr/simulation/human_data_generation_framework/csv.tar.gz" -O csv.tar.gz && rm -rf /tmp/cookies.txt
 
@@ -18,12 +18,12 @@ rm csv.tar.gz
 wget --load-cookies /tmp/cookies.txt "ftp://opendrdata.csd.auth.gr/simulation/human_data_generation_framework/img_ids.pkl" -O img_ids.pkl && rm -rf /tmp/cookies.txt
 
 
-mv ./img_ids.pkl ./background_images/CityScapes/img_ids.pkl
+mv ./img_ids.pkl ./background_images/Cityscapes/img_ids.pkl
 
 wget --load-cookies /tmp/cookies.txt "ftp://opendrdata.csd.auth.gr/simulation/human_data_generation_framework/human_colormap.txt" -O human_colormap.txt && rm -rf /tmp/cookies.txt
 
 wget --load-cookies /tmp/cookies.txt "ftp://opendrdata.csd.auth.gr/simulation/human_data_generation_framework/locations_colormap.txt" -O locations_colormap.txt && rm -rf /tmp/cookies.txt
 
 
-mv ./human_colormap.txt ./background_images/CityScapes/human_colormap.txt
-mv ./locations_colormap.txt ./background_images/CityScapes/locations_colormap.txt
+mv ./human_colormap.txt ./background_images/Cityscapes/human_colormap.txt
+mv ./locations_colormap.txt ./background_images/Cityscapes/locations_colormap.txt
