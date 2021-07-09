e953293a9f93429e4b21510529f026f1c24eb963
charsyme <63857415+charsyme@users.noreply.github.com>
2021-07-09 17:49:25 +0300

    Update model_3D.py

--- src/opendr/simulation/human_model_generation/utilities/model_3D.py
+++ src/opendr/simulation/human_model_generation/utilities/model_3D.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Aristotle University of Thessaloniki
+# Copyright 2020-2021 Aristotle University of Thessaloniki
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
