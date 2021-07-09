131187f98b64a1f26eb4703b7d7add2e08ee8227
charsyme <63857415+charsyme@users.noreply.github.com>
2021-07-09 17:34:53 +0300

    Update train_util.py

--- src/opendr/simulation/human_model_generation/utilities/PIFu/lib/train_util.py
+++ src/opendr/simulation/human_model_generation/utilities/PIFu/lib/train_util.py
@@ -1,11 +1,11 @@
-import torch
-import numpy as np
-from .mesh_util import *
-from .sample_util import *
-from .geometry import *
+from .mesh_util import reconstruction, save_obj_mesh_with_color
+from .geometry import index
 import cv2
 from PIL import Image
 from tqdm import tqdm
+import numpy as np
+import torch
+
 
 def reshape_multiview_tensors(image_tensor, calib_tensor):
     # Careful here! Because we put single view and multiview together,
@@ -56,7 +56,7 @@ def gen_mesh(opt, net, cuda, data, save_path, use_octree=True):
             save_img = (np.transpose(image_tensor[v].detach().cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
             save_img_list.append(save_img)
         save_img = np.concatenate(save_img_list, axis=1)
-        Image.fromarray(np.uint8(save_img[:,:,::-1])).save(save_img_path)
+        Image.fromarray(np.uint8(save_img[:, :, ::-1])).save()
 
         verts, faces, _, _ = reconstruction(
             net, cuda, calib_tensor, opt.resolution, b_min, b_max, use_octree=use_octree)
@@ -70,6 +70,7 @@ def gen_mesh(opt, net, cuda, data, save_path, use_octree=True):
         print(e)
         print('Can not create marching cubes at this time.')
 
+
 def gen_mesh_color(opt, netG, netC, cuda, data, use_octree=True):
     image_tensor = data['img'].to(device=cuda)
     calib_tensor = data['calib'].to(device=cuda)
@@ -82,11 +83,11 @@ def gen_mesh_color(opt, netG, netC, cuda, data, use_octree=True):
     b_max = data['b_max']
     try:
         save_img_list = []
-        #for v in range(image_tensor.shape[0]):
+        # for v in range(image_tensor.shape[0]):
         #    save_img = (np.transpose(image_tensor[v].detach().cpu().numpy(), (1, 2, 0)) * 0.5 + 0.5)[:, :, ::-1] * 255.0
         #    save_img_list.append(save_img)
-        #save_img = np.concatenate(save_img_list, axis=1)
-        #Image.fromarray(np.uint8(save_img[:,:,::-1])).save(save_img_path)
+        # save_img = np.concatenate(save_img_list, axis=1)
+        # Image.fromarray(np.uint8(save_img[:,:,::-1])).save(save_img_path)
 
         verts, faces, _, _ = reconstruction(
             netG, cuda, calib_tensor, opt.resolution, b_min, b_max, use_octree=use_octree)
@@ -105,11 +106,12 @@ def gen_mesh_color(opt, netG, netC, cuda, data, use_octree=True):
             rgb = netC.get_preds()[0].detach().cpu().numpy() * 0.5 + 0.5
             color[left:right] = rgb.T
 
-        return [verts,faces,color]
-        #save_obj_mesh_with_color(save_path, verts, faces, color)
+        return [verts, faces, color]
+        # save_obj_mesh_with_color(save_path, verts, faces, color)
     except Exception as e:
         print(e)
         print('Can not create marching cubes at this time.')
+
 
 def adjust_learning_rate(optimizer, epoch, lr, schedule, gamma):
     """Sets the learning rate to the initial LR decayed by schedule"""
@@ -175,6 +177,7 @@ def calc_error(opt, net, cuda, dataset, num_tests):
 
     return np.average(erorr_arr), np.average(IOU_arr), np.average(prec_arr), np.average(recall_arr)
 
+
 def calc_error_color(opt, netG, netC, cuda, dataset, num_tests):
     if num_tests > len(dataset):
         num_tests = len(dataset)
@@ -194,11 +197,11 @@ def calc_error_color(opt, netG, netC, cuda, dataset, num_tests):
             rgb_tensor = data['rgbs'].to(device=cuda).unsqueeze(0)
 
             netG.filter(image_tensor)
-            _, errorC = netC.forward(image_tensor, netG.get_im_feat(), color_sample_tensor, calib_tensor, labels=rgb_tensor)
+            _, errorC = netC.forward(image_tensor, netG.get_im_feat(), color_sample_tensor, calib_tensor,
+                                     labels=rgb_tensor)
 
             # print('{0}/{1} | Error inout: {2:06f} | Error color: {3:06f}'
             #       .format(idx, num_tests, errorG.item(), errorC.item()))
             error_color_arr.append(errorC.item())
 
     return np.average(error_color_arr)
-
