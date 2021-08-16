0602fb4037e15b03fe0fe9212f41e91e6f8423d3
Maria Tzelepi <mtzelepi@users.noreply.github.com>
2021-08-16 14:59:21 +0300

    Update CamVid.py

--- src/opendr/perception/semantic_segmentation/bisenet/CamVid.py
+++ src/opendr/perception/semantic_segmentation/bisenet/CamVid.py
@@ -63,15 +63,12 @@ class CamVidDataset(ExternalDataset, DatasetIterator, torch.utils.data.Dataset):
         for label_path_ in self.label_path:
             self.label_list.extend(glob.glob(os.path.join(label_path_, '*.png')))
         self.label_list.sort()
-        # self.image_name = [x.split('/')[-1].split('.')[0] for x in self.image_list]
-        # self.label_list = [os.path.join(label_path, x + '_L.png') for x in self.image_list]
         self.fliplr = iaa.Fliplr(0.5)
         self.label_info = get_label_info(os.path.join(path, "class_dict.csv"))
         self.to_tensor = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ])
-        # self.crop = transforms.RandomCrop(scale, pad_if_needed=True)
         self.image_size = scale
         self.scale = [0.5, 1, 1.25, 1.5, 1.75, 2]
 
@@ -79,20 +76,8 @@ class CamVidDataset(ExternalDataset, DatasetIterator, torch.utils.data.Dataset):
         # load image and crop
         seed = random.random()
         img = Image.open(self.image_list[index])
-        # random crop image
-        # =====================================
-        # w,h = img.size
-        # th, tw = self.scale
-        # i = random.randint(0, h - th)
-        # j = random.randint(0, w - tw)
-        # img = F.crop(img, i, j, th, tw)
-        # =====================================
         scale = random.choice(self.scale)
         scale = (int(self.image_size[0] * scale), int(self.image_size[1] * scale))
-        # randomly resize image and random crop
-        # =====================================
-        # randomly resize image and random crop
-        # =====================================
         if self.mode == 'train':
             img = transforms.Resize(scale, Image.BILINEAR)(img)
             img = RandomCrop(self.image_size, seed, pad_if_needed=True)(img)
@@ -100,12 +85,6 @@ class CamVidDataset(ExternalDataset, DatasetIterator, torch.utils.data.Dataset):
         img = np.array(img)
         # load label
         label = Image.open(self.label_list[index])
-        # crop the corresponding label
-        # =====================================
-        # label = F.crop(label, i, j, th, tw)
-        # =====================================
-        # randomly resize label and random crop
-        # =====================================
         if self.mode == 'train':
             label = transforms.Resize(scale, Image.NEAREST)(label)
             label = RandomCrop(self.image_size, seed, pad_if_needed=True)(label)
@@ -120,7 +99,6 @@ class CamVidDataset(ExternalDataset, DatasetIterator, torch.utils.data.Dataset):
         img = Image.fromarray(img)
         img = self.to_tensor(img).float()
         label = one_hot_it_v11(label, self.label_info).astype(np.uint8)
-        # label = label.astype(np.float32)
         label = torch.from_numpy(label).long()
 
         return img, label
