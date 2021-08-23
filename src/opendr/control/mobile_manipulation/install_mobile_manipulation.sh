6edd9b5b0bc1e48bc0df1a1d0084dee56737e9d2
Daniel Honerkamp <daniel.honerkamp@gmail.com>
2021-08-23 20:46:15 +0200

    sudo

--- src/opendr/control/mobile_manipulation/install_mobile_manipulation.sh
+++ src/opendr/control/mobile_manipulation/install_mobile_manipulation.sh
@@ -14,8 +14,8 @@ MODULE_PATH=${OPENDR_HOME}/src/opendr/control/mobile_manipulation
 WS_PATH=${OPENDR_HOME}/lib/catkin_ws_mobile_manipulation
 
 
-sudo sh -c 'echo \"deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main\" > /etc/apt/sources.list.d/ros-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 # ROS
 sudo apt-get update && sudo apt-get install \
