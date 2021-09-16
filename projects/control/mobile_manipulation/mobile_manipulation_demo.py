945e6a3284a6d3fc08121d43822ee7264c377bc9
dHonerkamp <31133390+dHonerkamp@users.noreply.github.com>
2021-09-16 11:39:35 +0200

    Update projects/control/mobile_manipulation/mobile_manipulation_demo.py
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- projects/control/mobile_manipulation/mobile_manipulation_demo.py
+++ projects/control/mobile_manipulation/mobile_manipulation_demo.py
@@ -1,4 +1,4 @@
-# Copyright 1996-2020 OpenDR European Project
+# Copyright 2020-2021 OpenDR European Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
