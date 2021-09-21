ae40c62b2b801a71402e0377958f7c323018ed9d
Nikolaos <passalis@csd.auth.gr>
2021-09-21 10:50:22 +0300

    Update onnx install script

--- dependencies/install_onnx.sh
+++ dependencies/install_onnx.sh
@@ -1,21 +1,25 @@
 if [ ! -f /usr/local/lib/libonnxruntime.so ]; then
 
-  echo "Downloading onnxruntime (this might take a while) ..."
-  git clone -q --recursive https://github.com/Microsoft/onnxruntime
-  cd onnxruntime
+  
+  VERSION="1.6.0"
+  if [ $OPENDR_DEVICE == "cuda" ]
+  then
+      echo "Downloading and installing onnxruntime (gpu support) ..."
+      DEVICE="-gpu"
+  else
+      echo "Downloading and installing onnxruntime (cpu-only) ..."
+  fi
 
-  # Checkout v1.6.0 (https://github.com/microsoft/onnxruntime/releases/tag/v1.6.0)
-  git checkout v1.6.0 > /dev/null 2>&1
+  wget https://github.com/microsoft/onnxruntime/releases/download/v${VERSION}/onnxruntime-linux-x64${DEVICE}-${VERSION}.tgz
+  tar zxf onnxruntime-linux-x64${DEVICE}-${VERSION}.tgz
+  cd onnxruntime-linux-x64${DEVICE}-${VERSION}
+  sudo mkdir -p /usr/local/include/onnxruntime
+  sudo cp include/* /usr/local/include/onnxruntime/
+  sudo cp lib/libonnxruntime.so* /usr/local/lib/
+  cd ..
+  rm -rf onnxruntime-linux-x64${DEVICE}-${VERSION}
+  rm -rf onnxruntime-linux-x64${DEVICE}-${VERSION}.tgz
 
-  echo "Building onnxruntime (this might take a while) ..."
-  ./build.sh --config Release --build_shared_lib --parallel --skip_tests > /dev/null 2>&1
-
-  echo "Installing onnxruntime..."
-  sudo cp include/onnxruntime /usr/local/include/ -r
-  sudo cp build/Linux/Release/libonnxruntime.so* /usr/local/lib/
   sudo ldconfig
 
-  echo "Cleaning up..."
-  cd ..
-  rm -rf onnxruntime
 fi
