4f3548c49426adbe1e2c1b09bc759a3a205dd3a1
charsyme <63857415+charsyme@users.noreply.github.com>
2021-09-29 14:04:28 +0300

    Update test_license.py

--- tests/test_license.py
+++ tests/test_license.py
@@ -87,7 +87,7 @@ class TestLicense(unittest.TestCase):
             'src/opendr/perception/object_tracking_2d/fair_mot/algorithm',
             'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/backbones',
             'src/opendr/simulation/human_model_generation/utilities/PIFu',
-            'src/opendr/perception/multimodal_human_centric/rgbd_hand_gesture_learner/algorithm/architectures'
+            'src/opendr/perception/multimodal_human_centric/rgbd_hand_gesture_learner/algorithm/architectures',
             'src/opendr/perception/multimodal_human_centric/rgbd_hand_gesture_learner/algorithm/architectures',
             'src/opendr/perception/skeleton_based_action_recognition/algorithm',
             'src/opendr/perception/semantic_segmentation/bisenet/algorithm',
