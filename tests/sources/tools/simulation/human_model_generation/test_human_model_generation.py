3ad539a8f258e7df96d37a05c775b06729f3bc73
charsyme <63857415+charsyme@users.noreply.github.com>
2021-10-05 13:22:10 +0300

    Update test_human_model_generation.py

--- tests/sources/tools/simulation/human_model_generation/test_human_model_generation.py
+++ tests/sources/tools/simulation/human_model_generation/test_human_model_generation.py
@@ -17,8 +17,6 @@ import cv2
 import shutil
 import os
 from opendr.simulation.human_model_generation.pifu_generator_learner import PIFuGeneratorLearner
-import warnings
-
 
 def rmfile(path):
     try:
