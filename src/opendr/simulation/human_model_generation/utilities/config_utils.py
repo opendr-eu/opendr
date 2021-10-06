84801cf4e225dba3e9f06209f6fdbd1848b7d8e6
charsyme <63857415+charsyme@users.noreply.github.com>
2021-10-06 11:48:00 +0300

    Create config_utils.py

---
+++ src/opendr/simulation/human_model_generation/utilities/config_utils.py
@@ -0,0 +1,5 @@
+from opendr.simulation.human_model_generation.utilities.visualizer import Visualizer
+
+
+def config_visualizer(out_path, mesh, pose=None, plot_kps=False):
+    return visualizer = config_visualizer(out_path=out_path, mesh=mesh, pose=pose, plot_kps=plot_kps)
