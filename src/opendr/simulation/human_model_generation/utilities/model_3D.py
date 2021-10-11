de9ee55d749e1b1b9fa422c3bcc5067a3cb9f292
charsyme <63857415+charsyme@users.noreply.github.com>
2021-10-11 14:34:53 +0300

    Update model_3D.py

--- src/opendr/simulation/human_model_generation/utilities/model_3D.py
+++ src/opendr/simulation/human_model_generation/utilities/model_3D.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 if os.getenv('DISPLAY') is not None:
     from opendr.simulation.human_model_generation.utilities.visualizer import Visualizer
 
