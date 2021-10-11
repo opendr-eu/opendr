5129e79a7a331a501aaa1833c90e3610747f0097
charsyme <63857415+charsyme@users.noreply.github.com>
2021-10-11 13:52:05 +0300

    Update bridge.py

--- projects/opendr_ws/src/ros_bridge/src/opendr_bridge/bridge.py
+++ projects/opendr_ws/src/ros_bridge/src/opendr_bridge/bridge.py
@@ -16,7 +16,8 @@ from opendr.engine.data import Image
 from opendr.engine.target import Pose
 import numpy as np
 from cv_bridge import CvBridge
-from vision_msgs.msg import Detection2DArray, Detection2D, BoundingBox2D, ObjectHypothesisWithPose, Detection3DArray, Detection3D, BoundingBox3D
+from vision_msgs.msg import Detection2DArray, Detection2D, BoundingBox2D, ObjectHypothesisWithPose,\
+     Detection3DArray, Detection3D, BoundingBox3D
 from geometry_msgs.msg import Pose2D, Pose as Pose3D
 from shape_msgs.msg import Mesh, MeshTriangle
 from std_msgs.msg import ColorRGBA
@@ -180,18 +181,18 @@ class ROSBridge:
         mesh_ROS = Mesh()
         vertex_colors_ROS = []
         if vertex_colors is not None:
-          for i in range(vertex_colors.shape[0]):   
-           v_color = ColorRGBA(vertex_colors[i,0], vertex_colors[i,1], vertex_colors[i,2],0)
-           vertex_colors_ROS.append(v_color)
+            for i in range(vertex_colors.shape[0]):   
+                v_color = ColorRGBA(vertex_colors[i,0], vertex_colors[i,1], vertex_colors[i,2],0)
+                vertex_colors_ROS.append(v_color)
         for i in range(vertices.shape[0]):   
-           point = Point(vertices[i,0], vertices[i,1], vertices[i,2])
-           mesh_ROS.vertices.append(point)
+            point = Point(vertices[i,0], vertices[i,1], vertices[i,2])
+            mesh_ROS.vertices.append(point)
         for i in range(faces.shape[0]): 
-           mesh_triangle = MeshTriangle()
-           mesh_triangle.vertex_indices[0] = int(faces[i][0])
-           mesh_triangle.vertex_indices[1] = int(faces[i][1])
-           mesh_triangle.vertex_indices[2] = int(faces[i][2])
-           mesh_ROS.triangles.append(mesh_triangle)
+            mesh_triangle = MeshTriangle()
+            mesh_triangle.vertex_indices[0] = int(faces[i][0])
+            mesh_triangle.vertex_indices[1] = int(faces[i][1])
+            mesh_triangle.vertex_indices[2] = int(faces[i][2])
+            mesh_ROS.triangles.append(mesh_triangle)
         return mesh_ROS, vertex_colors_ROS
         
     def from_ros_mesh(self, mesh_ROS, vertex_colors_ROS=None):
@@ -212,10 +213,10 @@ class ROSBridge:
         faces = np.zeros([len(mesh_ROS.triangles), 3]).astype(int)
         vertex_colors = np.zeros([len(vertex_colors_ROS), 3])
         for i in range(len(mesh_ROS.vertices)):
-           vertices[i] = np.array([mesh_ROS.vertices[i].x, mesh_ROS.vertices[i].y, mesh_ROS.vertices[i].z])
+            vertices[i] = np.array([mesh_ROS.vertices[i].x, mesh_ROS.vertices[i].y, mesh_ROS.vertices[i].z])
         for i in range(len(mesh_ROS.triangles)):
-           faces[i] = np.array([int(mesh_ROS.triangles[i].vertex_indices[0]), int(mesh_ROS.triangles[i].vertex_indices[1]), int(mesh_ROS.triangles[i].vertex_indices[2])]).astype(int)
+            faces[i] = np.array([int(mesh_ROS.triangles[i].vertex_indices[0]), int(mesh_ROS.triangles[i].vertex_indices[1]), int(mesh_ROS.triangles[i].vertex_indices[2])]).astype(int)
         if vertex_colors_ROS is not None:
-           for i in range(len(vertex_colors_ROS)):   
-              vertex_colors[i] = np.array([vertex_colors_ROS[i].r, vertex_colors_ROS[i].g, vertex_colors_ROS[i].b])
+            for i in range(len(vertex_colors_ROS)):   
+                vertex_colors[i] = np.array([vertex_colors_ROS[i].r, vertex_colors_ROS[i].g, vertex_colors_ROS[i].b])
         return vertices, faces, vertex_colors
