20ea816381aefd87581d8caf5d862fb1635afd13
charsyme <63857415+charsyme@users.noreply.github.com>
2021-10-15 12:07:07 +0300

    Update model_3D.py

--- src/opendr/simulation/human_model_generation/utilities/model_3D.py
+++ src/opendr/simulation/human_model_generation/utilities/model_3D.py
@@ -51,10 +51,10 @@ class Model_3D:
 
     def get_img_views(self, rotations=None, human_pose_3D=None, plot_kps=False):
         if os.getenv('DISPLAY') is None:
-            raise ValueError('Renderings of the model can\'t be generated without '
+            raise OSError('Renderings of the model can\'t be generated without '
                                       'a display...')
         if rotations is None:
-            raise NotImplementedError('List of rotations is empty...')
+            raise ValueError('List of rotations is empty...')
         if human_pose_3D is not None:
             visualizer = Visualizer(out_path='./', mesh=self, pose=human_pose_3D, plot_kps=plot_kps)
         else:
