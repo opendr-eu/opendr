62c2f575c62ca1b6fe5454f82626278cfdebd513
Nikolaos Passalis <passalis@users.noreply.github.com>
2021-10-18 14:13:18 +0300

    Added __all__ entry

--- src/opendr/perception/object_detection_2d/__init__.py
+++ src/opendr/perception/object_detection_2d/__init__.py
@@ -5,3 +5,5 @@ from opendr.perception.object_detection_2d.centernet.detr.detr_learner import De
 from opendr.perception.object_detection_2d.retinaface.yolov3.yolov3_learner import YOLOv3DetectorLearner
 
 from opendr.perception.object_detection_2d.datasets import transforms
+
+__all__ = ['SingleShotDetectorLearner', 'RetinaFaceLearner', 'CenterNetDetectorLearner', 'DetrLearner', 'YOLOv3DetectorLearner', 'transforms']
