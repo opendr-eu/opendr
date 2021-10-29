c4f3964ef26c94a3786e69d72ed84b0382c26b72
ekakalet <63847549+ekakalet@users.noreply.github.com>
2021-10-29 12:36:57 +0300

    Update SyntheticDataGeneration.py

--- projects/data_generation/synthetic-multi-view-facial-image-generation/3ddfa/SyntheticDataGeneration.py
+++ projects/data_generation/synthetic-multi-view-facial-image-generation/3ddfa/SyntheticDataGeneration.py
@@ -56,7 +56,7 @@ __all__ = ['sys', 'path_helper', 'path_helper2']
 class MultiviewDataGenerationLearner(Learner):
     
 
-    def __init__(self, path_in='blabla', path_3ddfa='blabla', save_path='blabla', val_yaw='10,20', val_pitch=' 30,40', device='cuda'):
+    def __init__(self, path_in='/home/user/Pictures/TEST', path_3ddfa='./', save_path='./results', val_yaw='10,20', val_pitch=' 30,40', device='cuda'):
         try:
          if torch.cuda.is_available():
             print("GPU found.")
