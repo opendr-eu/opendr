90b49353c3c96e8b2f421f38ad000d0525484489
ekakalet <63847549+ekakalet@users.noreply.github.com>
2021-11-05 16:26:30 +0200

    Update synthetic_facial_generation.py

--- projects/opendr_ws/src/data_generation/scripts/synthetic_facial_generation.py
+++ projects/opendr_ws/src/data_generation/scripts/synthetic_facial_generation.py
@@ -19,12 +19,10 @@ import torch
 import numpy as np
 from sensor_msgs.msg import Image as ROS_Image
 from opendr_bridge import ROSBridge
-import sys
 from SyntheticDataGeneration import MultiviewDataGenerationLearner
 import os
 import cv2
 import argparse
-from cv_bridge import CvBridge, CvBridgeError
 from src.opendr.engine.data import Image
 
 
@@ -53,15 +51,15 @@ class Synthetic_Data_Generation:
         self.ID = 0
 
         # Initialize the SyntheticDataGeneration
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-path_in', default='/home/ekakalet/Pictures/TEST', type=str, help='Give the path of image folder')
-        parser.add_argument('-path_3ddfa', default='./', type=str, help='Give the path of 3ddfa folder')
-        parser.add_argument('-save_path', default='./results/', type=str, help='Give the path of results folder')
-        parser.add_argument('-val_yaw',  default="15,-15", nargs='+', type=str, help='yaw poses list between [-90,90] ')
-        parser.add_argument('-val_pitch', default="15,-15", nargs='+', type=str,  help='pitch poses list between [-90,90] ')
-        args = parser.parse_args()
-        synthetic = MultiviewDataGenerationLearner(path_in=args.path_in, path_3ddfa=args.path_3ddfa, save_path=args.save_path,
-                                                   val_yaw=args.val_yaw, val_pitch=args.val_pitch)
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('-path_in', default='/home/ekakalet/Pictures/TEST', type=str, help='Give the path of image folder')
+        self.parser.add_argument('-path_3ddfa', default='./', type=str, help='Give the path of 3ddfa folder')
+        self.parser.add_argument('-save_path', default='./results/', type=str, help='Give the path of results folder')
+        self.parser.add_argument('-val_yaw',  default="15,-15", nargs='+', type=str, help='yaw poses list between [-90,90] ')
+        self.parser.add_argument('-val_pitch', default="15,-15", nargs='+', type=str,  help='pitch poses list between [-90,90] ')
+        self.args = parser.parse_args()
+        self.synthetic = MultiviewDataGenerationLearner(path_in=self.args.path_in, path_3ddfa=self.args.path_3ddfa, save_path=self.args.save_path,
+                                                   val_yaw=self.args.val_yaw, val_pitch=self.args.val_pitch)
 
     def listen(self):
         """
