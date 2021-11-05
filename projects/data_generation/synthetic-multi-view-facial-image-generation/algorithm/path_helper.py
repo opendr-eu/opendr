1a0093641b54d9d337fdecd233a810547664af99
ekakalet <63847549+ekakalet@users.noreply.github.com>
2021-11-05 15:54:04 +0200

    Delete path_helper.py

--- projects/data_generation/synthetic-multi-view-facial-image-generation/algorithm/path_helper.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-dir_path = os.path.dirname(os.path.realpath(__file__))
-parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
-sys.path.insert(0, parent_dir_path)
