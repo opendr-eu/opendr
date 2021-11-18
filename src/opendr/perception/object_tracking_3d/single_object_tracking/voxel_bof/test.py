648f09becf56c2757986addde523db407d034324
Illia Oleksiienko <io@ece.au.dk>
2021-11-18 12:54:01 +0000

    Add drawing tests

--- src/opendr/perception/object_tracking_3d/single_object_tracking/voxel_bof/test.py
+++ src/opendr/perception/object_tracking_3d/single_object_tracking/voxel_bof/test.py
@@ -5,15 +5,22 @@ from opendr.engine.target import TrackingAnnotation3D, TrackingAnnotation3DList
 from opendr.perception.object_tracking_3d.single_object_tracking.voxel_bof.voxel_bof_object_tracking_3d_learner import (
     VoxelBofObjectTracking3DLearner,
 )
-from opendr.perception.object_detection_3d.datasets.kitti import KittiDataset
+from opendr.perception.object_detection_3d.datasets.kitti import KittiDataset, LabeledPointCloudsDatasetIterator
 from opendr.perception.object_tracking_3d.datasets.kitti_tracking import (
     KittiTrackingDatasetIterator,
     LabeledTrackingPointCloudsDatasetIterator,
 )
-from opendr.perception.object_tracking_3d.single_object_tracking.voxel_bof.draw import draw_point_cloud_bev
+from opendr.perception.object_tracking_3d.single_object_tracking.voxel_bof.draw import (
+    draw_point_cloud_bev,
+    draw_point_cloud_projected_2,
+)
 from PIL import Image as PilImage
 import numpy as np
-from opendr.perception.object_tracking_3d.single_object_tracking.voxel_bof.second_detector.core.box_np_ops import box_camera_to_lidar, camera_to_lidar
+from opendr.perception.object_tracking_3d.single_object_tracking.voxel_bof.second_detector.core.box_np_ops import (
+    box_camera_to_lidar,
+    camera_to_lidar,
+    center_to_corner_box3d,
+)
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -66,18 +73,34 @@ car_configs = {
     "pointpillars_car": config_pointpillars_car,
 }
 
-dataset_detection = KittiDataset(dataset_detection_path, subsets_path)
+dataset_detection = LabeledPointCloudsDatasetIterator(
+    dataset_detection_path + "/training/velodyne_reduced",
+    dataset_detection_path + "/training/label_2",
+    dataset_detection_path + "/training/calib",
+)
+track_id = "0002"
 dataset_tracking = LabeledTrackingPointCloudsDatasetIterator(
-    dataset_tracking_path + "/training/velodyne/0000",
-    dataset_tracking_path + "/training/label_02/0000.txt",
-    dataset_tracking_path + "/training/calib/0000.txt",
+    dataset_tracking_path + "/training/velodyne/" + track_id,
+    dataset_tracking_path + "/training/label_02/" + track_id + ".txt",
+    dataset_tracking_path + "/training/calib/" + track_id + ".txt",
 )
 name = "pointpillars_car"
 config = all_configs[name]
 model_path = model_paths[name]
 
 
-def tracking_boxes_to_lidar(label, calib, classes=["Car", "Van", "Pedestrian", "Cyclist"]):
+tanet_name = "tanet_car"
+tanet_config = all_configs[tanet_name]
+tanet_model_path = model_paths[tanet_name]
+
+
+pq = 20
+lq = 10
+
+
+def tracking_boxes_to_lidar(
+    label, calib, classes=["Car", "Van", "Pedestrian", "Cyclist"]
+):
 
     label = label.kitti()
 
@@ -118,7 +141,7 @@ def tracking_boxes_to_lidar(label, calib, classes=["Car", "Van", "Pedestrian", "
     gt_boxes_lidar = box_camera_to_lidar(gt_boxes_camera, r0_rect, trv2c)
     locs_lidar = gt_boxes_lidar[:, 0:3]
     dims_lidar = gt_boxes_lidar[:, 3:6]
-    rots_lidar = gt_boxes_lidar[:, 6:7]
+    rots_lidar = gt_boxes_lidar[:, 6:7] # * (-1) + np.pi / 2
 
     new_label = {
         "name": label["name"][selected_objects],
@@ -130,11 +153,56 @@ def tracking_boxes_to_lidar(label, calib, classes=["Car", "Van", "Pedestrian", "
         "location": locs_lidar,
         "rotation_y": rots_lidar,
         "score": label["score"][selected_objects],
-        "id": label["id"][selected_objects],
-        "frame": label["frame"][selected_objects],
+        "id": label["id"][selected_objects] if "id" in label else np.array(list(range(len(selected_objects)))),
+        "frame": label["frame"][selected_objects] if "frame" in label else np.array([0] * len(selected_objects)),
     }
 
-    result = TrackingAnnotation3DList.from_kitti(new_label, new_label["id"], new_label["frame"])
+    result = TrackingAnnotation3DList.from_kitti(
+        new_label, new_label["id"], new_label["frame"]
+    )
+
+    return result
+
+
+def label_to_AABB(label):
+
+    if len(label) == 0:
+        return label
+
+    label = label.kitti()
+
+    dims = label["dimensions"]
+    locs = label["location"]
+    rots = label["rotation_y"]
+
+    origin = [0.5, 0.5, 0]
+    gt_corners = center_to_corner_box3d(
+        locs, dims, rots.reshape(-1), origin=origin, axis=2,
+    )
+
+    mins = np.min(gt_corners, axis=1)
+    maxs = np.max(gt_corners, axis=1)
+    centers = (maxs + mins) / 2
+    sizes = maxs - mins
+    rotations = np.zeros((centers.shape[0],), dtype=np.float32)
+
+    new_label = {
+        "name": label["name"],
+        "truncated": label["truncated"],
+        "occluded": label["occluded"],
+        "alpha": label["alpha"],
+        "bbox": label["bbox"],
+        "dimensions": sizes,
+        "location": centers,
+        "rotation_y": rotations,
+        "score": label["score"],
+        "id": label["id"],
+        "frame": label["frame"],
+    }
+
+    result = TrackingAnnotation3DList.from_kitti(
+        new_label, new_label["id"], new_label["frame"]
+    )
 
     return result
 
@@ -156,8 +224,8 @@ def test_eval_detection():
 
 def test_draw_tracking_dataset():
 
-    for q in range(4): #range(len(dataset_tracking)):\
-        i = q * 10
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q * pq
         print(i, "/", len(dataset_tracking))
         point_cloud_with_calibration, label = dataset_tracking[i]
         point_cloud = point_cloud_with_calibration.data
@@ -165,6 +233,59 @@ def test_draw_tracking_dataset():
         lidar_boxes = tracking_boxes_to_lidar(label, calib)
         image = draw_point_cloud_bev(point_cloud, lidar_boxes)
         PilImage.fromarray(image).save("./plots/kt_" + str(i) + ".png")
+
+
+def test_draw_detection_dataset():
+
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q
+        print(i, "/", len(dataset_detection))
+        point_cloud_with_calibration, label = dataset_detection[i]
+        point_cloud = point_cloud_with_calibration.data
+        calib = point_cloud_with_calibration.calib
+        lidar_boxes = tracking_boxes_to_lidar(label, calib)
+        image = draw_point_cloud_bev(point_cloud, lidar_boxes)
+        PilImage.fromarray(image).save("./plots/kd_" + str(i) + ".png")
+
+
+def test_draw_detection_projected():
+
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q
+        print(i, "/", len(dataset_detection))
+        point_cloud_with_calibration, label = dataset_detection[i]
+        point_cloud = point_cloud_with_calibration.data
+        calib = point_cloud_with_calibration.calib
+        lidar_boxes = tracking_boxes_to_lidar(label, calib)
+        image = draw_point_cloud_projected_2(point_cloud, lidar_boxes)
+        PilImage.fromarray(image).save("./plots/dpr_" + str(i) + ".png")
+
+
+def test_draw_tracking_projected():
+
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q * pq
+        print(i, "/", len(dataset_tracking))
+        point_cloud_with_calibration, label = dataset_tracking[i]
+        point_cloud = point_cloud_with_calibration.data
+        calib = point_cloud_with_calibration.calib
+        lidar_boxes = tracking_boxes_to_lidar(label, calib)
+        image = draw_point_cloud_projected_2(point_cloud, lidar_boxes)
+        PilImage.fromarray(image).save("./plots/pr_" + str(i) + ".png")
+
+
+def test_draw_tracking_aabb():
+
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q * pq
+        print(i, "/", len(dataset_tracking))
+        point_cloud_with_calibration, label = dataset_tracking[i]
+        point_cloud = point_cloud_with_calibration.data
+        calib = point_cloud_with_calibration.calib
+        lidar_boxes = tracking_boxes_to_lidar(label, calib)
+        aabb = label_to_AABB(lidar_boxes)
+        image = draw_point_cloud_bev(point_cloud, aabb)
+        PilImage.fromarray(image).save("./plots/aabb_" + str(i) + ".png")
 
 
 def test_pp_infer_tracking():
@@ -175,9 +296,38 @@ def test_pp_infer_tracking():
     )
     learner.load(model_path)
 
-    res = learner.infer(dataset_tracking[0])
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q * pq
+        print(i, "/", len(dataset_tracking))
+        point_cloud_with_calibration, label = dataset_tracking[i]
+        predictions = learner.infer(point_cloud_with_calibration)
+        image = draw_point_cloud_bev(
+            point_cloud_with_calibration.data, predictions
+        )
+        PilImage.fromarray(image).save("./plots/pp_" + str(i) + ".png")
 
-    print(res)
+
+def test_tanet_infer_tracking():
+    print("Eval", tanet_name, "start", file=sys.stderr)
+
+    learner = VoxelBofObjectTracking3DLearner(
+        model_config_path=tanet_config, device=DEVICE
+    )
+
+    if not os.path.exists(tanet_model_path):
+        learner.download("tanet_car_xyres_16", "models")
+
+    learner.load(tanet_model_path)
+
+    for q in range(lq):  # range(len(dataset_tracking)):\
+        i = q * pq
+        print(i, "/", len(dataset_tracking))
+        point_cloud_with_calibration, label = dataset_tracking[i]
+        predictions = learner.infer(point_cloud_with_calibration)
+        image = draw_point_cloud_bev(
+            point_cloud_with_calibration.data, predictions
+        )
+        PilImage.fromarray(image).save("./plots/tanet_" + str(i) + ".png")
 
 
 def test_pp_block1():
@@ -190,4 +340,8 @@ def test_pp_block1():
     learner.load(model_path)
 
 
+test_draw_tracking_projected()
 test_draw_tracking_dataset()
+# test_draw_detection_dataset()
+# test_draw_detection_projected()
+test_draw_tracking_aabb()
