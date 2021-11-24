bdd107a15d44e289d7c576a2aa88cebae38e4895
Jose Arce y de la Borbolla <joseab10@gmail.com>
2021-11-24 14:20:36 +0100

    Corrected licence information.

--- projects/perception/slam/full_map_posterior_gmapping/src/fmp_slam_eval/setup.py
+++ projects/perception/slam/full_map_posterior_gmapping/src/fmp_slam_eval/setup.py
@@ -1,5 +1,7 @@
 # ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
+# Copyright 2020-2021 OpenDR European Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,7 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License."""
+# limitations under the License.
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
