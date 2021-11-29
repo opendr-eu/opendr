ebc8619372aab9267edaac71f4668aba226b5f52
halil93ibrahim <halil93ibrahim@gmail.com>
2021-11-29 05:25:39 +0100

    env updated

--- src/opendr/planning/end_to_end_planning/envs/agi_env.py
+++ src/opendr/planning/end_to_end_planning/envs/agi_env.py
@@ -51,7 +51,8 @@ class AgiEnv(gym.Env):
                                   5: (np.cos(22.5 / 180 * np.pi), -np.sin(22.5 / 180 * np.pi), 0),
                                   6: (np.cos(22.5 / 180 * np.pi), -np.sin(22.5 / 180 * np.pi), -1),
                                   7: (0, 0, 2),
-                                  8: (0, 0, -2), }
+                                  8: (0, 0, -2)}
+        self.step_length = 1  # meter
 
         self.current_position = PoseStamped().pose.position
         self.current_yaw = 0
@@ -64,6 +65,10 @@ class AgiEnv(gym.Env):
         self.r = rospy.Rate(10)
         self.ros_pub_pose = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
         self.ros_pub_target = rospy.Publisher('target_position', PoseStamped, queue_size=10)
+        self.ros_pub_trajectory = rospy.Publisher('uav_trajectory', Path, queue_size=10)
+        self.ros_pub_global_trajectory = rospy.Publisher('uav_global_trajectory', Path, queue_size=10)
+        self.global_traj = Path()
+        self.uav_trajectory = Path()
         rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.pose_callback)
         rospy.Subscriber("/range_image_raw", Float32MultiArray, self.range_image_callback)
         rospy.Subscriber("/model_name", String, self.model_name_callback)
@@ -71,7 +76,14 @@ class AgiEnv(gym.Env):
         # print(self.model_name)
         rospy.Subscriber("/" + self.model_name + "/touch_sensor/value", BoolStamped, self.collision_callback)
 
-        self.target_y = -30 + np.random.randint(-1, 2)
+        self.target_y = -22  # + np.random.randint(-1, 2)
+        self.target_y_list = [-22, -16, -10, -4, 2, 8, 14, 20, 26, 32]  # [-22, -16, -10, -4, 2, 7, 12]
+        self.target_z = 2.5
+        self.start_x = -10
+        self.forward_direction = True
+        self.parkour_length = 30
+        self.episode_counter = 0
+
         self.set_target()
         self.r.sleep()
         vo = self.difference_between_points(self.target_position, self.current_position)
@@ -85,52 +97,80 @@ class AgiEnv(gym.Env):
         self.observation[0, 0:3, 1] = np.copy(self.vector_observation)
         # print(self.vector_observation)
         self.r.sleep()
-        self.reset()
+        # self.reset()
 
         self.image_count = 0
 
     def step(self, discrete_action):
         # take action
+        print("action:", discrete_action)
         action = self.action_dictionary[discrete_action]
-        # print(action)
-        # print("position before step", self.current_position)
-        # plt.imshow(self.range_image, cmap='gray', vmin=0, vmax=1)
-        # plt.show()
-        self.go_position(
-            self.current_position.x + action[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) - action[1] * np.sin(
-                self.current_yaw * 22.5 / 180 * np.pi),
-            self.current_position.y + action[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + action[1] * np.cos(
-                self.current_yaw * 22.5 / 180 * np.pi), 5, yaw=self.current_yaw + action[2], check_collision=True)
-        # calculate reward
-        prev_distance = np.linalg.norm(self.vector_observation * np.array([1, 4, 1]))
-        vo = self.difference_between_points(self.target_position, self.current_position)
-        current_distance = np.linalg.norm(
-            np.array([vo[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.sin(
-                self.current_yaw * 22.5 / 180 * np.pi),
-                      -vo[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.cos(
-                          self.current_yaw * 22.5 / 180 * np.pi),
-                      vo[2]]) * np.array([1, 4, 1]))
-        delta_distance = prev_distance - current_distance
+        action = (action[0] * self.step_length, action[1] * self.step_length, action[2])
+        prev_x = self.current_position.x
+        prev_yaw = self.current_yaw
+        if self.forward_direction:
+            self.go_position(
+                self.current_position.x + action[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) - action[
+                    1] * np.sin(self.current_yaw * 22.5 / 180 * np.pi),
+                self.current_position.y + action[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + action[
+                    1] * np.cos(self.current_yaw * 22.5 / 180 * np.pi), self.target_z, yaw=self.current_yaw + action[2],
+                check_collision=True)
+        else:
+            self.go_position(
+                self.current_position.x - action[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + action[
+                    1] * np.sin(self.current_yaw * 22.5 / 180 * np.pi),
+                self.current_position.y - action[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) - action[
+                    1] * np.cos(self.current_yaw * 22.5 / 180 * np.pi), self.target_z, yaw=self.current_yaw + action[2],
+                check_collision=True)
+        self.update_trajectory()
+
+        # prev_distance = np.linalg.norm(self.vector_observation * np.array([1, 4, 1]))
+        dx = np.abs(self.current_position.x - prev_x)
+        dy = np.abs(self.current_position.y - self.target_position.y)
+        dyaw = np.abs(self.current_yaw)
+        print("dx, dy, dyaw", dx, dy, dyaw)
+        reward = 2 * dx - 0.4 * dy - 0.3 * dyaw
+        # current_distance = np.linalg.norm(
+        #    np.array([vo[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.sin(
+        #        self.current_yaw * 22.5 / 180 * np.pi),
+        #              -vo[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.cos(
+        #                  self.current_yaw * 22.5 / 180 * np.pi),
+        #              vo[2]]) * np.array([1, 4, 1]))
+        # delta_distance = prev_distance - current_distance
         # print(prev_distance, current_distance)
-        reward = delta_distance
-        # set new observation
-        self.set_target()
-        vo = self.difference_between_points(self.target_position, self.current_position)
-        self.vector_observation = np.array([vo[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.sin(
-            self.current_yaw * 22.5 / 180 * np.pi),
-                                            -vo[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.cos(
-                                                self.current_yaw * 22.5 / 180 * np.pi),
-                                            vo[2]])
-        # print("position after step", self.current_position)
-        # print("vec:", self.vector_observation)
-        # print("yaw:", self.current_yaw)
-        self.observation[:, :, 0] = np.copy(self.range_image)
-        self.observation[0, 0:3, 1] = np.copy(self.vector_observation)
+        # reward = delta_distance
+
+        if self.forward_direction:
+            # set new observation
+            self.set_target()
+            vo = self.difference_between_points(self.target_position, self.current_position)
+            self.vector_observation = np.array([vo[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.sin(
+                self.current_yaw * 22.5 / 180 * np.pi),
+                                                -vo[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.cos(
+                                                    self.current_yaw * 22.5 / 180 * np.pi),
+                                                vo[2]])
+            self.observation[:, :, 0] = np.copy(self.range_image)
+            self.observation[0, 0:3, 1] = np.copy(self.vector_observation)
+            finish_passed = (self.current_position.x > self.parkour_length + self.start_x)
+        else:
+            # set new observation
+            self.set_target()
+            vo = self.difference_between_points(self.current_position, self.target_position)
+            self.vector_observation = np.array([vo[0] * np.cos(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.sin(
+                self.current_yaw * 22.5 / 180 * np.pi),
+                                                -vo[0] * np.sin(self.current_yaw * 22.5 / 180 * np.pi) + vo[1] * np.cos(
+                                                    self.current_yaw * 22.5 / 180 * np.pi),
+                                                vo[2]])
+            self.observation[:, :, 0] = np.copy(self.range_image)
+            self.observation[0, 0:3, 1] = np.copy(self.vector_observation)
+            finish_passed = (self.current_position.x < self.start_x - self.parkour_length)
+
         # check done
         # reward -= np.abs(self.current_yaw)*.5
-        if self.current_position.x > 20:  # length of parkour
+        if finish_passed:
+            reward = 20
             done = True
-        elif abs(self.current_position.y + 30) > 5:
+        elif abs(self.current_position.y - self.target_y) > 5:
             reward = -10
             done = True
         elif self.collision_flag:
@@ -138,36 +178,56 @@ class AgiEnv(gym.Env):
             done = True
         else:
             done = False
-        info = {"current_position": self.current_position}
+
+        info = {"current_position": self.current_position, "finish_passed": finish_passed}
         # np.save("depth_data/im"+str(self.image_count), self.range_image)
         # self.image_count += 1
-        # print("reward:", reward)
+        # print("reward:", reward, self.observation[0, 0:3, 1])
         return self.observation, reward, done, info
 
     def reset(self):
+        self.target_y = np.random.choice(self.target_y_list)
+        # if not self.episode_counter%2:
+        #     self.start_x = -10
+        #     self. forward_direction = True
+        # else:
+        #     self.start_x = 20
+        #     self.forward_direction = False
+        # self.episode_counter += 1
         # if self.current_position.y < -30:
         self.go_position(self.current_position.x, self.current_position.y, 8)
-        self.go_position(0, self.current_position.y, 8)
+        self.go_position(self.start_x, self.current_position.y, 8)
         # self.go_position(0, -29+np.random.randint(-2,3), 5)
         # else:
         # self.go_position(self.current_position.x, self.current_position.y, 5)
         # self.go_position(0, self.current_position.y, 5)
         # self.go_position(0, -29+np.random.randint(-2,3), 5)
-        self.go_position(0, -30 + np.random.randint(-2, 3), 5)
+        self.go_position(self.start_x, self.target_y, self.target_z)
+        # self.uav_trajectory = Path()
+        self.uav_trajectory.header.frame_id = "map"
+        self.update_trajectory()
+        self.publish_global_trajectory()
+
         # self.go_position(0, -29, 5)
         self.collision_flag = False
-        self.target_y = -29  # + np.random.randint(-1, 2)
+        # self.target_y = -29 # + np.random.randint(-1, 2)
         self.set_target()
-        self.vector_observation = self.difference_between_points(self.target_position, self.current_position)
+        if self.forward_direction:
+            self.vector_observation = self.difference_between_points(self.target_position, self.current_position)
+        else:
+            self.vector_observation = self.difference_between_points(self.current_position, self.target_position)
         self.observation[:, :, 0] = np.copy(self.range_image)
         self.observation[0, 0:3, 1] = np.copy(self.vector_observation)
         return self.observation
 
     def set_target(self):
         self.target_position = PoseStamped().pose.position
-        self.target_position.x = self.current_position.x + 5
+        if self.forward_direction:
+            self.target_position.x = self.current_position.x + 5
+        else:
+            self.target_position.x = self.current_position.x - 5
         self.target_position.y = self.target_y
-        self.target_position.z = 5
+        self.target_position.z = self.target_z
         self.publish_target()
 
     def render(self, mode='human', close=False):
@@ -177,10 +237,11 @@ class AgiEnv(gym.Env):
         self.current_position = data.pose.position
 
     def range_image_callback(self, data):
-        self.range_image = np.array(data.data).reshape((64, 64)) / 15.
+        self.range_image = np.clip(np.array(data.data).reshape((64, 64)), 0, 15) / 15.
 
     def model_name_callback(self, data):
-        self.model_name = data.data
+        if data.data[:5] == "robot":
+            self.model_name = data.data
 
     def collision_callback(self, data):
         if data.data:
@@ -208,8 +269,12 @@ class AgiEnv(gym.Env):
                            2: 0.3826834, 3: 0.5555702, 4: 0.7071068}
         quat_w_yaw_dict = {-4: 0.7071068, -3: 0.8314696, -2: 0.9238795, -1: 0.9807853, 0: 1.0, 1: 0.9807853,
                            2: 0.9238795, 3: 0.8314696, 4: 0.7071068}
-        goal.pose.orientation.z = quat_z_yaw_dict[yaw]
-        goal.pose.orientation.w = quat_w_yaw_dict[yaw]
+        if self.forward_direction:
+            goal.pose.orientation.z = quat_z_yaw_dict[yaw]
+            goal.pose.orientation.w = quat_w_yaw_dict[yaw]
+        else:
+            goal.pose.orientation.z = -quat_w_yaw_dict[yaw]
+            goal.pose.orientation.w = quat_z_yaw_dict[yaw]
         self.current_yaw = yaw
         self.ros_pub_pose.publish(goal)
         self.r.sleep()
@@ -233,6 +298,40 @@ class AgiEnv(gym.Env):
         goal.pose.orientation.z = 0.0
         goal.pose.orientation.w = 1.0
         self.ros_pub_target.publish(goal)
+
+    def update_trajectory(self):
+        new_point = PoseStamped()
+        new_point.header.seq = 1
+        new_point.header.stamp = rospy.Time.now()
+        new_point.header.frame_id = "map"
+        new_point.pose.position.x = self.current_position.x
+        new_point.pose.position.y = self.current_position.y
+        new_point.pose.position.z = self.current_position.z
+        self.uav_trajectory.poses.append(new_point)
+        self.ros_pub_trajectory.publish(self.uav_trajectory)
+
+    def publish_global_trajectory(self):
+        self.global_traj.header.frame_id = "map"
+        new_point = PoseStamped()
+        new_point.header.seq = 1
+        new_point.header.stamp = rospy.Time.now()
+        new_point.header.frame_id = "map"
+        new_point.pose.position.x = self.start_x
+        new_point.pose.position.y = self.target_y
+        new_point.pose.position.z = self.target_z
+        self.global_traj.poses.append(new_point)
+        new_point = PoseStamped()
+        new_point.header.seq = 1
+        new_point.header.stamp = rospy.Time.now()
+        new_point.header.frame_id = "map"
+        if self.forward_direction:
+            new_point.pose.position.x = self.start_x + self.parkour_length
+        else:
+            new_point.pose.position.x = self.start_x - self.parkour_length
+        new_point.pose.position.y = self.target_y
+        new_point.pose.position.z = self.target_z
+        self.global_traj.poses.append(new_point)
+        self.ros_pub_global_trajectory.publish(self.global_traj)
 
     def distance_between_points(self, p1, p2):
         x = p1.x - p2.x
