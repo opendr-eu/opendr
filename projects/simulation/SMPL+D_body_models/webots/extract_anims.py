dd0bb24588be7baaad02d5a4eca376a9c23c323e
charsyme <63857415+charsyme@users.noreply.github.com>
2021-12-06 12:44:24 +0200

    Update extract_anims.py

--- projects/simulation/SMPL+D_body_models/webots/extract_anims.py
+++ projects/simulation/SMPL+D_body_models/webots/extract_anims.py
@@ -18,15 +18,15 @@ import sys
 
 
 def getListOfFiles(dirName):
-    #create a list of file and sub directories 
-    #names in the given directory 
+    # create a list of file and sub directories 
+    # names in the given directory 
     listOfFile = os.listdir(dirName)
     allFiles = list()
     #Iterate over all the entries
     for entry in listOfFile:
-        #Create full path
+        # Create full path
         fullPath = os.path.join(dirName, entry)
-        #If entry is a directory then get the list of files in this directory 
+        # If entry is a directory then get the list of files in this directory 
         if os.path.isdir(fullPath):
             allFiles = allFiles + getListOfFiles(fullPath)
         else:
