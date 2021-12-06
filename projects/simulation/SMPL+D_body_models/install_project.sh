17486659edae718f7bb34f848c5352e464619a83
charsyme <63857415+charsyme@users.noreply.github.com>
2021-12-06 11:00:54 +0200

    Update install_project.sh

--- projects/simulation/SMPL+D_body_models/install_project.sh
+++ projects/simulation/SMPL+D_body_models/install_project.sh
@@ -5,3 +5,7 @@ cd $WEBOTS_HOME/projects/smpl_webots/libraries/smpl_util
 make
 cd $WEBOTS_HOME/projects/smpl_webots/controllers/smpl_animation
 make
+mkdir $WEBOTS_HOME/projects/smpl_webots/skins/model-202
+cp $OPENDR_HOME/projects/simulation/SMPL+D_body_models/fbx_models/female/model-202/model-202.fbx $WEBOTS_HOME/projects/smpl_webots/skins/model-202/model-202.fbx
+mkdir $WEBOTS_HOME/projects/smpl_webots/protos/textures/model-202
+cp $OPENDR_HOME/projects/simulation/SMPL+D_body_models/fbx_models/female/model-202/texture.png $WEBOTS_HOME/projects/smpl_webots/protos/textures/model-202/texture.png
