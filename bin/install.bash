69bc70726f623db5ff95905360c695cbd6a59f76
Nikolaos <passalis@csd.auth.gr>
2021-12-14 16:30:17 +0200

    Updated install and activate scripts

--- bin/install.bash
+++ bin/install.bash
@@ -1,20 +1,20 @@
 #!/bin/sh
 export OPENDR_HOME=$PWD
-export OPENDR_DEVICE=cpu
 export PYTHONPATH=$OPENDR_HOME/src:$PYTHONPATH
 export PYTHON=python3
 export DISABLE_BCOLZ_AVX2=true
 
+if [[ -z "${OPENDR_DEVICE}" ]]; then
+  echo "[INFO] Set available device to GPU. You can manually change this by running 'export OPENDR_DEVICE=cpu'."
+  export OPENDR_DEVICE=cpu
+fi
+
+# Install base ubuntu deps
+sudo apt-get install --yes libfreetype6-dev lsb-release git python python3-pip
+
 # Get all submodules
 git submodule init
 git submodule update
-
-# Create a virtual environment and update
-pip3 install virtualenv
-virtualenv -p python3 venv
-source venv/bin/activate
-python3 -m pip install -U pip
-pip3 install setuptools
 
 case $(lsb_release -r |cut -f2) in
   "18.04")
@@ -25,6 +25,13 @@ case $(lsb_release -r |cut -f2) in
     echo "Not tested for this ubuntu version" && exit 1;;
 esac
 
+# Create a virtual environment and update
+pip3 install virtualenv
+virtualenv -p python3 venv
+source venv/bin/activate
+python3 -m pip install -U pip
+pip3 install setuptools
+
 # Add repositories for ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' \
             && curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
@@ -32,3 +39,4 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 make install_compilation_dependencies
 make install_runtime_dependencies
 make install libopendrl
+deactivate
