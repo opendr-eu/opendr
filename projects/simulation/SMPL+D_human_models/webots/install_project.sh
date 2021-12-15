3f5f10b7d02df8790dce9edc011f83b975462cae
charsyme <63857415+charsyme@users.noreply.github.com>
2021-12-15 14:20:15 +0200

    Update install_project.sh

--- projects/simulation/SMPL+D_human_models/webots/install_project.sh
+++ projects/simulation/SMPL+D_human_models/webots/install_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r ./webots $WEBOTS_HOME/projects
+cp -r ./webots/smpl_webots $WEBOTS_HOME/projects
 cd $WEBOTS_HOME/projects/smpl_webots/libraries/smpl_util
 make
 cd $WEBOTS_HOME/projects/smpl_webots/controllers/smpl_animation
