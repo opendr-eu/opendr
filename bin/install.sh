9c77d0838ef70eed3bf319529904b3af06eb447e
Nikolaos Passalis <passalis@users.noreply.github.com>
2021-12-18 08:14:41 +0200

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -30,7 +30,7 @@ pip3 install virtualenv
 virtualenv -p python3 venv
 source venv/bin/activate
 python3 -m pip install -U pip
-pip3 install setuptools
+pip3 install setuptools configparser
 
 # Add repositories for ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' \
