1d73ee24484875372462464304eb257d321c02d0
Nikolaos Passalis <passalis@users.noreply.github.com>
2021-12-26 11:04:44 +0200

    Fix LD_LIBRARY_PATH

--- bin/activate.sh
+++ bin/activate.sh
@@ -2,7 +2,7 @@
 export OPENDR_HOME=$PWD
 export PYTHONPATH=$OPENDR_HOME/src:$PYTHONPATH
 export PYTHON=python3
-export LD_LIBRARY_PATH=$OPENDR_HOME/src:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$OPENDR_HOME/lib:$LD_LIBRARY_PATH
 
 if [[ -z "${OPENDR_DEVICE}" ]]; then
   echo "[INFO] Set available device to CPU. You can manually change this by running 'export OPENDR_DEVICE=gpu'."
