275a1fb9ae65724542c6ab4154678056135fadfe
Nikolaos <passalis@csd.auth.gr>
2022-01-20 15:17:45 +0200

    Split package creation

--- setup.py
+++ setup.py
@@ -1,53 +1,116 @@
+import os
+from os.path import join
 from setuptools import setup
 from setuptools import find_packages
 from Cython.Build import cythonize
+import numpy
+from configparser import ConfigParser
 
-packages = find_packages(where="./src")
-
-with open('requirements.txt') as fp:
-    install_requires = fp.read().splitlines()
-
+# Retrieve version
 exec(open('src/opendr/_version.py').read())
-
 try:
     __version__
 except NameError:
     __version__ = '0.0'
 
-setup(
-    name='opendr-toolkit',
-    version=__version__,
-    description='Open Deep Learning Toolkit for Robotics',
-    long_description="""The aim of OpenDR is to develop a modular, open and non-proprietary toolkit for core
-                     robotic functionalities by harnessing deep learning to provide advanced perception and
-                     cognition capabilities, meeting in this way the general requirements of robotics
-                     applications in the applications areas of healthcare, agri-food and agile production.
-                     The term toolkit in OpenDR refers to a set of deep learning software functions, packages
-                     and utilities used to help roboticists to develop and test a robotic application that
-                     incorporates deep learning. OpenDR will provide the means to link the robotics applications
-                     to software libraries (deep learning frameworks, e.g., Tensorflow) and to link it with the
-                     operating environment (ROS). OpenDR focuses on the AI and Cognition core technology in order
-                     to provide tools that make robotic systems cognitive, giving them the ability to a) interact
-                     with people and environments by developing deep learning methods for human centric and
-                     environment active perception and cognition, b) learn and categorise by developing deep
-                     learning tools for training and inference in common robotics settings, and c) make decisions
-                     and derive knowledge by developing deep learning tools for cognitive robot action and
-                     decision making (WP5). As a result, the developed OpenDR toolkit will also enable cooperative
-                     human-robot interaction as well as the development of cognitive mechatronics where sensing
-                     and actuation are closely coupled with cognitive systems thus contributing to another two
-                     core technologies beyond AI and Cognition. OpenDR will develop, train, deploy and evaluate
-                     deep learning models that improve the technical capabilities of the core technologies beyond
-                     the current state of the art. It will enable a greater range of robotics applications that
-                     can be demonstrated at TRL 3 and above, thus lowering the technical barriers within the
-                     prioritised application areas. OpenDR aims to an easily adopted methodology to adapt the
-                     provided tools in order to solve any robotics task without restricting it to any specific
-                     application""",
-    author='OpenDR consortium',
-    author_email='tefas@csd.auth.gr',
-    packages=packages,
-    url='https://github.com/opendr-eu/opendr',
-    license='LICENSE',
-    package_dir={"": "src"},
-    install_requires=install_requires,
-    ext_modules=cythonize(["src/opendr/perception/object_detection_2d/retinaface/algorithm/cython/*.pyx"])
-)
+# TODO: A common namespace should be defined (and still we will have conflicting files)...
+
+# Read the long description
+with open("description.txt") as f:
+    long_description = f.read()
+
+# Gather OpenDR submodules
+opendr_modules = []
+for base_dir in next(os.walk('src/opendr'))[1]:
+    if base_dir not in ('engine', '__pycache__'):
+        for x in next(os.walk('src/opendr/' + base_dir))[1]:
+            if x not in ([], '__pycache__'):
+                opendr_modules.append(join(base_dir, x))
+opendr_modules.append('/engine')
+
+# Add opendr-toolkit as meta package
+opendr_modules.append('/opendr-toolkit')
+
+for current_module in opendr_modules:
+    module_name = current_module.split("/")[1]
+
+    # Read the dependencies.ini
+    try:
+        parser = ConfigParser()
+        parser.read(join("src/opendr", current_module, 'dependencies.ini'))
+        dependencies = parser.get("runtime", "python").split('\n')
+        dependencies = [x for x in dependencies if 'git' not in x]
+    except Exception:
+        dependencies = []
+
+    if module_name == "opendr-toolkit":
+        dependencies = []
+        for x in opendr_modules:
+            if x not in ("/opendr-toolkit", "/engine"):
+                dependencies.append("opendr-toolkit-" + x.split("/")[1].replace("_", "-"))
+        dependencies.append("opendr-toolkit-engine")
+
+    # Gather all files
+    data_files = []
+
+    def traverse_dir(current_module):
+        for root, dirs, files in os.walk(join("src/opendr", current_module)):
+            for file in files:
+                file_extension = file.split(".")[-1]
+                # Add all files except from shared libraries
+                if file_extension != "so" and file_extension != "py":
+                    data_files.append(join(root.replace("src/opendr/", ""), file))
+
+    traverse_dir(current_module)
+    if module_name == 'engine':
+        traverse_dir('utils')
+
+    packages = find_packages(where="./src")
+    if module_name == 'engine':
+        packages = ['opendr.engine', 'opendr.utils']
+    else:
+        packages = [x for x in packages if module_name in x]
+    name = "opendr-toolkit-" + module_name.replace("_", "-")
+    print(len(data_files), len(packages))
+
+    if module_name == 'opendr-toolkit':
+        name = 'opendr-toolkit'
+        description = 'Open Deep Learning Toolkit for Robotics '
+    else:
+        name = 'opendr-toolkit-' + module_name.replace("_", "-")
+        description = 'Open Deep Learning Toolkit for Robotics (submodule: ' + current_module + ')'
+
+    if current_module == 'perception/object_detection_2d':
+        extra_params = {
+            'ext_modules': cythonize(["src/opendr/perception/object_detection_2d/retinaface/algorithm/cython/*.pyx"]),
+            'include_dirs': [numpy.get_include()]}
+    else:
+        extra_params = {}
+
+    with open("MANIFEST.in", "w") as f:
+        if module_name == 'opendr-toolkit':
+            pass
+        elif module_name == "engine":
+            f.write("recursive-include src/opendr/engine *\n")
+            f.write("include src/opendr/engine *\n")
+            f.write("include src/opendr/utils *\n")
+        else:
+            f.write("recursive-include src/opendr/" + current_module + " *\n")
+            f.write("include src/opendr/" + current_module.split("/")[0] + " *\n")
+        f.write("include description.txt\n")
+
+    setup(
+        name=name,
+        version=__version__,
+        description=description,
+        long_description=long_description,
+        author='OpenDR consortium',
+        author_email='tefas@csd.auth.gr',
+        packages=packages,
+        url='https://github.com/opendr-eu/opendr',
+        license='LICENSE',
+        package_dir={"": "src"},
+        install_requires=dependencies,
+        # package_data={'': data_files},
+        **extra_params
+    )
