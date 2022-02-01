5514099b4244d7c02f80493327fa0926b5c5ac0a
Nikolaos <passalis@csd.auth.gr>
2022-02-01 15:16:00 +0200

    Added separate setup.py

--- bin/build_wheel.sh
+++ bin/build_wheel.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# Build all OpenDR dependecies
-./bin/install.sh
 
 # Activate OpenDR
 source ./bin/activate.sh
+export OPENDR_HOME=$(pwd)
 
-# Prepare requirements.txt for wheel distributions
-pip3 freeze > requirements.txt
+
+rm dist/*
+rm src/*egg-info -rf
 
 # Remove detectron and git repositories (installation not supported through PyPI)
-sed -i '/detectron2/d' requirements.txt
-sed -i '/git/d' requirements.txt
-sed -i '/pkg_resources/d' requirements.txt
-sed -i '/auditwheel/d' requirements.txt
+#sed -i '/detectron2/d' requirements.txt
+#sed -i '/git/d' requirements.txt
+#sed -i '/pkg_resources/d' requirements.txt
+#sed -i '/auditwheel/d' requirements.txt
 
-# Build binary wheel and repair it
-python3 setup.py sdist
+# Build OpenDR packages
+while read p; do
+  echo "Build wheel for $p"
+  cd src/opendr/$p
+  cd $OPENDR_HOME
+done < packages.txt
+
+#cd src/opendr/engine
+#python3 setup.py sdist --dist-dir=$OPENDR_HOME/dist
+#cd $OPENDR_HOME
+#
+## Build hyperparameter tuner package
+#cd src/opendr/utils/hyperparameter_tuner
+#python3 setup.py sdist --dist-dir=$OPENDR_HOME/dist
+#cd $OPENDR_HOME
+#
+#
+#pip install dist/opendr-toolkit-engine-1.0.tar.gz
+#pip install dist/opendr-toolkit-hyperparameter-tuner-1.0.tar.gz
---
+++ src/opendr/perception/face_recognition/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+import os
+from os.path import join
+
+# Import tools for setup
+exec(open(join(os.environ['OPENDR_HOME'], 'src/opendr/_setup.py')).read())
+build_package("perception/face_recognition")
