cdbe7ac9026ab0d06c6f97209ac32ed89cb91750
Nikolaos <passalis@csd.auth.gr>
2022-02-02 09:34:14 +0200

    Cleaned build_wheel.sh

--- bin/build_wheel.sh
+++ bin/build_wheel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Fetch any submodules missing
+git submodule update --init --recursive
+
 # Activate OpenDR
 source venv/bin/activate
 
@@ -17,21 +20,3 @@ done < packages.txt
 
 rm setup.py
 rm MANIFEST.in
-
-# Install the built packages
-#pip install dist/opendr-toolkit-engine-1.0.tar.gz
-#pip install dist/opendr-toolkit-face-recognition-1.0.tar.gz
-#pip install dist/opendr-toolkit-pose-estimation-1.0.tar.gz
-#pip install dist/opendr-toolkit-hyperparameter-tuner-1.0.tar.gz
-#pip install dist/opendr-toolkit-semantic-segmentation-1.0.tar.gz
-#pip install dist/opendr-toolkit-speech-recognition-1.0.tar.gz
-#pip install dist/opendr-toolkit-compressive-learning-1.0.tar.gz
-#pip install dist/opendr-toolkit-facial-expression-recognition-1.0.tar.gz
-#pip install dist/opendr-toolkit-heart-anomaly-detection-1.0.tar.gz
-#pip install dist/opendr-toolkit-human-model-generation-1.0.tar.gz
-#pip install dist/opendr-toolkit-multimodal-human-centric-1.0.tar.gz
-#pip install dist/opendr-toolkit-skeleton-based-action-recognition-1.0.tar.gz
-#pip install dist/opendr-toolkit-activity-recognition-1.0.tar.gz
-#pip install dist/opendr-toolkit-object-detection-2d-1.0.tar.gz
-
-
