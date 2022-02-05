96ff29e4425f6ee2e588d1497a98e4907f6fb800
Jose Arce y de la Borbolla <joseab10@gmail.com>
2022-02-05 13:43:55 +0100

    Type hints and docstrings.

--- projects/perception/panoptic_segmentation/efficient_lps/example_usage.py
+++ projects/perception/panoptic_segmentation/efficient_lps/example_usage.py
@@ -22,26 +22,37 @@ from opendr.engine.data import PointCloud
 from opendr.perception.panoptic_segmentation import EfficientLpsLearner, SemanticKittiDataset, EfficientPsLearner
 
 
-def download_models(*, cp_dir, **_):
+def download_models(*,
+					cp_dir: str,
+					**_) -> None:
 	"""
-	TODO:
-	:param cp_dir:
-	:return:
+	Downloads models pre-trained on the KITTI and NuScenes datasets.
+
+	:param cp_dir:Path to where the pre-trained model checkpoint files will be saved.
+	:type cp_dir: str
+	:return: None
 	"""
 
 	EfficientLpsLearner.download(cp_dir, trained_on='nuscenes')
 	EfficientLpsLearner.download(cp_dir, trained_on='kitti')
 
 
-def train(*, kitti_dir, cp_dir, log_dir, **_):
+def train(*,
+		  kitti_dir: str,
+		  cp_dir: str,
+		  log_dir: str,
+		  **_) -> None:
 	"""
-	TODO:
+	Trains an EfficientLPS model on the KITTI dataset for 2 iterations, logs the output and saves a checkpoint.
 
-	:param kitti_dir:
-	:type kitti_dir:
-	:param cp_dir:
-	:param log_dir:
-	:return:
+	:param kitti_dir: Path to where the KITTI dataset is located. Must be the parent of the sequences/ directory.
+	:type kitti_dir: str
+	:param cp_dir: Path to where the pre-trained model checkpoint file is located.
+		The checkpoint file itself must be called model.pth.
+	:type cp_dir: str
+	:param log_dir: Path to where the training logs will be saved.
+	:type log_dir: str
+	:return: None
 	"""
 
 	train_data = SemanticKittiDataset(kitti_dir, split="train")
@@ -61,12 +72,19 @@ def train(*, kitti_dir, cp_dir, log_dir, **_):
 	assert train_stats  # This assert is just a workaround since pyflakes does not support the NOQA comment
 
 
-def evaluate(*, kitti_dir, cp_dir, **_):
+def evaluate(*,
+			 kitti_dir: str,
+			 cp_dir: str,
+			 **_) -> None:
 	"""
-	TODO:
-	:param kitti_dir:
-	:param cp_dir:
-	:return:
+	Evaluates a pre-trained model on the KITTI dataset evaluation data split.
+
+	:param kitti_dir: Path to where the KITTI dataset is located. Must be the parent of the sequences/ directory.
+	:type kitti_dir: str
+	:param cp_dir: Path to where the pre-trained model checkpoint file is located.
+		The checkpoint file itself must be called model.pth.
+	:type cp_dir: str
+	:return: None
 	"""
 
 	val_dataset = SemanticKittiDataset(path=kitti_dir, split="valid")
@@ -77,18 +95,35 @@ def evaluate(*, kitti_dir, cp_dir, **_):
 	assert eval_stats  # This assert is just a workaround since pyflakes does not support the NOQA comment
 
 
-def inference(*, kitti_dir, cp_dir, projected=False,
-			  save_figure=False, display_figure=False,
-			  detailed=False, **_):
+def inference(*,
+			  kitti_dir: str,
+			  cp_dir: str,
+			  projected: bool = False,
+			  save_figure: bool = False,
+			  display_figure: bool = False,
+			  detailed: bool = False,
+			  **_) -> None:
 	"""
-	TODO
-	:param kitti_dir:
-	:param cp_dir:
-	:param projected:
-	:param save_figure:
-	:param display_figure:
-	:param detailed:
-	:return:
+	Using a pretrained model, it makes panopptic label inferences on a predefined set of KITTI data and displays/saves
+	the output as plots.
+
+	:param kitti_dir: Path to where the KITTI dataset is located. Must be the parent of the sequences/ directory.
+	:type kitti_dir: str
+	:param cp_dir: Path to where the pre-trained model checkpoint file is located.
+		The checkpoint file itself must be called model.pth.
+	:type cp_dir: str
+	:param projected: Determines whether the output will be displayed as a 2D image of the spherical projection if True,
+		or as a 3D point cloud.
+	:type projected: bool
+	:param save_figure: If set to True, the resulting image will be saved to a *.png file in the current working
+		directory.
+	:type save_figure: bool
+	:param display_figure: If set to True, an interactive window will open for displaying the output.
+	:type display_figure: bool
+	:param detailed: Only used if projected==True. If True, then the output will be shown as a grid of the input,
+		panoptic  labels, semantic labels and contours. Otherwise, a single image combining the aforementioned outputs
+		will be generated.
+	:return: None
 	"""
 
 	pointcloud_filenames = [
@@ -122,8 +157,10 @@ def inference(*, kitti_dir, cp_dir, projected=False,
 
 def parse_args() -> dict:
 	"""
-	TODO
-	:return:
+	Parses the command line arguments.
+
+	:return: Dictionary of argument keys and values
+	:rtype: dict
 	"""
 
 	# Default values
@@ -133,9 +170,14 @@ def parse_args() -> dict:
 
 	def _resolve_path(path: str) -> str:
 		"""
-		TODO
-		:param path:
-		:return:
+		Takes a path string in, and makes it absolute and with the user and, potentially,
+		 other environmental variables resolved.
+		 For use with argument parser as argument type for seamless parsing of paths.
+
+		:param path: A path to become absolute and resolved.
+		:type path: str
+		:return: An absolute, resolved path, with variables expanded.
+		:rtype: str
 		"""
 		return str(Path(path).expanduser().resolve())
 
@@ -171,7 +213,7 @@ def parse_args() -> dict:
 	return kwargs
 
 
-def main():
+def main() -> None:
 	kwargs = parse_args()
 
 	if not kwargs["skip_download"]:
