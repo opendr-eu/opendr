6a32fa9d1983b92510b050515e030699aabd8222
Negar <negar.heidari@eng.au.dk>
2022-02-22 20:57:34 +0100

    mcdo is modified

--- projects/perception/facial_expression_recognition/landmark_based_facial_expression_recognition/benchmark/benchmark_pstbln.py
+++ projects/perception/facial_expression_recognition/landmark_based_facial_expression_recognition/benchmark/benchmark_pstbln.py
@@ -92,7 +92,7 @@ def benchmark_pstbln(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cpu")
+    parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda")
     parser.add_argument('--method', type=str, default='pstbln_afew',
                         help='action detection method')
     parser.add_argument('--dataset_name', type=str, default='afew',
