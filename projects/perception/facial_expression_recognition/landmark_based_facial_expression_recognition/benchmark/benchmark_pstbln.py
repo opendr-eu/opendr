779d60f3a39d62219993cd2d648f43fd9d598cda
Negar <negar.heidari@eng.au.dk>
2022-02-22 20:44:15 +0100

    mcdo is modified

--- projects/perception/facial_expression_recognition/landmark_based_facial_expression_recognition/benchmark/benchmark_pstbln.py
+++ projects/perception/facial_expression_recognition/landmark_based_facial_expression_recognition/benchmark/benchmark_pstbln.py
@@ -72,7 +72,7 @@ def benchmark_pstbln(args):
         return sample
 
     print("== Benchmarking learner.infer ==")
-    results1 = benchmark(model=learner.infer,
+    results1 = benchmark(model=learner.infer(monte_carlo_dropout=False),
                          sample=samples,
                          sample_with_batch_size1=None,
                          num_runs=num_runs,
