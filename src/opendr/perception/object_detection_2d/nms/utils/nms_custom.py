90bbe6fe5205af41615412520ae2ced2d5580e9d
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-08 11:19:47 +0200

    Update nms_custom.py

--- src/opendr/perception/object_detection_2d/nms/utils/nms_custom.py
+++ src/opendr/perception/object_detection_2d/nms/utils/nms_custom.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class NMSCustom(ABC):
     @abstractmethod
-    def run_nms(self, boxes=None, scores=None, threshold=0.2, img=None):
+    def run_nms(self, boxes=None, scores=None, threshold=0.2, img=None, device='cpu'):
         pass
