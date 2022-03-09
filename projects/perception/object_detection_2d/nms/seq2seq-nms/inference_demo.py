8a193b3db9d410671191abea402336cfef4e9dbe
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-09 18:46:10 +0200

    Update inference_demo.py

--- projects/perception/object_detection_2d/nms/seq2seq-nms/inference_demo.py
+++ projects/perception/object_detection_2d/nms/seq2seq-nms/inference_demo.py
@@ -2,14 +2,12 @@ from opendr.perception.object_detection_2d.nms.seq2seq_nms.seq2seq_nms_learner i
 from opendr.engine.data import Image
 from opendr.perception.object_detection_2d import SingleShotDetectorLearner
 from opendr.perception.object_detection_2d import draw_bounding_boxes
-import datetime
 import os
 OPENDR_HOME = os.environ['OPENDR_HOME']
 
-seq2SeqNMSLearner = Seq2SeqNMSLearner(fmod_map_type='EDGEMAP', iou_filtering = 0.8, experiment_name='auth_exp51', app_feats = 'fmod',
-                                      fmod_init_path=OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/'
-                                                                   'datasets/PETS/FMoD/pets_edgemap_b.pkl', device='cpu')
-seq2SeqNMSLearner.load(OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/temp/auth_exp51/'
+seq2SeqNMSLearner = Seq2SeqNMSLearner(fmod_map_type='EDGEMAP', iou_filtering = 0.8, experiment_name='pets_exp0',
+                                      app_feats='fmod', device='cpu')
+seq2SeqNMSLearner.load(OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/temp/pets_exp0/'
                                      'checkpoints/checkpoint_epoch_7', verbose=True)
 ssd = SingleShotDetectorLearner(device='cuda')
 ssd.download(".", mode="pretrained")
@@ -17,5 +15,5 @@ ssd.load("./ssd_default_person", verbose=True)
 img = Image.open(OPENDR_HOME + '/projects/perception/object_detection_2d/nms/img_temp/frame_0000.jpg')
 if not isinstance(img, Image):
     img = Image(img)
-boxes = ssd.infer(img, threshold=0.6, custom_nms=seq2SeqNMSLearner)
+boxes = ssd.infer(img, threshold=0.25, custom_nms=seq2SeqNMSLearner)
 draw_bounding_boxes(img.opencv(), boxes, class_names=ssd.classes, show=True)
