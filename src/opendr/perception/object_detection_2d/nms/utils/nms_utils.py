8705b07e1540958c3b2c18312d195d4bae1102b7
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-09 21:31:46 +0200

    Update nms_utils.py

--- src/opendr/perception/object_detection_2d/nms/utils/nms_utils.py
+++ src/opendr/perception/object_detection_2d/nms/utils/nms_utils.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import torch
+
 
 def jaccard(box_a, box_b, iscrowd: bool = False):
     """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
