231e3bc21bff4f31d5de75d6a5e8dfb29288ca26
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-11 17:02:04 +0200

    Update seq2seq_nms_learner.py

--- src/opendr/perception/object_detection_2d/nms/seq2seq_nms/seq2seq_nms_learner.py
+++ src/opendr/perception/object_detection_2d/nms/seq2seq_nms/seq2seq_nms_learner.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import cv2.cv2
 import torch
 import torch.nn.functional as F
 import pickle
