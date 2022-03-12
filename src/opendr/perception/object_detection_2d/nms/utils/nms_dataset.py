61e188d1de9fc46e940ba1ec3dcbe11220781cf2
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-12 15:37:50 +0200

    Update nms_dataset.py

--- src/opendr/perception/object_detection_2d/nms/utils/nms_dataset.py
+++ src/opendr/perception/object_detection_2d/nms/utils/nms_dataset.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from opendr.engine.datasets import Dataset
 import os
 from urllib.request import urlretrieve
