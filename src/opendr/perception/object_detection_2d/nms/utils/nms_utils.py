ef46d4b91aa9f95fa4ee1aec71515be9adf1dea2
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-16 15:06:50 +0200

    Update nms_utils.py

--- src/opendr/perception/object_detection_2d/nms/utils/nms_utils.py
+++ src/opendr/perception/object_detection_2d/nms/utils/nms_utils.py
@@ -1,33 +1,13 @@
-# Copyright 2020-2022 OpenDR European Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import torch
+import torchvision
+import numpy as np
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+import sys
+import os
 
 
 def jaccard(box_a, box_b, iscrowd: bool = False):
-    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
-    is simply the intersection over union of two boxes.  Here we operate on
-    ground truth boxes and default boxes. If iscrowd=True, put the crowd in box_b.
-    E.g.:
-        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
-    Args:
-        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
-        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
-    Return:
-        jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
-    """
     use_batch = True
     if box_a.dim() == 2:
         use_batch = False
@@ -46,16 +26,6 @@ def jaccard(box_a, box_b, iscrowd: bool = False):
 
 
 def intersect(box_a, box_b):
-    """ We resize both tensors to [A,B,2] without new malloc:
-    [A,2] -> [A,1,2] -> [A,B,2]
-    [B,2] -> [1,B,2] -> [A,B,2]
-    Then we compute the area of intersect between box_a and box_b.
-    Args:
-      box_a: (tensor) bounding boxes, Shape: [n,A,4].
-      box_b: (tensor) bounding boxes, Shape: [n,B,4].
-    Return:
-      (tensor) intersection area, Shape: [n,A,B].
-    """
     n = box_a.size(0)
     A = box_a.size(1)
     B = box_b.size(1)
@@ -67,17 +37,6 @@ def intersect(box_a, box_b):
 
 
 def diou(box_a, box_b, iscrowd: bool = False):
-    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
-    is simply the intersection over union of two boxes.  Here we operate on
-    ground truth boxes and default boxes. If iscrowd=True, put the crowd in box_b.
-    E.g.:
-        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
-    Args:
-        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
-        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
-    Return:
-        jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
-    """
     use_batch = True
     if box_a.dim() == 2:
         use_batch = False
@@ -114,17 +73,6 @@ def diou(box_a, box_b, iscrowd: bool = False):
 
 
 def distance(box_a, box_b, iscrowd: bool = False):
-    """Compute the jaccard overlap of two sets of boxes.  The jaccard overlap
-    is simply the intersection over union of two boxes.  Here we operate on
-    ground truth boxes and default boxes. If iscrowd=True, put the crowd in box_b.
-    E.g.:
-        A ∩ B / A ∪ B = A ∩ B / (area(A) + area(B) - A ∩ B)
-    Args:
-        box_a: (tensor) Ground truth bounding boxes, Shape: [num_objects,4]
-        box_b: (tensor) Prior boxes from priorbox layers, Shape: [num_priors,4]
-    Return:
-        jaccard overlap: (tensor) Shape: [box_a.size(0), box_b.size(0)]
-    """
     use_batch = True
     if box_a.dim() == 2:
         use_batch = False
@@ -154,3 +102,154 @@ def distance(box_a, box_b, iscrowd: bool = False):
     D = (((x2 - x1) ** 2 + (y2 - y1) ** 2) / ((cr - cl) ** 2 + (cb - ct) ** 2 + 1e-7)) ** 0.6
     out = D if iscrowd else D
     return out if use_batch else out.squeeze(0)
+
+
+def det_matching(scores, dt_boxes, gt_boxes, iou_thres, device='cuda'):
+    sorted_indices = torch.argsort(-scores, dim=0)
+    labels = torch.zeros(len(dt_boxes))
+    if device == 'cuda':
+        labels = labels.cuda()
+    if gt_boxes.shape[0] == 0:
+        return labels.unsqueeze(-1)
+    assigned_GT = -torch.ones(len(gt_boxes))
+    r = torch.tensor([-1, -1, -1, -1]).float().unsqueeze(0).unsqueeze(0)
+    if device == 'cuda':
+        r = r.cuda()
+    for s in sorted_indices:
+        gt_boxes_c = gt_boxes.clone().unsqueeze(0)
+        gt_boxes_c[0, assigned_GT > -1, :] = r
+        ious = bb_intersection_over_union(boxAs=dt_boxes[s].clone().unsqueeze(0), boxBs=gt_boxes_c)
+        annot_iou, annot_box_id = torch.sort(ious.squeeze(), descending=True)
+        if annot_box_id.ndim > 0:
+            annot_box_id = annot_box_id[0]
+            annot_iou = annot_iou[0]
+        if annot_iou > iou_thres:
+            assigned_GT[annot_box_id] = s
+            labels[s] = 1
+    return labels.unsqueeze(-1)
+
+
+def run_coco_eval(dt_file_path=None, gt_file_path=None, only_classes=None, max_dets=None,
+                  verbose=False):
+    if max_dets is None:
+        max_dets = [200, 400, 600, 800, 1000, 1200]
+    results = []
+    sys.stdout = open(os.devnull, 'w')
+    for i in range(len(max_dets)):
+        coco = COCO(gt_file_path)
+        coco_dt = coco.loadRes(dt_file_path)
+        cocoEval = COCOeval(coco, coco_dt, 'bbox')
+        cocoEval.params.iouType = 'bbox'
+        cocoEval.params.useCats = True
+        cocoEval.params.catIds = only_classes
+        cocoEval.params.maxDets = [max_dets[i]]
+        cocoEval.evaluate()
+        results.append([summarize_nms(coco_eval=cocoEval, maxDets=max_dets[i]), max_dets[i]])
+        # print(results[i])
+    del cocoEval, coco_dt, coco
+    sys.stdout = sys.__stdout__
+    return results
+
+
+def summarize_nms(coco_eval=None, maxDets=100):
+    def summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
+        p = coco_eval.params
+        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+        typeStr = '(AP)' if ap == 1 else '(AR)'
+        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if iouThr is None else '{:0.2f}'.format(iouThr)
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+        if ap == 1:
+            # dimension of precision: [TxRxKxAxM]
+            s = coco_eval.eval['precision']
+            # IoU
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:, :, :, aind, mind]
+        else:
+            # dimension of recall: [TxKxAxM]
+            s = coco_eval.eval['recall']
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:, :, aind, mind]
+        if len(s[s > -1]) == 0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s > -1])
+        stat_str = iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s)
+        return [mean_s, stat_str]
+
+    def summarizeDets():
+        stats = []
+        stat, stat_str = summarize(1, maxDets=maxDets)
+        stats.append([stat, stat_str])
+        stat, stat_str = summarize(1, iouThr=.5, maxDets=maxDets)
+        stats.append([stat, stat_str])
+        stat, stat_str = summarize(1, iouThr=.75, maxDets=maxDets)
+        stats.append([stat, stat_str])
+        stat, stat_str = summarize(0, maxDets=maxDets)
+        stats.append([stat, stat_str])
+        return stats
+
+    coco_eval.accumulate()
+    summarized = summarizeDets()
+    return summarized
+
+
+def drop_dets(boxes, scores, keep_ratio=0.85):
+    ids = np.arange(len(boxes))
+    np.random.shuffle(ids)
+    ids_keep = ids[0:int(len(boxes) * keep_ratio)]
+    boxes_new = boxes[ids_keep, :]
+    scores_new = scores[ids_keep]
+    return boxes_new, scores_new
+
+
+def filter_iou_boxes(boxes=None, iou_thres=0.2):
+    ious = bb_intersection_over_union(boxes.unsqueeze(1).repeat(1, boxes.shape[0], 1),
+                                      boxes.clone().unsqueeze(0).repeat(boxes.shape[0], 1, 1))
+    ids_boxes = ious >= iou_thres
+    return ids_boxes
+
+
+def bb_intersection_over_union(boxAs=None, boxBs=None):
+    xA = torch.maximum(boxAs[:, :, 0], boxBs[:, :, 0])
+    yA = torch.maximum(boxAs[:, :, 1], boxBs[:, :, 1])
+    xB = torch.minimum(boxAs[:, :, 2], boxBs[:, :, 2])
+    yB = torch.minimum(boxAs[:, :, 3], boxBs[:, :, 3])
+    interAreas = torch.maximum(torch.zeros_like(xB), xB - xA + 1) * torch.maximum(torch.zeros_like(yB), yB - yA + 1)
+    boxAAreas = (boxAs[:, :, 2] - boxAs[:, :, 0] + 1) * (boxAs[:, :, 3] - boxAs[:, :, 1] + 1)
+    boxBAreas = (boxBs[:, :, 2] - boxBs[:, :, 0] + 1) * (boxBs[:, :, 3] - boxBs[:, :, 1] + 1)
+    ious = interAreas / (boxAAreas + boxBAreas - interAreas)
+    return ious
+
+
+def compute_class_weights(pos_weights, max_dets=400, dataset_nms=None):
+    num_pos = np.ones([len(dataset_nms.classes), 1])
+    num_bg = np.ones([len(dataset_nms.classes), 1])
+    weights = np.zeros([len(dataset_nms.classes), 2])
+    for i in range(len(dataset_nms.src_data)):
+        for cls_index in range(len(dataset_nms.classes)):
+            num_pos[cls_index] = num_pos[cls_index] + \
+                                 min(max_dets, len(dataset_nms.src_data[i]['gt_boxes'][cls_index]))
+            num_bg[cls_index] = num_bg[cls_index] + max(0, min(max_dets,
+                                                               len(dataset_nms.src_data[i]['dt_boxes'][cls_index])) -
+                                                        min(max_dets,
+                                                            len(dataset_nms.src_data[i]['gt_boxes'][cls_index])))
+    for class_index in range(len(dataset_nms.classes)):
+        weights[class_index, 0] = (1 - pos_weights[class_index]) * (num_pos[class_index] +
+                                                                    num_bg[class_index]) / num_bg[class_index]
+        weights[class_index, 1] = pos_weights[class_index] * (num_pos[class_index] +
+                                                              num_bg[class_index]) / num_pos[class_index]
+    return weights
+
+
+def apply_torchNMS(boxes, scores, iou_thres):
+    ids_nms = torchvision.ops.nms(boxes, scores, iou_thres)
+    scores = scores[ids_nms]
+    boxes = boxes[ids_nms]
+    return boxes, scores
