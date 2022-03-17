1ad6e98c02393f804f9cc1433706d7ca968e8d6c
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-17 17:11:43 +0200

    Update train_demo.py

--- projects/perception/object_detection_2d/nms/seq2seq-nms/train_demo.py
+++ projects/perception/object_detection_2d/nms/seq2seq-nms/train_demo.py
@@ -17,12 +17,26 @@ from opendr.perception.object_detection_2d.nms.seq2seq_nms.seq2seq_nms_learner i
 import os
 OPENDR_HOME = os.environ['OPENDR_HOME']
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", help="Dataset to train on", type=str, default="voc", choices=["PETS", "COCO",
+                                                                                                   "TEST_MODULE"])
+    parser.add_argument("--data_root", help="Dataset root folder", type=str)
+    parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
+    parser.add_argument("--max_dt_boxes", help="Maximum number of input RoIs to Seq2Seq-NMS", type=int, default=450)
+    parser.add_argument("--nms_gt_iou", help="Learning rate to use for training", type=float, default=5e-4)
+    parser.add_argument("--val-after", help="Epochs in-between  evaluations", type=int, default=5)
+    parser.add_argument("--checkpoint-freq", help="Frequency in-between checkpoint saving", type=int, default=5)
+    parser.add_argument("--n-epochs", help="Number of total epochs", type=int, default=25)
+    parser.add_argument("--resume-from", help="Epoch to load checkpoint file and resume training from", type=int, default=0)
+
+    args = parser.parse_args()
+
 seq2seq_tmp_path = OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/coco_tmp'
 seq2seq_logs_path = OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/coco_logs'
 
 seq2SeqNMSLearner = Seq2SeqNMSLearner(fmod_map_type='EDGEMAP', iou_filtering=None, app_feats='fmod',
                                       checkpoint_after_iter=1, temp_path=seq2seq_tmp_path, epochs=8)
-seq2SeqNMSLearner.fit(dataset='COCO', use_ssd=False,
-                      datasets_folder=OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/datasets',
+seq2SeqNMSLearner.fit(dataset=args.dataset, use_ssd=False,
+                      datasets_folder= args.data_root,
                       logging_path=seq2seq_logs_path, silent=False, verbose=True, nms_gt_iou=0.50,
-                      max_dt_boxes=450)
+                      max_dt_boxes=args.max_dt_boxes)
