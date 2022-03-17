949bcc1203d1c35b95b97e5f03a6c96d55d2e242
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-17 15:51:47 +0200

    Update inference_demo.py

--- projects/perception/object_detection_2d/nms/cluster_nms/inference_demo.py
+++ projects/perception/object_detection_2d/nms/cluster_nms/inference_demo.py
@@ -17,7 +17,6 @@ from opendr.perception.object_detection_2d.nms.cluster_nms.cluster_nms import Cl
 from opendr.engine.data import Image
 from opendr.perception.object_detection_2d import SingleShotDetectorLearner
 from opendr.perception.object_detection_2d import draw_bounding_boxes
-import datetime
 import os
 OPENDR_HOME = os.environ['OPENDR_HOME']
 
@@ -27,7 +26,6 @@ ssd.load("./ssd_default_person", verbose=True)
 img = Image.open(OPENDR_HOME + '/projects/perception/object_detection_2d/nms/img_temp/frame_0000.jpg')
 if not isinstance(img, Image):
     img = Image(img)
-start_time = datetime.datetime.now()
 cluster_nms = ClusterNMS(device='cuda', nms_type='default', cross_class=True)
 boxes = ssd.infer(img, threshold=0.3, custom_nms=cluster_nms)
 draw_bounding_boxes(img.opencv(), boxes, class_names=ssd.classes, show=True)
