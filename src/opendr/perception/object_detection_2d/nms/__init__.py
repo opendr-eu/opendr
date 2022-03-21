229b2b0c9a05cac0f5b048e63285f9c5e6f6c857
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-21 15:11:11 +0200

    Update __init__.py

--- src/opendr/perception/object_detection_2d/nms/__init__.py
+++ src/opendr/perception/object_detection_2d/nms/__init__.py
@@ -1,6 +1,6 @@
-from opendr.perception.object_detection_2d.nms.cluster_nms import ClusterNMS
-from opendr.perception.object_detection_2d.nms.fast_nms import FastNMS
-from opendr.perception.object_detection_2d.nms.soft_nms import SoftNMS
+from opendr.perception.object_detection_2d.nms.cluster_nms.cluster_nms import ClusterNMS
+from opendr.perception.object_detection_2d.nms.fast_nms.fast_nms import FastNMS
+from opendr.perception.object_detection_2d.nms.soft_nms.soft_nms import SoftNMS
 from opendr.perception.object_detection_2d.nms.seq2seq_nms.seq2seq_nms_learner import Seq2SeqNMSLearner
 
 from opendr.perception.object_detection_2d.nms.seq2seq_nms.seq2seq_nms_learner.algorithm.fmod import FMoD
