69c382ca002ae02f471dce417a84b2d297bafd82
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-21 20:50:09 +0200

    Delete __init__.py

--- src/opendr/perception/object_detection_2d/nms/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from opendr.perception.object_detection_2d.nms.utils.nms_dataset import Dataset_NMS
-from opendr.perception.object_detection_2d.nms.utils.nms_custom import NMSCustom
-
-
-__all__ = ['Dataset_NMS', 'NMSCustom']
