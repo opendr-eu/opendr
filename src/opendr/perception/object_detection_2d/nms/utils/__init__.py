775e1c22c6da140b0237702bcb29923778677542
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-21 19:22:47 +0200

    Create __init__.py

---
+++ src/opendr/perception/object_detection_2d/nms/utils/__init__.py
@@ -0,0 +1,5 @@
+from opendr.perception.object_detection_2d.nms.utils.nms_dataset import Dataset_NMS
+from opendr.perception.object_detection_2d.nms.utils.nms_custom import NMSCustom
+
+
+__all__ = ['Dataset_NMS', 'NMSCustom']
