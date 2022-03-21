b5cac43c7e41c676f99740c8c9198644da21ba93
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-21 14:20:18 +0200

    Update object_detection_2d_ssd.py

--- projects/opendr_ws/src/perception/scripts/object_detection_2d_ssd.py
+++ projects/opendr_ws/src/perception/scripts/object_detection_2d_ssd.py
@@ -22,10 +22,8 @@ from opendr_bridge import ROSBridge
 from opendr.engine.data import Image
 from opendr.perception.object_detection_2d import SingleShotDetectorLearner
 from opendr.perception.object_detection_2d import draw_bounding_boxes
-from opendr.perception.object_detection_2d.nms.seq2seq_nms.seq2seq_nms_learner import Seq2SeqNMSLearner
-from opendr.perception.object_detection_2d.nms.soft_nms.soft_nms import SoftNMS
-from opendr.perception.object_detection_2d.nms.fast_nms.fast_nms import FastNMS
-from opendr.perception.object_detection_2d.nms.cluster_nms.cluster_nms import ClusterNMS
+from opendr.perception.object_detection_2d.nms import Seq2SeqNMSLearner, SoftNMS, FastNMS, ClusterNMS
+
 
 
 class ObjectDetectionSSDNode:
