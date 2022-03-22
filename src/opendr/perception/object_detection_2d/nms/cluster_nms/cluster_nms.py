00594d5781c7b54eb03a6acc91d2b806954b5d67
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-22 17:31:34 +0200

    Update cluster_nms.py

--- src/opendr/perception/object_detection_2d/nms/cluster_nms/cluster_nms.py
+++ src/opendr/perception/object_detection_2d/nms/cluster_nms/cluster_nms.py
@@ -14,17 +14,17 @@
 
 # This file contains code from the CIoU distribution (https://github.com/Zzh-tju/CIoU).
 # Copyright (c) 2020 Zheng, Zhaohui.
-# 
-# This program is free software: you can redistribute it and/or modify  
-# it under the terms of the GNU General Public License as published by  
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License 
+# You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from opendr.perception.object_detection_2d.nms.utils import NMSCustom
