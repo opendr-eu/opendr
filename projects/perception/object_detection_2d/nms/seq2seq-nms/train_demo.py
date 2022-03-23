aa6ef6c52d63c8f9357b5e6a6fa100d137b7d616
charsyme <63857415+charsyme@users.noreply.github.com>
2022-03-23 18:35:53 +0200

    Update train_demo.py

--- projects/perception/object_detection_2d/nms/seq2seq-nms/train_demo.py
+++ projects/perception/object_detection_2d/nms/seq2seq-nms/train_demo.py
@@ -19,25 +19,32 @@ import argparse
 OPENDR_HOME = os.environ['OPENDR_HOME']
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", help="Dataset to train on", type=str, default="voc", choices=["PETS", "COCO",
-                                                                                               "TEST_MODULE"])
-parser.add_argument("--data_root", help="Dataset root folder", type=str)
+parser.add_argument("--app_feats", help="Type of appearance-based features", type=str, default="fmod",
+                    choices=["fmod", "zeros"])
+parser.add_argument("--fmod_type", help="Type of fmod maps", type=str, default="EDGEMAP",
+                    choices=["EDGEMAP", "FAST", "AKAZE", "BRISK", "ORB"])
+parser.add_argument("--iou_filtering", help="Pre-processing IoU threshold", type=float, default=1.0)
 parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
-parser.add_argument("--max_dt_boxes", help="Maximum number of input RoIs to Seq2Seq-NMS", type=int, default=450)
-parser.add_argument("--nms_gt_iou", help="Learning rate to use for training", type=float, default=5e-4)
-parser.add_argument("--val-after", help="Epochs in-between  evaluations", type=int, default=5)
-parser.add_argument("--checkpoint-freq", help="Frequency in-between checkpoint saving", type=int, default=5)
-parser.add_argument("--n-epochs", help="Number of total epochs", type=int, default=25)
+parser.add_argument("--lr", help="Learning rate to use for training", type=float, default=1e-4)
+parser.add_argument("--n_epochs", help="Number of total epochs", type=int, default=10)
+parser.add_argument("--tmp_path", help="Temporary path where weights will be saved", type=str,
+                    default=os.path.join(OPENDR_HOME, 'projects/perception/object_detection_2d/nms/seq2seq-nms/tmp'))
+parser.add_argument("--checkpoint_freq", help="Frequency in-between checkpoint saving", type=int, default=1)
 parser.add_argument("--resume-from", help="Epoch to load checkpoint file and resume training from", type=int, default=0)
-
+parser.add_argument("--dataset", help="Dataset to train on", type=str, default="PETS", choices=["PETS", "COCO",
+                                                                                                "TEST_MODULE"])
+parser.add_argument("--use_ssd", help="Train using SSD as default detector", type=bool, default=False)
+parser.add_argument("--max_dt_boxes", help="Maximum number of input RoIs fed to Seq2Seq-NMS", type=int, default=500)
+parser.add_argument("--data-root", help="Dataset root folder", type=str,
+                    default=os.path.join(OPENDR_HOME,
+                                         'projects/perception/object_detection_2d/nms/seq2seq-nms/datasets'))
 args = parser.parse_args()
-
-seq2seq_tmp_path = OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/coco_tmp'
-seq2seq_logs_path = OPENDR_HOME + '/src/opendr/perception/object_detection_2d/nms/seq2seq_nms/coco_logs'
-
-seq2SeqNMSLearner = Seq2SeqNMSLearner(fmod_map_type='EDGEMAP', iou_filtering=None, app_feats='fmod',
-                                      checkpoint_after_iter=1, temp_path=seq2seq_tmp_path, epochs=8)
-seq2SeqNMSLearner.fit(dataset=args.dataset, use_ssd=False,
-                      datasets_folder=args.data_root,
-                      logging_path=seq2seq_logs_path, silent=False, verbose=True, nms_gt_iou=0.50,
+seq2SeqNMSLearner = Seq2SeqNMSLearner(epochs=args.n_epochs, lr=args.lr, device=args.device, app_feats=args.app_feats,
+                                      fmod_map_type=args.fmod_type, iou_filtering=args.iou_filtering,
+                                      temp_path=args.tmp_path, checkpoint_after_iter=args.checkpoint_freq,
+                                      checkpoint_load_iter=args.resume_from)
+seq2SeqNMSLearner.fit(dataset=args.dataset, use_ssd=args.use_ssd,
+                      datasets_folder=args.data_root, silent=False, verbose=True,
                       max_dt_boxes=args.max_dt_boxes)
+seq2SeqNMSLearner.save(path=os.path.join(args.tmp_path, 'saved_model'), current_epoch=args.n_epochs-1,
+                       max_dt_boxes=args.max_dt_boxes)
