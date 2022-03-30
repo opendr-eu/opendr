5490b71c2ffee08f43c68fe0224a46b0da72326a
halil93ibrahim <halil@ece.au.dk>
2022-03-30 13:14:43 +0200

    End to end planning (#223)
    * v0
    
    * v0
    
    * Ros nodes and documentation.
    
    * fit method
    
    * cleanup
    
    * test for eval and infer
    
    * env updated
    
    * env updated - tests passed
    
    * pep8 - cleanup
    
    * pep8 - cleanup
    
    * pep8
    
    * licenses fixed
    
    * clang
    
    * test_sources
    
    * dep - test only this tool
    
    * dep - test only this tool
    
    * test only this tool
    
    * test only this tool
    
    * test only this tool
    
    * test only this tool
    
    * test python 3.6
    
    * test python 3.6
    
    * test python 3.6
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test python 3.8
    
    * test - exclude control/mm
    
    * test all
    
    * clean up
    
    * clean up
    
    * test
    
    * undo
    
    * install webots
    
    * Update e2e_planning_learner.py
    
    * Update download_ardupilot_files.py
    
    * Update range_image.py
    
    * Update agi_env.py
    
    * Update children_robot.cpp
    
    * Update take_off.cpp
    
    * gym version issue with save and load
    
    * gym version
    
    * clean up test licence skipped directory
    
    * gym version issue
    
    * gym version 0.19
    
    * stable baselines3 version 1.3
    
    * gym ver 0.20 sb3 ver 1.1
    
    * docs updated
    
    * docs updated
    
    * docs updated
    
    * fix
    
    * Update .github/workflows/tests_suite.yml
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    
    * Update .github/workflows/tests_suite_develop.yml
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    
    * Update docs/reference/end_to_end_planning.md
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    
    * update tests_suite.yml and tests_suite_develop.yml
    
    * added infer to docs
    
    * requested changes
    
    * fix pep8
    
    * init files
    
    * preformance evaluation and init files under tests & test/sources
    
    * undo
    
    * test
    
    * Revert "test"
    
    This reverts commit 7f20428174051bbf5ac046ecfc1ad10877fba0ea.
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    
    * added TEST_DEVICE in tests
    
    * eval/fit return dictionary
    
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>
    Co-authored-by: ad-daniel <daniel.dias@epfl.ch>
    Co-authored-by: Illia Oleksiienko <io@ece.au.dk>
    Co-authored-by: Nikolaos Passalis <passalis@users.noreply.github.com>

---
+++ src/opendr/planning/end_to_end_planning/download_ardupilot_files.py
@@ -0,0 +1,20 @@
+# Copyright 2020-2022 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from urllib.request import urlretrieve
+from opendr.engine.constants import OPENDR_SERVER_URL
+
+url = OPENDR_SERVER_URL + "planning/end_to_end_planning/ardupilot.zip"
+file_destination = "./ardupilot.zip"
+urlretrieve(url=url, filename=file_destination)
---
+++ src/opendr/planning/end_to_end_planning/src/range_image.py
@@ -0,0 +1,43 @@
+# Copyright 2020-2022 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import String
+from cv_bridge import CvBridge
+
+
+class range_image_node():
+
+    def __init__(self):
+        rospy.init_node('listener', anonymous=True)
+        self.r = rospy.Rate(10)
+        self.raw_image_pub = rospy.Publisher('range_image_raw', Float32MultiArray, queue_size=10)
+        rospy.Subscriber("/model_name", String, self.model_name_callback)
+        self.r.sleep()
+        rospy.Subscriber("/" + self.model_name + "/range_finder/range_image", Image, self.range_callback)
+        rospy.spin()
+
+    def range_callback(self, data):
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(data)
+        arr = Float32MultiArray()
+        arr.data = list(cv_image.reshape(4096))
+        self.raw_image_pub.publish(arr)
+
+    def model_name_callback(self, data):
+        self.model_name = data.data
+
+
+node_class = range_image_node()
