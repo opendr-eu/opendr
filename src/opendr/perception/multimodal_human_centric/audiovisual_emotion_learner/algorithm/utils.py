db515fda502684a0c5e34cdbf68a6bd0874d0fce
katerynaCh <eichum@mail.ru>
2022-04-26 21:22:20 +0300

    added av emotion learner

---
+++ src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/utils.py
@@ -0,0 +1,103 @@
+# Copyright 2020-2022 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+'''
+This code is modified from https://github.com/okankop/Efficient-3DCNNs/
+'''
+
+import csv
+import torch
+import shutil
+import numpy as np
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+class Logger(object):
+
+    def __init__(self, path, header):
+        self.log_file = open(path, 'w')
+        self.logger = csv.writer(self.log_file, delimiter='\t')
+
+        self.logger.writerow(header)
+        self.header = header
+
+    def __del(self):
+        self.log_file.close()
+
+    def log(self, values):
+        write_values = []
+        for col in self.header:
+            assert col in values
+            write_values.append(values[col])
+
+        self.logger.writerow(write_values)
+        self.log_file.flush()
+
+
+def load_value_file(file_path):
+    with open(file_path, 'r') as input_file:
+        value = float(input_file.read().rstrip('\n\r'))
+
+    return value
+
+
+def calculate_accuracy(output, target, topk=(1,), binary=False):
+    """Computes the precision@k for the specified values of k"""
+
+    maxk = max(topk)
+    if maxk > output.size(1):
+        maxk = output.size(1)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        if k > maxk:
+            k = maxk
+        correct_k = correct[:k].reshape(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+def save_checkpoint(state, is_best, save_path, save_name):
+    torch.save(state, '%s/%s_last' % (save_path, save_name)+'.pth')
+    if is_best:
+        shutil.copyfile('%s/%s_last' % (save_path, save_name)+'.pth', '%s/%s_best' % (save_path, save_name)+'.pth')
+
+
+def adjust_learning_rate(optimizer, epoch, learning_rate, lr_steps):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr_new = learning_rate * (0.1 ** (sum(epoch >= np.array(lr_steps))))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr_new
