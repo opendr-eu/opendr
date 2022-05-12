3ec01fb72cab461b0089468af9af708c89fa06bc
Negar <negar.heidari@eng.au.dk>
2022-05-12 17:46:19 +0200

    ensemble based CNN learner & demo added

--- tests/sources/tools/perception/facial_expression_recognition/ensemble_based_cnn/test_esr.py
+++ tests/sources/tools/perception/facial_expression_recognition/ensemble_based_cnn/test_esr.py
@@ -54,7 +54,7 @@ class TestEnsembleBasedCNNLearner(unittest.TestCase):
 
         cls.learner = EnsembleCNNLearner(device="cpu", temp_path=cls.temp_dir,
                                          batch_size=2, max_training_epoch=1, ensemble_size=1,
-                                         name_experiment='esr_test', base_path_experiment=PATH_,
+                                         name_experiment='esr_9', base_path_experiment=PATH_,
                                          lr=1e-1, categorical_train=True, dimensional_finetune=True,
                                          base_path_to_dataset=DATA_PATH, max_tuning_epoch=1)
 
