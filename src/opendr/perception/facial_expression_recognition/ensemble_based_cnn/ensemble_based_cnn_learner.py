4a7bbdcb94cca62167d4729c045fdf01a9c40f72
Negar <negar.heidari@eng.au.dk>
2022-05-16 21:36:14 +0200

    ensemble based CNN learner & demo added

--- src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/ensemble_based_cnn_learner.py
+++ src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/ensemble_based_cnn_learner.py
@@ -104,7 +104,7 @@ class EnsembleCNNLearner(Learner):
             model_metadata["model_paths"] = [base_path_to_save_model]
             model_metadata["optimized"] = True
             model_metadata["format"] = "onnx"
-            shutil.copy2(path.join(self.temp_path, self.name_experiment, ".onnx"),
+            shutil.copy2(path.join(self.temp_path, self.name_experiment + ".onnx"),
                          model_metadata["model_paths"][0])
             print("ONNX model has been "
                   "saved at: {}".format(base_path_to_save_model))
@@ -620,15 +620,14 @@ class EnsembleCNNLearner(Learner):
         if self.ort_session is not None:
             raise UserWarning("Model is already optimized in ONNX.")
         try:
-            self.__convert_to_onnx(path.join(self.temp_path, self.name_experiment,
-                                             ".onnx"), do_constant_folding)
+            self.__convert_to_onnx(path.join(self.temp_path, self.name_experiment + ".onnx"), do_constant_folding)
         except FileNotFoundError:
             # Create temp directory
             os.makedirs(path.join(self.temp_path, self.name_experiment), exist_ok=True)
-            self.__convert_to_onnx(path.join(self.temp_path, self.name_experiment, ".onnx"),
+            self.__convert_to_onnx(path.join(self.temp_path, self.name_experiment + ".onnx"),
                                    do_constant_folding, verbose=False)
 
-        self.__load_from_onnx(path.join(self.temp_path, self.name_experiment, ".onnx"))
+        self.__load_from_onnx(path.join(self.temp_path, self.name_experiment + ".onnx"))
 
     def __convert_to_onnx(self, output_name, do_constant_folding=False, verbose=False):
         """
