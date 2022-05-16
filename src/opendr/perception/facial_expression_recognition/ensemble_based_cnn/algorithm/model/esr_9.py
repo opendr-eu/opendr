213c71ff3a316eca1d1f9cee15df3d1b6bacfbd6
Negar <negar.heidari@eng.au.dk>
2022-05-16 12:23:54 +0200

    ensemble based CNN learner & demo added

--- src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
+++ src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
@@ -242,10 +242,10 @@ class ESR(nn.Module):
 
         # Add to the lists of predictions outputs from each convolutional branch in the ensemble
         for branch in self.convolutional_branches:
-            '''if self.optimize_:
+            if self.optimize_:
                 x_shared_representations = x_shared_representations.detach()
             output_emotion, output_affect = branch(x_shared_representations)
             emotions.append(output_emotion)
-            affect_values.append(output_affect)'''
+            affect_values.append(output_affect)
 
-        return emotions, affect_values  # 9 lists for each output. each list of emotions ha 8 values and each list of affect values has 2 values! 9*8 , 9*2
+        return emotions#, affect_values  # 9 lists for each output. each list of emotions ha 8 values and each list of affect values has 2 values! 9*8 , 9*2
