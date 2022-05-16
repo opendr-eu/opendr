d8083a24d79e6526dd7854e97ceb5a4be98c943a
Negar <negar.heidari@eng.au.dk>
2022-05-16 18:53:58 +0200

    ensemble based CNN learner & demo added

--- src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
+++ src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
@@ -250,7 +250,7 @@ class ESR(nn.Module):
             x_shared_representations = x_shared_representations.detach().numpy()
             # x_shared_representations = nn.Parameter(x_shared_representations.detach())
             with torch.no_grad():
-                x_shared_representations = Variable(x_shared_representations.type(torch.FloatTensor),
+                x_shared_representations = Variable(torch.from_numpy(x_shared_representations).type(torch.FloatTensor),
                                                                                         requires_grad=False)
                 output_emotion, output_affect = branch(x_shared_representations)
                 emotions.append(output_emotion)
