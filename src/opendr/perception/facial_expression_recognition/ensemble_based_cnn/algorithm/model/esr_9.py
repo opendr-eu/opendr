51558e82a9f5f3bda7eaabf75df160d534541c95
Negar <negar.heidari@eng.au.dk>
2022-05-16 18:56:39 +0200

    ensemble based CNN learner & demo added

--- src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
+++ src/opendr/perception/facial_expression_recognition/ensemble_based_cnn/algorithm/model/esr_9.py
@@ -237,21 +237,19 @@ class ESR(nn.Module):
         # List of emotions and affect values from the ensemble
         emotions = []
         affect_values = []
-
-        # Get shared representations
-        x_shared_representations = self.base(x)
-
-        # Add to the lists of predictions outputs from each convolutional branch in the ensemble
-        for branch in self.convolutional_branches:
-            #if self.optimize_:
-                # x_shared_representations = x_shared_representations.detach()
-                #x_shared_representations = Variable(x_shared_representations.type(torch.FloatTensor),
-                #                                    requires_grad=False)
-            x_shared_representations = x_shared_representations.detach().numpy()
-            # x_shared_representations = nn.Parameter(x_shared_representations.detach())
-            with torch.no_grad():
-                x_shared_representations = Variable(torch.from_numpy(x_shared_representations).type(torch.FloatTensor),
-                                                                                        requires_grad=False)
+        with torch.no_grad():
+            # Get shared representations
+            x_shared_representations = self.base(x)
+            # Add to the lists of predictions outputs from each convolutional branch in the ensemble
+            for branch in self.convolutional_branches:
+                #if self.optimize_:
+                    # x_shared_representations = x_shared_representations.detach()
+                    #x_shared_representations = Variable(x_shared_representations.type(torch.FloatTensor),
+                    #                                    requires_grad=False)
+                #x_shared_representations = x_shared_representations.detach().numpy()
+                # x_shared_representations = nn.Parameter(x_shared_representations.detach())
+                #x_shared_representations = Variable(torch.from_numpy(x_shared_representations).type(torch.FloatTensor),
+                                                                                        #requires_grad=False)
                 output_emotion, output_affect = branch(x_shared_representations)
                 emotions.append(output_emotion)
                 affect_values.append(output_affect)
