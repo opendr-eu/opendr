7e20a74ab8f07748e27f14c74c166bc031a63e11
Kostas Tsampazis <tsampaka@csd.auth.gr>
2022-06-10 15:21:42 +0300

    Added object detection 2d ssd node according to ros1 node

--- projects/opendr_ws_2/src/perception/setup.py
+++ projects/opendr_ws_2/src/perception/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'pose_estimation = perception.pose_estimation_node:main',
+            'object_detection_2d_ssd = perception.object_detection_2d_ssd_node:main',
             'tester = perception.subscriber_tester:main',
         ],
     },
