60bbb58b862a573f35f11f0671f02ea56dbd7c35
tsampazk <tsampaka@csd.auth.gr>
2022-08-18 13:37:27 +0300

    Updated, cleaned up and added argparse to ROS1 object detection 2d yolov3 node

--- projects/opendr_ws/src/perception/scripts/object_detection_2d_yolov3.py
+++ projects/opendr_ws/src/perception/scripts/object_detection_2d_yolov3.py
@@ -13,111 +13,126 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import rospy
+import argparse
 import mxnet as mx
-import numpy as np
+
+import rospy
 from vision_msgs.msg import Detection2DArray
 from sensor_msgs.msg import Image as ROS_Image
 from opendr_bridge import ROSBridge
+
 from opendr.engine.data import Image
 from opendr.perception.object_detection_2d import YOLOv3DetectorLearner
 from opendr.perception.object_detection_2d import draw_bounding_boxes
 
 
 class ObjectDetectionYOLONode:
-    def __init__(self, input_image_topic="/usb_cam/image_raw", output_image_topic="/opendr/image_boxes_annotated",
-                 detections_topic="/opendr/objects", device="cuda", backbone="darknet53"):
+    def __init__(self, input_rgb_image_topic="/usb_cam/image_raw",
+                 output_rgb_image_topic="/opendr/image_objects_annotated", detections_topic="/opendr/objects",
+                 device="cuda", backbone="darknet53"):
         """
-        Creates a ROS Node for face detection
-        :param input_image_topic: Topic from which we are reading the input image
-        :type input_image_topic: str
-        :param output_image_topic: Topic to which we are publishing the annotated image (if None, we are not publishing
-        annotated image)
-        :type output_image_topic: str
-        :param detections_topic: Topic to which we are publishing the annotations (if None, we are not publishing
-        annotated pose annotations)
+        Creates a ROS Node for object detection with YOLOV3.
+        :param input_rgb_image_topic: Topic from which we are reading the input image
+        :type input_rgb_image_topic: str
+        :param output_rgb_image_topic: Topic to which we are publishing the annotated image (if None, no annotated
+        image is published)
+        :type output_rgb_image_topic: str
+        :param detections_topic: Topic to which we are publishing the annotations (if None, no object detection message
+        is published)
         :type detections_topic:  str
         :param device: device on which we are running inference ('cpu' or 'cuda')
         :type device: str
         :param backbone: backbone network
         :type backbone: str
         """
+        self.input_rgb_image_topic = input_rgb_image_topic
 
-        # Initialize the face detector
-        self.object_detector = YOLOv3DetectorLearner(backbone=backbone, device=device)
-        self.object_detector.download(path=".", verbose=True)
-        self.object_detector.load("yolo_default")
-        self.class_names = self.object_detector.classes
-
-        # Initialize OpenDR ROSBridge object
-        self.bridge = ROSBridge()
-
-        # setup communications
-        if output_image_topic is not None:
-            self.image_publisher = rospy.Publisher(output_image_topic, ROS_Image, queue_size=10)
+        if output_rgb_image_topic is not None:
+            self.image_publisher = rospy.Publisher(output_rgb_image_topic, ROS_Image, queue_size=1)
         else:
             self.image_publisher = None
 
         if detections_topic is not None:
-            self.bbox_publisher = rospy.Publisher(detections_topic, Detection2DArray, queue_size=10)
+            self.object_publisher = rospy.Publisher(detections_topic, Detection2DArray, queue_size=1)
         else:
-            self.bbox_publisher = None
+            self.object_publisher = None
 
-        rospy.Subscriber(input_image_topic, ROS_Image, self.callback)
+        self.bridge = ROSBridge()
+
+        # Initialize the object detector
+        self.object_detector = YOLOv3DetectorLearner(backbone=backbone, device=device)
+        self.object_detector.download(path=".", verbose=True)
+        self.object_detector.load("yolo_default")
+
+    def listen(self):
+        """
+        Start the node and begin processing input data.
+        """
+        rospy.init_node('object_detection_yolov3_node', anonymous=True)
+        rospy.Subscriber(self.input_rgb_image_topic, ROS_Image, self.callback, queue_size=1, buff_size=10000000)
+        rospy.loginfo("Object detection YOLOV3 node started.")
+        rospy.spin()
 
     def callback(self, data):
         """
-        Callback that process the input data and publishes to the corresponding topics
+        Callback that process the input data and publishes to the corresponding topics.
         :param data: input message
         :type data: sensor_msgs.msg.Image
         """
-
         # Convert sensor_msgs.msg.Image into OpenDR Image
         image = self.bridge.from_ros_image(data, encoding='bgr8')
-        rospy.loginfo("image info: {}".format(image.numpy().shape))
 
-        # Run pose estimation
+        # Run object detection
         boxes = self.object_detector.infer(image, threshold=0.1, keep_size=False)
 
         # Get an OpenCV image back
-        image = np.float32(image.opencv())
+        image = image.opencv()
 
-        # Convert detected boxes to ROS type and publish
-        ros_boxes = self.bridge.to_ros_boxes(boxes)
-        if self.bbox_publisher is not None:
-            self.bbox_publisher.publish(ros_boxes)
-            rospy.loginfo("Published face boxes")
+        # Publish detections in ROS message
+        ros_boxes = self.bridge.to_ros_bounding_box_list(boxes)  # Convert to ROS bounding_box_list
+        if self.object_publisher is not None:
+            self.object_publisher.publish(ros_boxes)
 
-        # Annotate image and publish result
-        # NOTE: converting back to OpenDR BoundingBoxList is unnecessary here,
-        # only used to test the corresponding bridge methods
-        odr_boxes = self.bridge.from_ros_boxes(ros_boxes)
-        image = draw_bounding_boxes(image, odr_boxes, class_names=self.class_names)
         if self.image_publisher is not None:
-            message = self.bridge.to_ros_image(Image(image), encoding='bgr8')
-            self.image_publisher.publish(message)
-            rospy.loginfo("Published annotated image")
+            # Annotate image with object detection boxes
+            image = draw_bounding_boxes(image, boxes, class_names=self.object_detector.classes)
+            # Convert the annotated OpenDR image to ROS2 image message using bridge and publish it
+            self.image_publisher.publish(self.bridge.to_ros_image(Image(image), encoding='bgr8'))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_rgb_image_topic", help="Topic name for input rgb image",
+                        type=str, default="/usb_cam/image_raw")
+    parser.add_argument("-o", "--output_rgb_image_topic", help="Topic name for output annotated rgb image",
+                        type=str, default="/opendr/image_objects_annotated")
+    parser.add_argument("-d", "--detections_topic", help="Topic name for detection messages",
+                        type=str, default="/opendr/objects")
+    parser.add_argument("--device", help="Device to use, either \"cpu\" or \"cuda\", defaults to \"cuda\"",
+                        type=str, default="cuda", choices=["cuda", "cpu"])
+    parser.add_argument("--backbone", help="Backbone network, defaults to \"darknet53\"",
+                        type=str, default="darknet53", choices=["darknet53"])
+    args = parser.parse_args()
+
+    try:
+        if args.device == "cuda" and mx.context.num_gpus() > 0:
+            device = "cuda"
+        elif args.device == "cuda":
+            print("GPU not found. Using CPU instead.")
+            device = "cpu"
+        else:
+            print("Using CPU.")
+            device = "cpu"
+    except:
+        print("Using CPU.")
+        device = "cpu"
+
+    object_detection_yolov3_node = ObjectDetectionYOLONode(device=device, backbone=args.backbone,
+                                                           input_rgb_image_topic=args.input_rgb_image_topic,
+                                                           output_rgb_image_topic=args.output_rgb_image_topic,
+                                                           detections_topic=args.detections_topic)
+    object_detection_yolov3_node.listen()
 
 
 if __name__ == '__main__':
-    # Automatically run on GPU/CPU
-    try:
-        if mx.context.num_gpus() > 0:
-            print("GPU found.")
-            device = 'cuda'
-        else:
-            print("GPU not found. Using CPU instead.")
-            device = 'cpu'
-    except:
-        device = 'cpu'
-
-    # initialize ROS node
-    rospy.init_node('opendr_object_detection', anonymous=True)
-    rospy.loginfo("Object detection node started!")
-
-    input_image_topic = rospy.get_param("~input_image_topic", "/videofile/image_raw")
-
-    # created node object
-    object_detection_node = ObjectDetectionYOLONode(device=device, input_image_topic=input_image_topic)
-    # begin ROS communications
-    rospy.spin()
+    main()
