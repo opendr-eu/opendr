7424a97bb1a676a1cc92e124c374a51c7d79e000
ekakalet <63847549+ekakalet@users.noreply.github.com>
2022-08-30 14:54:21 +0300

    Update test_flake8.py

--- projects/opendr_ws_2/src/data_generation/test/test_flake8.py
+++ projects/opendr_ws_2/src/data_generation/test/test_flake8.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Open Source Robotics Foundation, Inc.
+# Copyright 2020-2022 OpenDR European Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
