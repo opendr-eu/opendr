3c2fb00e71e9d1cb1a1dae2749f8e01dfe2bb069
Negar <negar.heidari@eng.au.dk>
2022-10-19 13:19:11 +0200

    pep8 fixed

--- projects/opendr_ws/src/perception/scripts/landmark_based_facial_expression_recognition.py
+++ projects/opendr_ws/src/perception/scripts/landmark_based_facial_expression_recognition.py
@@ -165,10 +165,10 @@ if __name__ == '__main__':
         device = "cpu"
 
     landmark_expression_estimation_node = \
-        LandmarkFacialExpressionRecognitionNode(input_image_topic=args.input_image_topic,
-                                                output_category_topic=args.output_category_topic,
-                                                output_category_description_topic=
-                                                args.output_category_description_topic,
-                                                device=device, model=args.model,
-                                                shape_predictor=args.shape_predictor)
+        LandmarkFacialExpressionRecognitionNode(
+            input_image_topic=args.input_image_topic,
+            output_category_topic=args.output_category_topic,
+            output_category_description_topic=args.output_category_description_topic,
+            device=device, model=args.model,
+            shape_predictor=args.shape_predictor)
     landmark_expression_estimation_node.listen()
