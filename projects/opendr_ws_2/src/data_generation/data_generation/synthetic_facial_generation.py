cca077a838daf844fcedd5eb000f646e9cf3f489
ekakalet <63847549+ekakalet@users.noreply.github.com>
2022-10-25 11:07:36 +0300

    Update synthetic_facial_generation.py

--- projects/opendr_ws_2/src/data_generation/data_generation/synthetic_facial_generation.py
+++ projects/opendr_ws_2/src/data_generation/data_generation/synthetic_facial_generation.py
@@ -31,7 +31,7 @@ from opendr.projects.python.simulation.synthetic_multi_view_facial_image_generat
     import MultiviewDataGeneration
 
 
-class SyntheticDataGeneration(Node):
+class SyntheticDataGenerator(Node):
 
     def __init__(self, args, input_rgb_image_topic="/image_raw",
                  output_rgb_image_topic="/opendr/synthetic_facial_images"):
@@ -156,7 +156,7 @@ def main(args=None):
     args = parser.parse_args()
 
 
-    synthetic_data_generation_node = SyntheticDataGeneration(args=args, 
+    synthetic_data_generation_node = SyntheticDataGenerator(args=args, 
                                                              input_rgb_image_topic=args.input_rgb_image_topic, 
                                                              output_rgb_image_topic=args.output_rgb_image_topic)
 
