da744e3a7cd1eb04ba521f96045b21bd4fd9095a
ekakalet <63847549+ekakalet@users.noreply.github.com>
2022-11-03 17:08:49 +0200

    Update data.py

--- src/opendr/engine/data.py
+++ src/opendr/engine/data.py
@@ -15,7 +15,7 @@
 from pathlib import Path
 import cv2
 from abc import ABC, abstractmethod
-from opendr.src.opendr.engine.target import BoundingBoxList
+from opendr.engine.target import BoundingBoxList
 import numpy as np
 import torch
 from typing import Union
