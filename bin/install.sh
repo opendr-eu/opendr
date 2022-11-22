8fb2c1d3dc2faff7cea30e7062dd5a27cb5ca5bf
ad-daniel <daniel.dias@epfl.ch>
2022-11-22 17:14:41 +0100

    Improvements

--- bin/install.sh
+++ bin/install.sh
@@ -29,7 +29,7 @@ esac
 python3 -m venv venv
 source venv/bin/activate
 python3 -m pip install -U pip
-pip3 install setuptools configparser
+python3 -m pip install setuptools configparser
 
 # Add repositories for ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' \
@@ -40,18 +40,18 @@ make install_compilation_dependencies
 make install_runtime_dependencies
 
 # Install additional ROS packages
-sudo apt-get install ros-noetic-vision-msgs ros-noetic-audio-common-msgs 
+sudo apt-get install ros-$ROS_DISTRO-vision-msgs ros-$ROS_DISTRO-audio-common-msgs
 
 # If working on GPU install GPU dependencies as needed
 if [[ "${OPENDR_DEVICE}" == "gpu" ]]; then
-  pip3 uninstall -y mxnet
-  pip3 uninstall -y torch
+  python3 -m pip uninstall -y mxnet
+  python3 -m pip uninstall -y torch
   echo "[INFO] Replacing  mxnet-cu112==1.8.0post0 to enable CUDA acceleration."
-  pip3 install mxnet-cu112==1.8.0post0
+  python3 -m pip install mxnet-cu112==1.8.0post0
   echo "[INFO] Replacing torch==1.9.0+cu111 to enable CUDA acceleration."
-  pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+  python3 -m pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
   echo "[INFO] Reinstalling detectronv2."
-  pip3 install 'git+https://github.com/facebookresearch/detectron2.git@5aeb252b194b93dc2879b4ac34bc51a31b5aee13'
+  python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git@5aeb252b194b93dc2879b4ac34bc51a31b5aee13'
 fi
 
 make libopendr
