bf61b2ca700ec172be634ba246ace7057fe6625f
ManosMpampis <93824600+ManosMpampis@users.noreply.github.com>
2022-11-25 12:35:58 +0200

    Update eval_demo.py

--- projects/python/perception/object_detection_2d/nanodet/eval_demo.py
+++ projects/python/perception/object_detection_2d/nanodet/eval_demo.py
@@ -20,14 +20,14 @@ from opendr.engine.datasets import ExternalDataset
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", help="Dataset to evaluate on", type=str, default="coco", choices=["voc", "coco"])
     parser.add_argument("--data-root", help="Dataset root folder", type=str)
     parser.add_argument("--model", help="Model that config file will be used", type=str, default="m")
     parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
 
     args = parser.parse_args()
-    data_root = "/home/manos/data/coco2017"
 
-    val_dataset = ExternalDataset(data_root, 'coco')
+    val_dataset = ExternalDataset(args.data_root, args.dataset)
     nanodet = NanodetLearner(model_to_use=args.model, device=args.device)
 
     nanodet.download("./predefined_examples", mode="pretrained", verbose=False)
