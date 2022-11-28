88ba9eaa051a5a2b21813a2522a290650cc749c3
halil93ibrahim <halil@ece.au.dk>
2022-11-28 15:51:20 +0100

    ROS2 node for e2e planner

--- projects/opendr_ws_2/src/opendr_planning/opendr_planning/end_to_end_planner_node.py
+++ projects/opendr_ws_2/src/opendr_planning/opendr_planning/end_to_end_planner_node.py
@@ -17,7 +17,7 @@ import rclpy
 from rclpy.node import Node
 import numpy as np
 import webots_ros.srv
-# import ros_numpy
+from cv_bridge import CvBridge
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu, Image
 from geometry_msgs.msg import PoseStamped, PointStamped
@@ -38,51 +38,27 @@ class EndToEndPlannerNode(Node):
         self.target_pose = PoseStamped()
         self.current_pose.header.frame_id = "map"
         self.target_pose.header.frame_id = "map"
-        # self.r = rclpy.Rate(25)
-        # self.create_subscription("/model_name", String, self.model_name_callback)
-        # counter = 0
-        # while self.model_name == "":
-        #     self.r.sleep()
-        #     counter += 1
-        #     if counter > 25:
-        #         break
-        # if self.model_name == "":
-        #     rospy.loginfo("Webots model is not started!")
-        #     return
+        self.bridge = CvBridge()
         self.input_depth_image_topic = "/quad_plus_sitl/range_finder"
         self.position_topic = "/quad_plus_sitl/gps1"
         self.orientation_topic = "/imu"
-        # self.ros_srv_range_sensor_enable = rospy.ServiceProxy(
-        #     "/range_finder/enable", webots_ros.srv.set_int)
-        # self.ros_srv_gps1_sensor_enable = rospy.ServiceProxy(
-        #     "/gps1/enable", webots_ros.srv.set_int)
-        # self.ros_srv_inertial_unit_enable = rospy.ServiceProxy(
-        #     "/inertial_unit/enable", webots_ros.srv.set_int)
         self.end_to_end_planner = EndToEndPlanningRLLearner(env=None)
 
-        # try:
-        #     self.ros_srv_gps1_sensor_enable(1)
-        #     self.ros_srv_inertial_unit_enable(1)
-        #     self.ros_srv_range_sensor_enable(1)
-        # except rospy.ServiceException as exc:
-        #     print("Service did not process request: " + str(exc))
-        self.ros2_pub_current_pose = self.create_publisher(PoseStamped, 'current_uav_pose', queue_size=10)
-        self.ros2_pub_target_pose = self.create_publisher(PoseStamped, 'target_uav_pose', queue_size=10)
-        self.create_subscription(Imu, self.orientation_topic, self.imu_callback)
-        self.create_subscription(PointStamped, self.position_topic, self.gps_callback)
-        self.create_subscription(Image, self.input_depth_image_topic, self.range_callback, queue_size=1)
+        self.ros2_pub_current_pose = self.create_publisher(PoseStamped, 'current_uav_pose', 10)
+        self.ros2_pub_target_pose = self.create_publisher(PoseStamped, 'target_uav_pose', 10)
+        self.create_subscription(Imu, self.orientation_topic, self.imu_callback, 1)
+        self.create_subscription(PointStamped, self.position_topic, self.gps_callback, 1)
+        self.create_subscription(Image, self.input_depth_image_topic, self.range_callback, 1)
         self.get_logger().info("End-to-end planning node initialized.")
 
     def range_callback(self, data):
-        print(data)
-        # image_arr = ros_numpy.numpify(data)
-        # self.range_image = ((np.clip(image_arr.reshape((64, 64, 1)), 0, 15) / 15.) * 255).astype(np.uint8)
-        # observation = {'depth_cam': np.copy(self.range_image), 'moving_target': np.array([5, 0, 0])}
-        # action = self.end_to_end_planner.infer(observation, deterministic=True)[0]
-        # self.publish_poses(action)
+        image_arr = self.bridge.imgmsg_to_cv2(data)
+        self.range_image = ((np.clip(image_arr.reshape((64, 64, 1)), 0, 15) / 15.) * 255).astype(np.uint8)
+        observation = {'depth_cam': np.copy(self.range_image), 'moving_target': np.array([5, 0, 0])}
+        action = self.end_to_end_planner.infer(observation, deterministic=True)[0]
+        self.publish_poses(action)
 
     def gps_callback(self, data):
-        self.current_pose.header.stamp = self.get_clock().now()
         self.current_pose.pose.position.x = -data.point.x
         self.current_pose.pose.position.y = -data.point.y
         self.current_pose.pose.position.z = data.point.z
@@ -99,18 +75,17 @@ class EndToEndPlannerNode(Node):
             self.model_name = data.data
 
     def publish_poses(self, action):
-        self.ros_pub_current_pose.publish(self.current_pose)
+        self.ros2_pub_current_pose.publish(self.current_pose)
         forward_step = np.cos(action[0] * 22.5 / 180 * np.pi)
         side_step = np.sin(action[0] * 22.5 / 180 * np.pi)
         yaw_step = action[1] * 22.5 / 180 * np.pi
-        self.target_pose.header.stamp = self.get_clock().now()
         self.target_pose.pose.position.x = self.current_pose.pose.position.x + forward_step * np.cos(
             self.current_yaw) - side_step * np.sin(self.current_yaw)
         self.target_pose.pose.position.y = self.current_pose.pose.position.y + forward_step * np.sin(
             self.current_yaw) + side_step * np.cos(self.current_yaw)
         self.target_pose.pose.position.z = self.current_pose.pose.position.z
         self.target_pose.pose.orientation = euler_to_quaternion(0, 0, yaw=self.current_yaw+yaw_step)
-        self.ros_pub_target_pose.publish(self.target_pose)
+        self.ros2_pub_target_pose.publish(self.target_pose)
 
 
 def main(args=None):
