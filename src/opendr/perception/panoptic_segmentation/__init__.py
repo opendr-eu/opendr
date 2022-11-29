3b586a5a699b856ef0ba2074ed876e1d1609eec9
aselimc <canakcia@cs.uni-freiburg.de>
2022-11-29 14:21:07 +0100

    Deleting nuscenes

--- src/opendr/perception/panoptic_segmentation/__init__.py
+++ src/opendr/perception/panoptic_segmentation/__init__.py
@@ -1,7 +1,7 @@
 from opendr.perception.panoptic_segmentation.datasets import CityscapesDataset, KittiDataset,\
-    NuscenesDataset, SemanticKittiDataset
+    SemanticKittiDataset
 from opendr.perception.panoptic_segmentation.efficient_ps import EfficientPsLearner
 from opendr.perception.panoptic_segmentation.efficient_lps import EfficientLpsLearner
 
 __all__ = ['CityscapesDataset', 'KittiDataset', 'EfficientPsLearner',
-           'NuscenesDataset', 'SemanticKittiDataset', 'EfficientLpsLearner']
+           'SemanticKittiDataset', 'EfficientLpsLearner']
--- src/opendr/perception/panoptic_segmentation/datasets/__init__.py
+++ src/opendr/perception/panoptic_segmentation/datasets/__init__.py
@@ -1,6 +1,5 @@
 from opendr.perception.panoptic_segmentation.datasets.cityscapes import CityscapesDataset
 from opendr.perception.panoptic_segmentation.datasets.kitti import KittiDataset
-from opendr.perception.panoptic_segmentation.datasets.nuscenes import NuscenesDataset
 from opendr.perception.panoptic_segmentation.datasets.semantic_kitti import SemanticKittiDataset
 
-__all__ = ['CityscapesDataset', 'KittiDataset', 'NuscenesDataset', 'SemanticKittiDataset']
+__all__ = ['CityscapesDataset', 'KittiDataset', 'SemanticKittiDataset']
