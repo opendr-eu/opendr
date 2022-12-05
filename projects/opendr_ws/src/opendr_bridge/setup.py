1eb3b3d5b664bc8e01254e501e5b17b7be8e4a39
tsampazk <27914645+tsampazk@users.noreply.github.com>
2022-12-05 12:21:54 +0200

    Renamed dirs, etc. from opendr_ros_bridge to opendr_bridge to match the actual package name

--- projects/opendr_ws/src/opendr_bridge/setup.py
+++ projects/opendr_ws/src/opendr_bridge/setup.py
@@ -15,7 +15,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['opendr_bridge'],  # comment for review: should this change?
+    packages=['opendr_bridge'],
     package_dir={'': 'src'}
 )
 setup(**d)
