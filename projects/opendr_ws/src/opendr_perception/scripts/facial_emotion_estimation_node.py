b219b52de164c801b8566cbde368638350247ae6
tsampazk <27914645+tsampazk@users.noreply.github.com>
2022-12-19 14:13:52 +0200

    Moved and applied several fixes on facial emotion ros node

--- projects/opendr_ws/src/opendr_perception/scripts/facial_emotion_estimation_node.py
+++ projects/opendr_ws/src/opendr_perception/scripts/facial_emotion_estimation_node.py
@@ -35,11 +35,11 @@ from opendr.perception.facial_expression_recognition import ESR
 class FacialEmotionEstimationNode:
     def __init__(self,
                  input_rgb_image_topic="/usb_cam/image_raw",
-                 output_emotions_topic="/opendr/facial_emotion",
+                 output_emotions_topic="/opendr/facial_emotion_estimation",
                  output_emotions_description_topic="/opendr/facial_emotion_estimation_description",
                  device="cuda"):
         """
-        Creates a ROS Node for facial emotion estimation
+        Creates a ROS Node for facial emotion estimation.
         :param input_rgb_image_topic: Topic from which we are reading the input image
         :type input_rgb_image_topic: str
         :param output_emotions_topic: Topic to which we are publishing the facial emotion results
@@ -71,25 +71,22 @@ class FacialEmotionEstimationNode:
                                                              ensemble_size=9,
                                                              name_experiment='esr_9')
 
-        URL_PATH = OPENDR_SERVER_URL + "perception/ensemble_based_cnn"
-        model_saved_path = self.facial_emotion_estimator.download(self, path=None, mode="pretrained", url=URL_PATH)
+        model_saved_path = self.facial_emotion_estimator.download(path=None, mode="pretrained")
 
-        self.facial_emotion_estimator.load(self, ensemble_size=9, path_to_saved_network=model_saved_path,
-                                           file_name_base_network="Net-Base-Shared_Representations.pt",
-                                           file_name_conv_branch="Net-Branch_{}.pt", fix_backbone=True)
+        self.facial_emotion_estimator.load(ensemble_size=9, path_to_saved_network=model_saved_path)
 
     def listen(self):
         """
         Start the node and begin processing input data
         """
-        rospy.init_node('opendr_facial_emotion_estimation', anonymous=True)
-        rospy.Subscriber(self.input_image_topic, ROS_Image, self.callback)
-        rospy.loginfo("Facial emotion estimation node started!")
+        rospy.init_node('opendr_facial_emotion_estimation_node', anonymous=True)
+        rospy.Subscriber(self.input_rgb_image_topic, ROS_Image, self.callback, queue_size=1, buff_size=10000000)
+        rospy.loginfo("Facial emotion estimation node started.")
         rospy.spin()
 
     def callback(self, data):
         """
-        Callback that process the input data and publishes to the corresponding topics
+        Callback that processes the input data and publishes to the corresponding topics.
         :param data: input message
         :type data: sensor_msgs.msg.Image
         """
@@ -108,7 +105,7 @@ class FacialEmotionEstimationNode:
             return
         else:
             face = image[face_coordinates[0][1]:face_coordinates[1][1],
-                         face_coordinates[0][0]:face_coordinates[1][0], :]
+                   face_coordinates[0][0]:face_coordinates[1][0], :]
 
             # Pre_process detected face
             input_face = _pre_process_input_image(face)
@@ -164,10 +161,8 @@ def _pre_process_input_image(image):
 
 
 if __name__ == '__main__':
-    # Select the device for running the
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_image_topic', type=str, help='Topic name for input rgb image',
+    parser.add_argument('-i', '--input_rgb_image_topic', type=str, help='Topic name for input rgb image',
                         default='/usb_cam/image_raw')
     parser.add_argument("-o", "--output_emotions_topic", help="Topic name for output emotion",
                         type=lambda value: value if value.lower() != "none" else None,
