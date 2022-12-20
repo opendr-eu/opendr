01deac14f167ee044d2f5016661c3735ce45b486
tsampazk <27914645+tsampazk@users.noreply.github.com>
2022-12-20 15:25:02 +0200

    Fixes from ros1

--- projects/opendr_ws_2/src/opendr_perception/opendr_perception/object_tracking_3d_ab3dmot_node.py
+++ projects/opendr_ws_2/src/opendr_perception/opendr_perception/object_tracking_3d_ab3dmot_node.py
@@ -16,7 +16,6 @@
 import torch
 import argparse
 import os
-from opendr.engine.learners import Learner
 import rclpy
 from rclpy.node import Node
 from vision_msgs.msg import Detection3DArray
@@ -30,7 +29,7 @@ from opendr.perception.object_detection_3d import VoxelObjectDetection3DLearner
 class ObjectTracking3DAb3dmotNode(Node):
     def __init__(
         self,
-        detector: Learner,
+        detector=None,
         input_point_cloud_topic="/opendr/dataset_point_cloud",
         output_detection3d_topic="/opendr/detection3d",
         output_tracking3d_id_topic="/opendr/tracking3d_id",
@@ -49,7 +48,7 @@ class ObjectTracking3DAb3dmotNode(Node):
         :param device: device on which we are running inference ('cpu' or 'cuda')
         :type device: str
         """
-        super().__init__('object_tracking_3d_ab3dmot_node')
+        super().__init__('opendr_object_tracking_3d_ab3dmot_node')
 
         self.detector = detector
         self.learner = ObjectTracking3DAb3dmotLearner(
@@ -71,11 +70,11 @@ class ObjectTracking3DAb3dmotNode(Node):
 
         self.create_subscription(ROS_PointCloud, input_point_cloud_topic, self.callback, 1)
 
-        self.get_logger().info("Ready to listen")
+        self.get_logger().info("Object Tracking 3D Ab3dmot Node initialized.")
 
     def callback(self, data):
         """
-        Callback that process the input data and publishes to the corresponding topics
+        Callback that processes the input data and publishes to the corresponding topics.
         :param data: input message
         :type data: sensor_msgs.msg.Image
         """
@@ -83,7 +82,6 @@ class ObjectTracking3DAb3dmotNode(Node):
         # Convert sensor_msgs.msg.Image into OpenDR Image
         point_cloud = self.bridge.from_ros_point_cloud(data)
         detection_boxes = self.detector.infer(point_cloud)
-        tracking_boxes = self.learner.infer(detection_boxes)
 
         # Convert detected boxes to ROS type and publish
         if self.detection_publisher is not None:
@@ -92,6 +90,7 @@ class ObjectTracking3DAb3dmotNode(Node):
             self.get_logger().info("Published " + str(len(detection_boxes)) + " detection boxes")
 
         if self.tracking_id_publisher is not None:
+            tracking_boxes = self.learner.infer(detection_boxes)
             ids = [tracking_box.id for tracking_box in tracking_boxes]
             ros_ids = Int32MultiArray()
             ros_ids.data = ids
@@ -99,42 +98,41 @@ class ObjectTracking3DAb3dmotNode(Node):
             self.get_logger().info("Published " + str(len(ids)) + " tracking ids")
 
 
-def main(
-    args=None,
-):
+def main(args=None):
     rclpy.init(args=args)
+
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_point_cloud_topic",
+                        help="Point Cloud topic provided by either a point_cloud_dataset_node or any other 3D Point Cloud Node",
+                        type=str, default="/opendr/dataset_point_cloud")
+    parser.add_argument("-d", "--detections_topic",
+                        help="Output detections topic",
+                        type=lambda value: value if value.lower() != "none" else None, default="/opendr/objects3d")
+    parser.add_argument("-t", "--tracking3d_id_topic",
+                        help="Output tracking ids topic with the same element count as in output_detection_topic",
+                        type=lambda value: value if value.lower() != "none" else None, default="/opendr/objects_tracking_id")
+    parser.add_argument("--device", help="Device to use, either \"cpu\" or \"cuda\", defaults to \"cuda\"",
+                        type=str, default="cuda", choices=["cuda", "cpu"])
     parser.add_argument("-dn", "--detector_model_name", help="Name of the trained model",
-                        type=str, default="tanet_car_xyres_16")
+                        type=str, default="tanet_car_xyres_16", choices=["tanet_car_xyres_16"])
     parser.add_argument(
         "-dc", "--detector_model_config_path", help="Path to a model .proto config",
         type=str, default=os.path.join(
-            "..", "..", "src", "opendr", "perception", "object_detection_3d",
+            "$OPENDR_HOME", "src", "opendr", "perception", "object_detection_3d",
             "voxel_object_detection_3d", "second_detector", "configs", "tanet",
             "car", "xyres_16.proto"
         )
     )
-    parser.add_argument("-t", "--temp_dir", help="Path to a temp dir with models",
+    parser.add_argument("-td", "--temp_dir", help="Path to a temporary directory with models",
                         type=str, default="temp")
-    parser.add_argument("-i", "--input_point_cloud_topic",
-                        help="Point Cloud topic provided by either a point_cloud_dataset_node or any other 3D Point Cloud Node",
-                        type=str, default="/opendr/dataset_point_cloud")
-    parser.add_argument("-od", "--output_detection3d_topic",
-                        help="Output detections topic",
-                        type=str, default="/opendr/detection3d")
-    parser.add_argument("-ot", "--output_tracking3d_id_topic",
-                        help="Output tracking topic",
-                        type=str, default="/opendr/tracking3d_id")
-    parser.add_argument("--device", help="Device to use, either \"cpu\" or \"cuda\", defaults to \"cuda\"",
-                        type=str, default="cuda", choices=["cuda", "cpu"])
     args = parser.parse_args()
 
     input_point_cloud_topic = args.input_point_cloud_topic
     detector_model_name = args.detector_model_name
     temp_dir = args.temp_dir
     detector_model_config_path = args.detector_model_config_path
-    output_detection3d_topic = args.output_detection3d_topic
-    output_tracking3d_id_topic = args.output_tracking3d_id_topic
+    output_detection3d_topic = args.detections_topic
+    output_tracking3d_id_topic = args.tracking3d_id_topic
 
     try:
         if args.device == "cuda" and torch.cuda.is_available():
@@ -159,13 +157,12 @@ def main(
 
     detector.load(os.path.join(temp_dir, detector_model_name), verbose=True)
 
-    # created node object
     ab3dmot_node = ObjectTracking3DAb3dmotNode(
         detector=detector,
         device=device,
         input_point_cloud_topic=input_point_cloud_topic,
-        output_detection3d_topic=output_detection3d_topic if output_detection3d_topic != "None" else None,
-        output_tracking3d_id_topic=output_tracking3d_id_topic if output_tracking3d_id_topic != "None" else None,
+        output_detection3d_topic=output_detection3d_topic,
+        output_tracking3d_id_topic=output_tracking3d_id_topic,
     )
 
     rclpy.spin(ab3dmot_node)
