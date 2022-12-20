1cff960fca1b3656b3b18e847d4e53aa4dd33a32
aselimc <canakcia@cs.uni-freiburg.de>
2022-12-20 15:03:06 +0100

    feat(Continual-SLAM): Finished __getitem__ dataset iterator
    Currently, still work in progress. But I have finished the simple data iteration, which returns the OpenDR type Image, Distance and Speed

--- src/opendr/perception/continual_slam/datasets/kitti.py
+++ src/opendr/perception/continual_slam/datasets/kitti.py
@@ -1,9 +1,11 @@
 import numpy as np
+import os
+import re
 from datetime import datetime
 
 from os import PathLike
 from pathlib import Path
-from typing import Union, List
+from typing import Tuple, Union, Any, List
 
 from opendr.engine.data import Image
 from opendr.engine.datasets import ExternalDataset, DatasetIterator
@@ -15,7 +17,7 @@ class KittiDataset(ExternalDataset, DatasetIterator):
     def __init__(self, path: Union[str, Path, PathLike]):
         super().__init__(path, dataset_type='kitti')
 
-        self._path = Path(path/ 'sequences')
+        self._path = Path(os.path.join(path, 'sequence'))
     
         # ========================================================
         self.frame_ids = [0, -1, 1]
@@ -23,7 +25,10 @@ class KittiDataset(ExternalDataset, DatasetIterator):
         self.height = 192
         self.width = 640
         # self.sequences = ['00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10']
-        self.sequences = ['00'] # for debugging
+        self.sequences = os.listdir(self._path)
+
+        if self.sequences is None:
+            raise FileNotFoundError(f'No sequences found in {self._path}')
         # ========================================================
 
         self._images = {}
@@ -32,7 +37,7 @@ class KittiDataset(ExternalDataset, DatasetIterator):
         for sequence in self.sequences:
             self._image_folder = self._path / sequence /'image_2'
             self._velocity_folder = self._path / sequence /'oxts/data'
-            self._timestamps_file = self._path / sequence /'oxst/timestamps.txt'
+            self._timestamps_file = self._path / sequence /'oxts/timestamps.txt'
 
             self._images[sequence] = sorted(self._image_folder.glob(f'*.png'))
             self._velocities[sequence] = sorted(self._velocity_folder.glob(f'*.txt'))
@@ -40,6 +45,9 @@ class KittiDataset(ExternalDataset, DatasetIterator):
 
         self.camera_matrix = np.array(
             [[0.58, 0, 0.5, 0], [0, 1.92, 0.5, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
+
+        # Now we simply put all sequences available on single lists
+        self._create_lists_from_sequences()
 
 
    
@@ -62,16 +70,65 @@ class KittiDataset(ExternalDataset, DatasetIterator):
         return timestamps
 
 
-    def __getitem__(self, idx):
+    def _load_relative_distance(self, index: int) -> float:
         """
-        This method is used for loading the idx-th sample of a dataset along with its annotation.
-        In this case, the annotation is split up into different files and, thus, a different interface is used.
+        Distance in meters and with respect to the previous frame.
+        """
+        previous_timestamp = self.timestamps[index - 1]
+        current_timestamp = self.timestamps[index]
+        delta_timestamp = current_timestamp - previous_timestamp  # s
+        # Compute speed as norm of forward, leftward, and upward elements
+        previous_speed = np.linalg.norm(np.loadtxt(str(self.velocities[index - 1]))[8:11])
+        current_speed = np.linalg.norm(np.loadtxt(str(self.velocities[index]))[8:11])
+        speed = (previous_speed + current_speed) / 2  # m/s
+        distance = speed * delta_timestamp
+        return distance, speed
 
-        :param idx: the index of the sample to load
-        :type idx: int
-        :return: the idx-th sample and a placeholder for the corresponding annotation
-        :rtype: Tuple of (Image, None)
-        
+    def _create_lists_from_sequences(self):
         """
-        return super().__getitem__(idx)
+        This method is used for creating a single list of images, velocities and timestamps from the sequences.
+        """
+
+        self.images = []
+        self.velocities = []
+        self.timestamps = []
+
+        for sequence in self.sequences:
+            self.images.extend(self._images[sequence])
+            self.velocities.extend(self._velocities[sequence])
+            self.timestamps.extend(self._timestamps[sequence])
+
+    def __getitem__(self, idx: int) -> Tuple[Any, None]:
+        """
+        This method is used for getting the item at the given index.
+
+        :param idx: index of the item
+        :type idx: int
+        :return: the item at the given index
+        :rtype: Tuple[Any, None]
+        """
+        image = Image.open(str(self.images[idx]))
+        distance, speed = self._load_relative_distance(idx)
+        
+        # Find Image ID from the path
+        image_id = self.images[idx].name.split('.')[0]
+        sequence_id = re.findall("sequence/\d\d", str(self.images[idx]))[0].split('/')[1]
+        image_id = sequence_id + '_' + image_id
+        data = {image_id: (image, distance, speed)}
+        return data, None
+
+    def __len__(self):
+        """
+        This method is used for getting the length of the dataset.
+
+        :return: the length of the dataset
+        :rtype: int
+        """
+        return len(self.images)
+
+if __name__ == '__main__':
+    kitti = KittiDataset(path='/home/canakcia/Desktop/')
+    for i in range(len(kitti)):
+        x = kitti[i]
+        
 
