accd192e4506517a7067ffffcc4e2c622b628f6a
Negar <negar.heidari@eng.au.dk>
2022-12-21 12:23:55 +0100

    ROS2 update

--- projects/opendr_ws_2/src/opendr_perception/opendr_perception/facial_emotion_estimation_node.py
+++ projects/opendr_ws_2/src/opendr_perception/opendr_perception/facial_emotion_estimation_node.py
@@ -27,14 +27,22 @@ from vision_msgs.msg import ObjectHypothesis
 from sensor_msgs.msg import Image as ROS_Image
 from opendr_ros2_bridge import ROS2Bridge
 
+from opendr.engine.data import Image
 from opendr.perception.facial_expression_recognition import FacialEmotionLearner
 from opendr.perception.facial_expression_recognition import image_processing
-from opendr.perception.facial_expression_recognition import ESR
+from opendr.perception.object_detection_2d import RetinaFaceLearner
+from opendr.perception.object_detection_2d.datasets.transforms import BoundingBoxListToNumpyArray
+
+INPUT_IMAGE_SIZE = (96, 96)
+INPUT_IMAGE_NORMALIZATION_MEAN = [0.0, 0.0, 0.0]
+INPUT_IMAGE_NORMALIZATION_STD = [1.0, 1.0, 1.0]
 
 
 class FacialEmotionEstimationNode(Node):
     def __init__(self,
+                 face_detector_learner,
                  input_rgb_image_topic="/usb_cam/image_raw",
+                 output_rgb_image_topic="/opendr/image_emotion_estimation_annotated",
                  output_emotions_topic="/opendr/facial_emotion_estimation",
                  output_emotions_description_topic="/opendr/facial_emotion_estimation_description",
                  device="cuda"):
@@ -42,6 +50,9 @@ class FacialEmotionEstimationNode(Node):
         Creates a ROS Node for facial emotion estimation.
         :param input_rgb_image_topic: Topic from which we are reading the input image
         :type input_rgb_image_topic: str
+        :param output_rgb_image_topic: Topic to which we are publishing the annotated image (if None, no annotated
+        image is published)
+        :type output_rgb_image_topic: str
         :param output_emotions_topic: Topic to which we are publishing the facial emotion results
         (if None, we are not publishing the info)
         :type output_emotions_topic: str
@@ -53,6 +64,12 @@ class FacialEmotionEstimationNode(Node):
         """
 
         self.image_subscriber = self.create_subscription(ROS_Image, input_rgb_image_topic, self.callback, 1)
+        self.bridge = ROS2Bridge()
+
+        if output_rgb_image_topic is not None:
+            self.image_publisher = self.create_publisher(ROS_Image, output_rgb_image_topic, 1)
+        else:
+            self.image_publisher = None
 
         if output_emotions_topic is not None:
             self.hypothesis_publisher = self.create_publisher(ObjectHypothesis, output_emotions_topic, 1)
@@ -64,15 +81,15 @@ class FacialEmotionEstimationNode(Node):
         else:
             self.string_publisher = None
 
-        self.bridge = ROS2Bridge()
+        # Initialize the face detector
+        self.face_detector = face_detector_learner
 
         # Initialize the facial emotion estimator
         self.facial_emotion_estimator = FacialEmotionLearner(device=device, batch_size=2,
                                                              ensemble_size=9,
                                                              name_experiment='esr_9')
-
+        self.facial_emotion_estimator.init_model(num_branches=9)
         model_saved_path = self.facial_emotion_estimator.download(path=None, mode="pretrained")
-
         self.facial_emotion_estimator.load(ensemble_size=9, path_to_saved_network=model_saved_path)
 
         self.get_logger().info("Facial emotion estimation node started.")
@@ -85,56 +102,47 @@ class FacialEmotionEstimationNode(Node):
         """
 
         # Convert sensor_msgs.msg.Image into OpenDR Image
-        image = self.bridge.from_ros_image(data, encoding='bgr8')
-        if image is None:
-            return
-        # preprocess the image
-        image = image_processing.read(image)
+        image = self.bridge.from_ros_image(data, encoding='bgr8').opencv()
+        emotion = None
+        # Run face detection and emotion estimation
 
-        # Detect face
-        face_coordinates = detect_face(image)
+        if image is not None:
+            bounding_boxes = self.face_detector.infer(image)
+            if bounding_boxes:
+                bounding_boxes = BoundingBoxListToNumpyArray()(bounding_boxes)
+                boxes = bounding_boxes[:, :4]
+                for idx, box in enumerate(boxes):
+                    (startX, startY, endX, endY) = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+                    face_crop = image[startY:endY, startX:endX]
 
-        if face_coordinates is None:
-            return
-        else:
-            face = image[face_coordinates[0][1]:face_coordinates[1][1],
-                   face_coordinates[0][0]:face_coordinates[1][0], :]
+                    # Preprocess detected face
+                    input_face = _pre_process_input_image(face_crop)
 
-            # Pre_process detected face
-            input_face = _pre_process_input_image(face)
-            input_face = input_face.to(device)
+                    # Recognize facial expression
+                    emotion, affect = self.facial_emotion_estimator.infer(input_face)
 
-            # Recognize facial expression
-            emotion, affect = self.facial_emotion_estimator.infer(input_face)
+                    # Converts from Tensor to ndarray
+                    affect = np.array([a.cpu().detach().numpy() for a in affect])
+                    affect = affect[0]  # a numpy array of valence and arousal values
+                    emotion = emotion[0]  # the emotion class with confidence tensor
 
-        if self.hypothesis_publisher is not None:
+                    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 255), thickness=2)
+                    cv2.putText(image, "Valence: %.2f" % affect[0], (startX, endY - 30), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.5, (0, 255, 255), 1, cv2.LINE_AA)
+                    cv2.putText(image, "Arousal: %.2f" % affect[1], (startX, endY - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.5, (0, 255, 255), 1, cv2.LINE_AA)
+                    cv2.putText(image, emotion.description, (startX, endY), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.5, (0, 255, 255), 1, cv2.LINE_AA)
+
+        if self.hypothesis_publisher is not None and emotion:
             self.hypothesis_publisher.publish(self.bridge.to_ros_category(emotion))
 
-        if self.string_publisher is not None:
+        if self.string_publisher is not None and emotion:
             self.string_publisher.publish(self.bridge.to_ros_category_description(emotion))
 
-
-def detect_face(image):
-    """
-    Detects faces in an image.
-
-    :param image: (ndarray) Raw input image.
-    :return: (list) Tuples with coordinates of a detected face.
-    """
-
-    # Converts to greyscale
-    greyscale_image = image_processing.convert_bgr_to_grey(image)
-
-    _FACE_DETECTOR_HAAR_CASCADE = cv2.CascadeClassifier("perception/facial_expression_recognition/"
-                                                        "image_based_facial_expression_recognition/"
-                                                        "face_detector/frontal_face.xml")
-    faces = _FACE_DETECTOR_HAAR_CASCADE.detectMultiScale(greyscale_image, scaleFactor=1.2, minNeighbors=9,
-                                                         minSize=(60, 60))
-    face_coordinates = [[[x, y], [x + w, y + h]] for (x, y, w, h) in faces] if not (faces is None) else []
-    face_coordinates = np.array(face_coordinates)
-
-    # Returns None if no face is detected
-    return face_coordinates[0] if (len(face_coordinates) > 0 and (np.sum(face_coordinates[0]) > 0)) else None
+        if self.image_publisher is not None:
+            # Convert the annotated OpenDR image to ROS image message using bridge and publish it
+            self.image_publisher.publish(self.bridge.to_ros_image(Image(image), encoding='bgr8'))
 
 
 def _pre_process_input_image(image):
@@ -145,10 +153,10 @@ def _pre_process_input_image(image):
     :return: (ndarray) image
     """
 
-    image = image_processing.resize(image, ESR.INPUT_IMAGE_SIZE)
+    image = image_processing.resize(image, INPUT_IMAGE_SIZE)
     image = PIL.Image.fromarray(image)
-    image = transforms.Normalize(mean=ESR.INPUT_IMAGE_NORMALIZATION_MEAN,
-                                 std=ESR.INPUT_IMAGE_NORMALIZATION_STD)(transforms.ToTensor()(image)).unsqueeze(0)
+    image = transforms.Normalize(mean=INPUT_IMAGE_NORMALIZATION_MEAN,
+                                 std=INPUT_IMAGE_NORMALIZATION_STD)(transforms.ToTensor()(image)).unsqueeze(0)
 
     return image
 
@@ -157,6 +165,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_rgb_image_topic', type=str, help='Topic name for input rgb image',
                         default='/usb_cam/image_raw')
+    parser.add_argument("-o", "--output_rgb_image_topic", help="Topic name for output annotated rgb image",
+                        type=lambda value: value if value.lower() != "none" else None,
+                        default="/opendr/image_emotion_estimation_annotated")
     parser.add_argument("-o", "--output_emotions_topic", help="Topic name for output emotion",
                         type=lambda value: value if value.lower() != "none" else None,
                         default="/opendr/facial_emotion_estimation")
@@ -182,16 +193,19 @@ if __name__ == '__main__':
         print("Using CPU")
         device = 'cpu'
 
+    # Initialize the face detector
+    face_detector = RetinaFaceLearner(backbone="resnet", device=device)
+    face_detector.download(path=".", verbose=True)
+    face_detector.load("retinaface_{}".format("resnet"))
+
     facial_emotion_estimation_node = FacialEmotionEstimationNode(
+        face_detector,
         input_rgb_image_topic=args.input_rgb_image_topic,
+        output_rgb_image_topic=args.output_rgb_image_topic,
         output_emotions_topic=args.output_emotions_topic,
         output_emotions_description_topic=args.output_emotions_description_topic,
         device=device)
 
     rclpy.spin(facial_emotion_estimation_node)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     facial_emotion_estimation_node.destroy_node()
     rclpy.shutdown()
