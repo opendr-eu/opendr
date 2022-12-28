5ec200fa8ae983671a8e2ff45925faf9288ea54c
ad-daniel <daniel.dias@epfl.ch>
2022-12-28 18:34:04 +0100

    Test

--- src/opendr/planning/end_to_end_planning/setup_end_to_end_planning.sh
+++ src/opendr/planning/end_to_end_planning/setup_end_to_end_planning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ -z "$OPENDR_HOME" ]]; then
-       echo "OPENDR_HOME is not defined"
-       exit 1
+  echo "OPENDR_HOME is not defined"
+  exit 1
 fi
 
 if [[ -z "$ROS_DISTRO" ]]; then
@@ -15,6 +15,9 @@ echo "Setup End-to-end Planning"
 sudo sh -c 'echo deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
+MODULE_PATH=${OPENDR_HOME}/src/opendr/planning/end_to_end_planning
+WS_PATH=${OPENDR_HOME}/projects/opendr_ws/
+
 # ROS
 if [[ ${ROS_DISTRO} == "noetic" || ${ROS_DISTRO} == "melodic" ]]; then
   sudo apt-get update && sudo apt-get install -y \
@@ -23,6 +26,14 @@ if [[ ${ROS_DISTRO} == "noetic" || ${ROS_DISTRO} == "melodic" ]]; then
     python3-rosdep python3-empy || exit;
 
     git clone --branch $ROS_DISTRO https://github.com/ros-perception/vision_opencv $WS_PATH/src/vision_opencv
+
+    source /opt/ros/${ROS_DISTRO}/setup.bash
+
+    # build the catkin workspace
+    cd ${WS_PATH} || exit
+    source /opt/ros/${ROS_DISTRO}/setup.bash
+    catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3
+    source devel/setup.bash
 fi
 
 # ROS2
