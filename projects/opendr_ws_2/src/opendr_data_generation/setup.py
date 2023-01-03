a2ddbbeb43072d45cbdc6be8452e498db54f21e4
ad-daniel <daniel.dias@epfl.ch>
2023-01-03 22:58:46 +0100

    Version bump

--- projects/opendr_ws_2/src/opendr_data_generation/setup.py
+++ projects/opendr_ws_2/src/opendr_data_generation/setup.py
@@ -18,7 +18,7 @@ package_name = 'opendr_data_generation'
 
 setup(
     name=package_name,
-    version='2.0.0',
+    version='2.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
