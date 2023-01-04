84e1bc24f90b6290373b5ec6e6a3cbcfb0996c2b
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-01-04 14:23:28 +0200

    Fixed license in cpp files

--- projects/python/perception/slam/full_map_posterior_gmapping/src/slam_gmapping/gmapping/src/slam_gmapping.cpp
+++ projects/python/perception/slam/full_map_posterior_gmapping/src/slam_gmapping/gmapping/src/slam_gmapping.cpp
@@ -30,7 +30,7 @@
 /* Author: Brian Gerkey */
 /* Modified by: Charles DuHadway */
 
-// Copyright 2020-2022 OpenDR European Project
+// Copyright 2020-2023 OpenDR European Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
