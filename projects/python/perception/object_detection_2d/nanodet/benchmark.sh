54e7d637976b02648ff77b69a374a5f80320b37a
ManosMpampis <93824600+ManosMpampis@users.noreply.github.com>
2023-01-20 02:26:43 +0200

    Update benchmark.sh

--- projects/python/perception/object_detection_2d/nanodet/benchmark.sh
+++ projects/python/perception/object_detection_2d/nanodet/benchmark.sh
@@ -14,5 +14,3 @@ python3 ./benchmark_demo.py --optimize-onnx --optimize-jit --model plus_m_320 >>
 python3 ./benchmark_demo.py --optimize-onnx --optimize-jit --model plus_m_1.5x_320 >> ./measures.txt
 python3 ./benchmark_demo.py --optimize-onnx --optimize-jit --model plus_m_416 >> ./measures.txt
 python3 ./benchmark_demo.py --optimize-onnx --optimize-jit --model plus_m_1.5x_416 >> ./measures.txt
-sshpass -p "4vpxYX9N" scp -r ./measures.txt manos@155.207.131.99:/home/manos/develop/c_api_implementations/opendr/projects/python/perception/object_detection_2d/nanodet/measures.txt
-
