38a00e423482f917289733c48d2f85d5747ee2c2
aselimc <canakcia@cs.uni-freiburg.de>
2023-02-02 18:00:14 +0100

    feat(efficientLPS): Add test data download option to PointCloud2 Publisher Node

--- projects/opendr_ws/src/opendr_perception/scripts/point_cloud_2_publisher_node.py
+++ projects/opendr_ws/src/opendr_perception/scripts/point_cloud_2_publisher_node.py
@@ -13,15 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import os
 import rospy
 import time
 import argparse
+from tqdm import tqdm
+import urllib
+from pathlib import Path
 
 from sensor_msgs.msg import PointCloud2 as ROS_PointCloud2
 from opendr_bridge import ROSBridge
 
 from opendr.perception.panoptic_segmentation import SemanticKittiDataset
+from zipfile import ZipFile
+
+from opendr.engine.constants import OPENDR_SERVER_URL
 
 
 class PointCloud2DatasetNode:
@@ -81,6 +87,47 @@ class PointCloud2DatasetNode:
             rospy.logerr("Dataset not found. Please download the dataset and extract it or enter available path.")
             return False
 
+
+def download(path):
+    url = f"{OPENDR_SERVER_URL}perception/panoptic_segmentation/efficient_lps/test_data.zip"
+    if not isinstance(path, Path):
+        path = Path(path)
+    filename = path / url.split("/")[-1]
+    path.mkdir(parents=True, exist_ok=True)
+
+    def pbar_hook(prog_bar: tqdm):
+        prev_b = [0]
+
+        def update_to(b=1, bsize=1, total=None):
+            if total is not None:
+                prog_bar.total = total
+            prog_bar.update((b - prev_b[0]) * bsize)
+            prev_b[0] = b
+
+        return update_to
+
+    if os.path.exists(filename) and os.path.isfile(filename):
+        print(f'File already downloaded: {filename}')
+    else:
+        with tqdm(unit="B", unit_scale=True, unit_divisor=1024, miniters=1, desc=f"Downloading {filename}")\
+                as pbar:
+            urllib.request.urlretrieve(url, filename, pbar_hook(pbar))
+    return str(filename)
+
+
+def prepare_test_data():
+        path = download(args.dataset_path)
+        try:
+            with ZipFile(path, 'r') as zipObj:
+                zipObj.extractall(args.dataset_path)
+            os.remove(path)
+        except:
+            pass
+        path = os.path.join(args.dataset_path, "test_data", "eval_data")
+
+        return path
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', '--dataset_path', type=str, default='./datasets/semantickitti',
@@ -89,7 +136,13 @@ if __name__ == "__main__":
                         help='split of the dataset to use (train, valid, test)')
     parser.add_argument('-o', '--output_point_cloud_2_topic', type=str, default='/opendr/dataset_point_cloud2',
                         help='topic for the output point cloud')
+    parser.add_argument('-t', '--test_data', action='store_true',
+                        help='Use uploaded test data on the FTP server')
+
     args = parser.parse_args()
+
+    if args.test_data:
+        args.dataset_path = prepare_test_data()
 
     dataset_node = PointCloud2DatasetNode(args.dataset_path, args.split, args.output_point_cloud_2_topic)
     dataset_node.start()
