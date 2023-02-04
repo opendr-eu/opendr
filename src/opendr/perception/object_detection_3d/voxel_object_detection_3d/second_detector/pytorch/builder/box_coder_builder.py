f448392c9167e9f556f4d0db813f7548d8ca334b
Illia Oleksiienko <io@ece.u.dk>
2023-02-04 13:05:33 +0000

    Empty build

--- src/opendr/perception/object_detection_3d/voxel_object_detection_3d/second_detector/pytorch/builder/box_coder_builder.py
+++ src/opendr/perception/object_detection_3d/voxel_object_detection_3d/second_detector/pytorch/builder/box_coder_builder.py
@@ -5,25 +5,26 @@
 
 
 def build(box_coder_config):
-    """Create optimizer based on config.
+    pass
+    # """Create optimizer based on config.
 
-    Args:
-        optimizer_config: A Optimizer proto message.
+    # Args:
+    #     optimizer_config: A Optimizer proto message.
 
-    Returns:
-        An optimizer and a list of variables for summary.
+    # Returns:
+    #     An optimizer and a list of variables for summary.
 
-    Raises:
-        ValueError: when using an unsupported input data type.
-    """
-    box_coder_type = box_coder_config.WhichOneof("box_coder")
-    if box_coder_type == "ground_box3d_coder":
-        cfg = box_coder_config.ground_box3d_coder
-        return GroundBox3dCoderTorch(cfg.linear_dim, cfg.encode_angle_vector)
-    elif box_coder_type == "bev_box_coder":
-        cfg = box_coder_config.bev_box_coder
-        return BevBoxCoderTorch(
-            cfg.linear_dim, cfg.encode_angle_vector, cfg.z_fixed, cfg.h_fixed
-        )
-    else:
-        raise ValueError("unknown box_coder type")
+    # Raises:
+    #     ValueError: when using an unsupported input data type.
+    # """
+    # box_coder_type = box_coder_config.WhichOneof("box_coder")
+    # if box_coder_type == "ground_box3d_coder":
+    #     cfg = box_coder_config.ground_box3d_coder
+    #     return GroundBox3dCoderTorch(cfg.linear_dim, cfg.encode_angle_vector)
+    # elif box_coder_type == "bev_box_coder":
+    #     cfg = box_coder_config.bev_box_coder
+    #     return BevBoxCoderTorch(
+    #         cfg.linear_dim, cfg.encode_angle_vector, cfg.z_fixed, cfg.h_fixed
+    #     )
+    # else:
+    #     raise ValueError("unknown box_coder type")
