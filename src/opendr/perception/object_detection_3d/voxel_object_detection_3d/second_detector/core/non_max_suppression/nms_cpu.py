f2fb18252685a7e8385947aad01d9267f7ef6202
Illia Oleksiienko <io@ece.u.dk>
2023-02-05 14:51:53 +0000

    Restore numba link

--- src/opendr/perception/object_detection_3d/voxel_object_detection_3d/second_detector/core/non_max_suppression/nms_cpu.py
+++ src/opendr/perception/object_detection_3d/voxel_object_detection_3d/second_detector/core/non_max_suppression/nms_cpu.py
@@ -3,8 +3,8 @@ from pathlib import Path
 # from opendr.perception.object_detection_3d.voxel_object_detection_3d.second_detector.utils.buildtools.pybind11_build import (
 #     load_pb11,
 # )
-# import numba
-# import numpy as np
+import numba
+import numpy as np
 
 # try:
 #     from opendr.perception.object_detection_3d.voxel_object_detection_3d.second_detector.core.non_max_suppression.nms import (
