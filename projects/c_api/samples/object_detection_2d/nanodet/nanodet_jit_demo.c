b46ec04b1697392f0c016379c91ca5433c632491
manos <gmp.manos@gmail.com>
2023-02-09 12:55:42 +0200

    fix paths

--- projects/c_api/samples/object_detection_2d/nanodet/nanodet_jit_demo.c
+++ projects/c_api/samples/object_detection_2d/nanodet/nanodet_jit_demo.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
   NanodetModelT model;
 
   printf("start init model\n");
-  loadNanodetModel("./data/object_detection_2d/nanodet/new_opt_model", "m", "cuda", 0.35, 0, 0, &model);
+  loadNanodetModel("./data/object_detection_2d/nanodet/optimized_model", "m", "cuda", 0.35, 0, 0, &model);
   printf("success\n");
 
   OpenDRImageT image;
