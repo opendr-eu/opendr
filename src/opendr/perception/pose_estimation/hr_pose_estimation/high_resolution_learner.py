be172829e72b9459416ddded95957270e0303c44
mthodoris <thodorisman@gmail.com>
2023-02-14 15:44:55 +0200

    test licences

--- src/opendr/perception/pose_estimation/hr_pose_estimation/high_resolution_learner.py
+++ src/opendr/perception/pose_estimation/hr_pose_estimation/high_resolution_learner.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2022 OpenDR European Project
+# Copyright 2020-2023 OpenDR European Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License."""
+# limitations under the License.
 
 # General imports
 
