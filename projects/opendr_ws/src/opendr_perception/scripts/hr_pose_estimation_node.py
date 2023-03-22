472e3f89a739396cadcbdd40835727253a7c5445
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-03-22 14:01:47 +0200

    hr_pose_estimation_node.py performance

--- projects/opendr_ws/src/opendr_perception/scripts/hr_pose_estimation_node.py
+++ projects/opendr_ws/src/opendr_perception/scripts/hr_pose_estimation_node.py
@@ -15,8 +15,10 @@
 
 import argparse
 import torch
+from time import perf_counter
 
 import rospy
+from std_msgs.msg import Float32
 from sensor_msgs.msg import Image as ROS_Image
 from opendr_bridge.msg import OpenDRPose2D
 from opendr_bridge import ROSBridge
@@ -30,7 +32,7 @@ class PoseEstimationNode:
 
     def __init__(self, input_rgb_image_topic="/usb_cam/image_raw",
                  output_rgb_image_topic="/opendr/image_pose_annotated", detections_topic="/opendr/poses", device="cuda",
-                 num_refinement_stages=2, use_stride=False, half_precision=False):
+                 performance_topic=None, num_refinement_stages=2, use_stride=False, half_precision=False):
         """
         Creates a ROS Node for high resolution pose estimation with HR Pose Estimation.
         :param input_rgb_image_topic: Topic from which we are reading the input image
@@ -41,6 +43,9 @@ class PoseEstimationNode:
         :param detections_topic: Topic to which we are publishing the annotations (if None, no pose detection message
         is published)
         :type detections_topic:  str
+        :param performance_topic: Topic to which we are publishing performance information (if None, no performance
+        message is published)
+        :type performance_topic:  str
         :param device: device on which we are running inference ('cpu' or 'cuda')
         :type device: str
         :param num_refinement_stages: Specifies the number of pose estimation refinement stages are added on the
@@ -66,6 +71,11 @@ class PoseEstimationNode:
         else:
             self.pose_publisher = None
 
+        if performance_topic is not None:
+            self.performance_publisher = rospy.Publisher(performance_topic, Float32, queue_size=1)
+        else:
+            self.performance_publisher = None
+
         self.bridge = ROSBridge()
 
         # Initialize the high resolution pose estimation learner
@@ -90,11 +100,20 @@ class PoseEstimationNode:
         :param data: Input image message
         :type data: sensor_msgs.msg.Image
         """
+        if self.performance_publisher:
+            start_time = perf_counter()
         # Convert sensor_msgs.msg.Image into OpenDR Image
         image = self.bridge.from_ros_image(data, encoding='bgr8')
 
         # Run pose estimation
         poses = self.pose_estimator.infer(image)
+
+        if self.performance_publisher:
+            end_time = perf_counter()
+            fps = 1.0 / (end_time - start_time)  # NOQA
+            fps_msg = Float32()
+            fps_msg.data = fps
+            self.performance_publisher.publish(fps_msg)
 
         #  Publish detections in ROS message
         if self.pose_publisher is not None:
@@ -124,6 +143,8 @@ def main():
     parser.add_argument("-d", "--detections_topic", help="Topic name for detection messages",
                         type=lambda value: value if value.lower() != "none" else None,
                         default="/opendr/poses")
+    parser.add_argument("--performance_topic", help="Topic name for performance messages, disabled (None) by default",
+                        type=str, default=None)
     parser.add_argument("--device", help="Device to use, either \"cpu\" or \"cuda\", defaults to \"cuda\"",
                         type=str, default="cuda", choices=["cuda", "cpu"])
     parser.add_argument("--accelerate", help="Enables acceleration flags (e.g., stride)", default=False,
@@ -156,6 +177,7 @@ def main():
                                              input_rgb_image_topic=args.input_rgb_image_topic,
                                              output_rgb_image_topic=args.output_rgb_image_topic,
                                              detections_topic=args.detections_topic,
+                                             performance_topic=args.performance_topic,
                                              num_refinement_stages=stages, use_stride=stride, half_precision=half_prec)
     pose_estimator_node.listen()
 
