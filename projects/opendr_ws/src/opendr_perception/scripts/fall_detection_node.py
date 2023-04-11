e61de5c71d3c1205bf789157df95e98055266417
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-04-11 14:22:06 +0300

    Added performance to fall detection ROS1 node

--- projects/opendr_ws/src/opendr_perception/scripts/fall_detection_node.py
+++ projects/opendr_ws/src/opendr_perception/scripts/fall_detection_node.py
@@ -16,8 +16,11 @@
 import cv2
 import argparse
 import torch
+from numpy import ndarray
+from time import perf_counter
 
 import rospy
+from std_msgs.msg import Float32
 from vision_msgs.msg import Detection2D
 from sensor_msgs.msg import Image as ROS_Image
 from opendr_bridge import ROSBridge
@@ -32,9 +35,13 @@ from opendr.perception.fall_detection import FallDetectorLearner
 
 class FallDetectionNode:
 
-    def __init__(self, input_rgb_image_topic=None, input_pose_topic="/opendr/poses",
-                 output_rgb_image_topic="/opendr/image_fallen_annotated", detections_topic="/opendr/fallen",
-                 device="cuda", num_refinement_stages=2, use_stride=False, half_precision=False):
+    def __init__(self, input_rgb_image_topic=None,
+                 input_pose_topic="/opendr/poses",
+                 output_rgb_image_topic="/opendr/image_fallen_annotated",
+                 detections_topic="/opendr/fallen",
+                 performance_topic=None,
+                 device="cuda",
+                 num_refinement_stages=2, use_stride=False, half_precision=False):
         """
         Creates a ROS Node for rule-based fall detection via pose estimation.
         :param input_rgb_image_topic: Topic from which we are reading the input image
@@ -47,6 +54,9 @@ class FallDetectionNode:
         :param detections_topic: Topic to which we are publishing the annotations (if None, no fall detection message
         is published)
         :type detections_topic:  str
+        :param performance_topic: Topic to which we are publishing performance information (if None, no performance
+        message is published)
+        :type performance_topic:  str
         :param device: device on which we are running pose estimation inference ('cpu' or 'cuda')
         :type device: str
         :param num_refinement_stages: Specifies the number of pose estimation refinement stages are added on the
@@ -71,12 +81,25 @@ class FallDetectionNode:
                                                              half_precision=half_precision)
             self.pose_estimator.download(path=".", verbose=True)
             self.pose_estimator.load("openpose_default")
+
+            if performance_topic is not None:
+                self.image_performance_publisher = rospy.Publisher(performance_topic + "/image", Float32, queue_size=1)
+            else:
+                self.image_performance_publisher = None
         else:
             self.input_rgb_image_topic = None
             self.image_publisher = None
             self.pose_estimator = None
 
-        self.input_pose_topic = input_pose_topic
+        if input_pose_topic is not None:
+            self.input_pose_topic = input_pose_topic
+
+            if performance_topic is not None:
+                self.fall_performance_publisher = rospy.Publisher(performance_topic + "/wave", Float32, queue_size=1)
+            else:
+                self.fall_performance_publisher = None
+        else:
+            self.input_pose_topic = None
 
         self.fall_publisher = rospy.Publisher(detections_topic, Detection2D, queue_size=1)
 
@@ -110,6 +133,8 @@ class FallDetectionNode:
         :param data: Input pose message
         :type data: opendr_bridge.msg.OpenDRPose2D
         """
+        if self.fall_performance_publisher:
+            start_time = perf_counter()
         poses = [self.bridge.from_ros_pose(data)]
         x, y, w, h = get_bbox(poses[0])  # Get bounding box for pose
         detections = self.fall_detector.infer(poses)  # Run fall detection
@@ -118,6 +143,14 @@ class FallDetectionNode:
         # Create Detection2D that contains the bbox of the pose as well as the detection class
         ros_detection = self.bridge.to_ros_box(BoundingBox(left=x, top=y, width=w, height=h,
                                                            name=fallen, score=poses[0].confidence))
+
+        if self.fall_performance_publisher:
+            end_time = perf_counter()
+            fps = 1.0 / (end_time - start_time)  # NOQA
+            fps_msg = Float32()
+            fps_msg.data = fps
+            self.fall_performance_publisher.publish(fps_msg)
+
         self.fall_publisher.publish(ros_detection)
 
     def image_callback(self, data):
@@ -126,32 +159,44 @@ class FallDetectionNode:
         :param data: Input image message
         :type data: sensor_msgs.msg.Image
         """
+        if self.image_performance_publisher:
+            start_time = perf_counter()
         # Convert sensor_msgs.msg.Image into OpenDR Image
         image = self.bridge.from_ros_image(data, encoding='bgr8')
 
         # Run fall detection
         detections = self.fall_detector.infer(image)
 
-        # Get an OpenCV image back
-        image = image.opencv()
-
         for detection in detections:
             fallen = detection[0].data
             pose = detection[1]
             x, y, w, h = get_bbox(pose)
 
+            if self.image_performance_publisher:
+                end_time = perf_counter()
+                fps = 1.0 / (end_time - start_time)  # NOQA
+                fps_msg = Float32()
+                fps_msg.data = fps
+                self.image_performance_publisher.publish(fps_msg)
+
+            # Create Detection2D that contains the bbox of the pose as well as the detection class
+            self.fall_publisher.publish(self.bridge.to_ros_box(BoundingBox(left=x, top=y, width=w, height=h,
+                                                                           name=fallen, score=pose.confidence)))
+
             if fallen == 1:
                 if self.image_publisher is not None:
+                    if type(image) != ndarray:
+                        # Get an OpenCV image back
+                        image = image.opencv()
                     # Paint person bounding box inferred from pose
                     color = (0, 0, 255)
                     cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
                     cv2.putText(image, "Fallen person", (x, y + h - 10), cv2.FONT_HERSHEY_SIMPLEX,
                                 1, color, 2, cv2.LINE_AA)
 
-            # Create Detection2D that contains the bbox of the pose as well as the detection class
-            ros_detection = self.bridge.to_ros_box(BoundingBox(left=x, top=y, width=w, height=h,
-                                                               name=fallen, score=pose.confidence))
-            self.fall_publisher.publish(ros_detection)
+        if type(image) != ndarray:
+            # Get an OpenCV image back
+            image = image.opencv()
         self.image_publisher.publish(self.bridge.to_ros_image(Image(image), encoding='bgr8'))
 
 
@@ -166,6 +211,8 @@ def main():
                         type=str, default=None)
     parser.add_argument("-o", "--output_rgb_image_topic", help="Topic name for output annotated rgb image",
                         type=str, default="/opendr/image_fallen_annotated")
+    parser.add_argument("--performance_topic", help="Topic name for performance messages, disabled (None) by default",
+                        type=str, default=None)
     parser.add_argument("--device", help="Device to use for pose estimation which runs when an input_rgb_image_topic "
                                          "is provided, either \"cpu\" or \"cuda\", defaults to \"cuda\"",
                         type=str, default="cuda", choices=["cuda", "cpu"])
@@ -204,6 +251,7 @@ def main():
                                             input_pose_topic=args.input_pose_topic,
                                             output_rgb_image_topic=args.output_rgb_image_topic,
                                             detections_topic=args.detections_topic,
+                                            performance_topic=args.performance_topic,
                                             num_refinement_stages=stages, use_stride=stride, half_precision=half_prec)
     fall_detection_node.listen()
 
