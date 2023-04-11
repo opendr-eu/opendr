f2b56de9536dc6f445a60f44de58a8d8895c56cc
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-04-11 13:13:33 +0300

    Added new learner to init

--- src/opendr/perception/semantic_segmentation/__init__.py
+++ src/opendr/perception/semantic_segmentation/__init__.py
@@ -1,4 +1,5 @@
 from opendr.perception.semantic_segmentation.bisenet.bisenet_learner import BisenetLearner
 from opendr.perception.semantic_segmentation.bisenet.CamVid import CamVidDataset
+from opendr.perception.semantic_segmentation.segment_anything_model.sam_learner import SamLearner
 
-__all__ = ['BisenetLearner', 'CamVidDataset']
+__all__ = ['BisenetLearner', 'CamVidDataset', 'SamLearner']
