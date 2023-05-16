5f902e0949a9a86eaf8bd4610b2c7eb1d0d5f04a
katerynaCh <eichum@mail.ru>
2023-05-16 18:56:09 +0300

    init commit

---
+++ projects/opendr_ws/src/opendr_perception/scripts/gesture_recognition_node.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# Copyright 2020-2023 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import torch
+
+import rospy
+from vision_msgs.msg import Detection2DArray
+from sensor_msgs.msg import Image as ROS_Image
+from opendr_bridge import ROSBridge
+
+from opendr.engine.data import Image
+from opendr.perception.gesture_recognition import GestureRecognitionLearner
+from opendr.perception.object_detection_2d import draw_bounding_boxes
+
+
+class GestureRecognitionNode:
+
+    def __init__(self, input_rgb_image_topic="/usb_cam/image_raw",
+                 output_rgb_image_topic="/opendr/images", detections_topic="/opendr/gestures",
+                 device="cuda", model="plus_m_1.5x_416", threshold=0.5):
+        """
+        :param input_rgb_image_topic: Topic from which we are reading the input image
+        :type input_rgb_image_topic: str
+        :param output_rgb_image_topic: Topic to which we are publishing the annotated image (if None, no annotated
+        image is published)
+        :type output_rgb_image_topic: str
+        :param detections_topic: Topic to which we are publishing the annotations (if None, no object detection message
+        is published)
+        :type detections_topic:  str
+        :param device: device on which we are running inference ('cpu' or 'cuda')
+        :type device: str
+        :param model: the name of the model of which we want to load the config file
+        :type model: str
+        """
+        self.input_rgb_image_topic = input_rgb_image_topic
+
+        if output_rgb_image_topic is not None:
+            self.image_publisher = rospy.Publisher(output_rgb_image_topic, ROS_Image, queue_size=1)
+        else:
+            self.image_publisher = None
+
+        if detections_topic is not None:
+            self.object_publisher = rospy.Publisher(detections_topic, Detection2DArray, queue_size=1)
+        else:
+            self.object_publisher = None
+
+        self.bridge = ROSBridge()
+
+        # Initialize the object detector
+        self.gesture_model = GestureRecognitionLearner(model_to_use=model, device=device)
+        self.gesture_model.download(path=".", mode="pretrained", verbose=True)
+        self.gesture_model.load("./nanodet_{}".format(model))
+        
+        self.threshold = threshold
+
+    def listen(self):
+        """
+        Start the node and begin processing input data.
+        """
+        rospy.init_node('opendr_gesture_recognition_node', anonymous=True)
+        rospy.Subscriber(self.input_rgb_image_topic, ROS_Image, self.callback, queue_size=1, buff_size=10000000)
+        rospy.loginfo("Object detection 2D Nanodet node started.")
+        rospy.spin()
+
+    def callback(self, data):
+        """
+        Callback that processes the input data and publishes to the corresponding topics.
+        :param data: input message
+        :type data: sensor_msgs.msg.Image
+        """
+        # Convert sensor_msgs.msg.Image into OpenDR Image
+        image = self.bridge.from_ros_image(data, encoding='bgr8')
+
+        # Run object detection
+        boxes = self.gesture_model.infer(image, threshold=self.threshold)
+
+        # Get an OpenCV image back
+        image = image.opencv()
+
+        # Publish detections in ROS message
+        ros_boxes = self.bridge.to_ros_boxes(boxes)  # Convert to ROS boxes
+        if self.object_publisher is not None:
+            self.object_publisher.publish(ros_boxes)
+
+        if self.image_publisher is not None:
+            # Annotate image with object detection boxes
+            image = draw_bounding_boxes(image, boxes, class_names=self.object_detector.classes)
+            # Convert the annotated OpenDR image to ROS2 image message using bridge and publish it
+            self.image_publisher.publish(self.bridge.to_ros_image(Image(image), encoding='bgr8'))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_rgb_image_topic", help="Topic name for input rgb image",
+                        type=str, default="/usb_cam/image_raw")
+    parser.add_argument("-o", "--output_rgb_image_topic", help="Topic name for output annotated rgb image",
+                        type=lambda value: value if value.lower() != "none" else None,
+                        default="/opendr/images")
+    parser.add_argument("-d", "--detections_topic", help="Topic name for detection messages",
+                        type=lambda value: value if value.lower() != "none" else None,
+                        default="/opendr/detections")
+    parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
+    parser.add_argument("--threshold", help="Confidence threshold for prediction", type=float, default=0.5)
+    parser.add_argument("--model", help="Model that config file will be used", type=str, default="plus_m_1.5x_416")
+    args = parser.parse_args()
+
+    try:
+        if args.device == "cuda" and torch.cuda.is_available():
+            device = "cuda"
+        elif args.device == "cuda":
+            print("GPU not found. Using CPU instead.")
+            device = "cpu"
+        else:
+            print("Using CPU.")
+            device = "cpu"
+    except:
+        print("Using CPU.")
+        device = "cpu"
+
+    gesture_recognition_node = GestureRecognitionNode(device=device, model=args.model,
+                                                               input_rgb_image_topic=args.input_rgb_image_topic,
+                                                               output_rgb_image_topic=args.output_rgb_image_topic,
+                                                               detections_topic=args.detections_topic, threshold=args.threshold)
+    gesture_recognition_node.listen()
+
+
+if __name__ == '__main__':
+    main()
---
+++ projects/python/perception/gesture_recognition/demo.py
@@ -0,0 +1,100 @@
+# Copyright 2020-2023 OpenDR European Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+import cv2
+import time
+import sys
+from opendr.engine.data import Image
+from opendr.perception.gesture_recognition import GestureRecognitionLearner
+from opendr.perception.gesture_recognition import draw_bounding_boxes
+
+
+class VideoReader(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.cap = cv2.VideoCapture(self.file_name)
+        if not self.cap.isOpened():
+            raise IOError('Video {} cannot be opened'.format(self.file_name))
+        try:  # OpenCV needs int to read from webcam
+            self.file_name = int(file_name)
+        except ValueError:
+            pass
+
+    def __iter__(self):
+        if not self.cap.isOpened():
+            raise IOError('Video {} cannot be opened'.format(self.file_name))
+        return self
+
+    def __next__(self):
+        was_read, img = self.cap.read()
+        if not was_read:
+            raise StopIteration
+        return img
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cpu", choices=["cuda", "cpu"])
+    parser.add_argument("--model", help="Model for which a config file will be used", type=str, default="plus_m_1.5x_416")
+    parser.add_argument("--optimize", help="", type=str, default="", choices=["", "onnx", "jit"])
+    args = parser.parse_args()
+
+    optimize, device, model = args.optimize, args.device, args.model
+
+    nanodet = NanodetLearner(model_to_use=model, device=device)
+    #nanodet.download("./predefined_examples", mode="pretrained")
+    nanodet.load("./predefined_examples/nanodet_{}".format(args.model), verbose=True)
+
+    # Use the first camera available on the system
+    image_provider = VideoReader(0)
+
+    if args.optimize != "":
+        nanodet.optimize("./{}/nanodet_{}".format(args.optimize, args.model), optimization=args.optimize,
+                         conf_threshold=0.35, iou_threshold=0.6, nms_max_num=20)
+
+    if 1==1: #try:
+        counter, avg_fps = 0, 0
+        for img in image_provider:
+
+            img = Image(img)
+
+            start_time = time.perf_counter()
+
+            # Perform inference
+            boxes = nanodet.infer(img, conf_threshold=0.5, iou_threshold=0.6, nms_max_num=20)
+            end_time = time.perf_counter()
+            fps = 1.0 / (end_time - start_time)
+
+            # Calculate a running average on FPS
+            avg_fps = 0.8 * fps + 0.2 * avg_fps
+
+            img = img.opencv()
+
+            if boxes:
+                draw_bounding_boxes(img, boxes, class_names=nanodet.classes, line_thickness=3)
+
+            # Wait a few frames for FPS to stabilize
+            if counter < 5:
+                counter += 1
+            else:
+                img = cv2.putText(img, "FPS: %.2f" % (avg_fps,), (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                  1, (255, 0, 0), 2, cv2.LINE_AA)
+
+            cv2.imshow('Result', img)
+            cv2.waitKey(1)
+    else: #except:
+        print("Average inference fps: ", avg_fps)
+
