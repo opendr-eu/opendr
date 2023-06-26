424215cee4bcf598d69f3ae9c6c1a0cd68e07249
Amir <60806416+a-ramezani@users.noreply.github.com>
2023-06-26 15:31:43 +0200

    Add files via upload

--- projects/python/simulation/agricultural-simulation-dataset-generator/agricultural-environment-robotti-mavicpro/controllers/robotti/robotti.py
+++ projects/python/simulation/agricultural-simulation-dataset-generator/agricultural-environment-robotti-mavicpro/controllers/robotti/robotti.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from controller import Robot, Motor, Camera, Lidar, LidarPoint, GPS, CameraRecognitionObject
+from controller import Robot, Camera, Lidar
 
 MAX_SPEED = 6.28
 
