87b1ad5d33b3b4cb239e48d71f012c9dd8f86eca
katerynaCh <eichum@mail.ru>
2023-06-30 14:28:05 +0300

    style fixes

--- tests/sources/tools/perception/multimodal_human_centric/intent_recognition/test_intent_recognition.py
+++ tests/sources/tools/perception/multimodal_human_centric/intent_recognition/test_intent_recognition.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
-sys.path.insert(0,'/home/chumache/opendr_gesture/opendr/src/')
 import unittest
 import torch
 import tempfile
@@ -29,6 +27,7 @@ from opendr.engine.datasets import DatasetIterator
 
 DEVICE = os.getenv('TEST_DEVICE') if os.getenv('TEST_DEVICE') else 'cpu'
 
+
 class DummyDataset(DatasetIterator):
     def __init__(self, args):
         super(DummyDataset, self).__init__()
@@ -41,7 +40,8 @@ class DummyDataset(DatasetIterator):
 
         sample = {
             'label_ids': torch.tensor(0).to(torch.long),
-            'text_feats': torch.cat((torch.ones((1, self.args.max_seq_length_text)), torch.zeros(2,self.args.max_seq_length_text))).to(torch.long),
+            'text_feats': torch.cat((torch.ones((1, self.args.max_seq_length_text)),
+                                     torch.zeros(2, self.args.max_seq_length_text))).to(torch.long),
             'video_feats': torch.zeros(self.args.max_seq_length_video, self.args.video_feat_dim).to(torch.double),
             'audio_feats': torch.zeros(self.args.max_seq_length_audio, self.args.audio_feat_dim).to(torch.double)
         }
@@ -58,15 +58,17 @@ class TestIntentRecognitionLearner(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         return
-    
+
     def test_fit(self):
         tmp_direc = tempfile.TemporaryDirectory()
         tmp_dir = tmp_direc.name
-        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE, log_path=tmp_dir, cache_path=tmp_dir, results_path = tmp_dir, output_path=tmp_dir)
+        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE,
+                                           log_path=tmp_dir, cache_path=tmp_dir, results_path=tmp_dir,
+                                           output_path=tmp_dir)
 
         train_set = DummyDataset(learner.train_config)
         val_set = DummyDataset(learner.train_config)
-        learner.method.args.num_train_epochs=1
+        learner.method.args.num_train_epochs = 1
 
         old_weight = list(learner.model.model.parameters())[0].clone()
         learner.fit(train_set, val_set, silent=True, verbose=False)
@@ -79,7 +81,9 @@ class TestIntentRecognitionLearner(unittest.TestCase):
     def test_eval_trim(self):
         tmp_direc = tempfile.TemporaryDirectory()
         tmp_dir = tmp_direc.name
-        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE, log_path=tmp_dir, cache_path=tmp_dir, results_path = tmp_dir, output_path=tmp_dir)
+        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE,
+                                           log_path=tmp_dir, cache_path=tmp_dir, results_path=tmp_dir,
+                                           output_path=tmp_dir)
 
         dataset = DummyDataset(learner.train_config)
         performance = learner.eval(dataset, modality='language', silent=True, verbose=False, restore_best_model=False)
@@ -91,12 +95,16 @@ class TestIntentRecognitionLearner(unittest.TestCase):
         tmp_direc.cleanup()
 
     def test_infer(self):
-        test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+                     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+                     laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \
+                     voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
         tmp_direc = tempfile.TemporaryDirectory()
         tmp_dir = tmp_direc.name
         # create learner and download pretrained model
-        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE, log_path=tmp_dir, cache_path=tmp_dir, results_path = tmp_dir, output_path=tmp_dir)
-
+        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE,
+                                           log_path=tmp_dir, cache_path=tmp_dir, results_path=tmp_dir,
+                                           output_path=tmp_dir)
 
         # make inference
         pred = learner.infer({'text': test_text}, modality='language')
@@ -107,21 +115,28 @@ class TestIntentRecognitionLearner(unittest.TestCase):
         self.assertTrue(pred[0].confidence <= 1,
                         msg="Confidence of prediction must be less or equal than 1")
         tmp_direc.cleanup()
-    
+
     def test_save_load(self):
-        test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+        test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+                     ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \
+                     laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \
+                     voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
 
         temp_dir = 'tmp_'+str(time.time())
         tmp_direc = tempfile.TemporaryDirectory()
         tmp_dir2 = tmp_direc.name
-        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE, log_path=tmp_dir2, cache_path=tmp_dir2, results_path = tmp_dir2, output_path=tmp_dir2)
+        learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE,
+                                           log_path=tmp_dir2, cache_path=tmp_dir2, results_path=tmp_dir2,
+                                           output_path=tmp_dir2)
         train_set = DummyDataset(learner.train_config)
-        learner.method.args.num_train_epochs=1
+        learner.method.args.num_train_epochs = 1
         learner.fit(train_set, silent=True, verbose=False)
 
         learner.save(temp_dir)
 
-        new_learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE, log_path=tmp_dir2, cache_path=tmp_dir2, results_path = tmp_dir2, output_path=tmp_dir2)
+        new_learner = IntentRecognitionLearner(text_backbone='prajjwal1/bert-tiny', mode='joint', device=DEVICE,
+                                               log_path=tmp_dir2, cache_path=tmp_dir2, results_path=tmp_dir2,
+                                               output_path=tmp_dir2)
 
         new_learner.load(temp_dir)
         old_pred = learner.infer({'text': test_text}, modality='language')[0].confidence
@@ -130,6 +145,7 @@ class TestIntentRecognitionLearner(unittest.TestCase):
         self.assertEqual(old_pred, new_pred)
         os.remove(temp_dir)
         tmp_direc.cleanup()
+
 
 if __name__ == "__main__":
     unittest.main()
