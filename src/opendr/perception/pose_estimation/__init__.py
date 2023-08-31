e940f22f78ec1d1d3051336bb67eeaba7bbb6a00
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2023-08-31 14:47:52 +0300

    Removed dummy code change

--- src/opendr/perception/pose_estimation/__init__.py
+++ src/opendr/perception/pose_estimation/__init__.py
@@ -6,5 +6,3 @@ from opendr.perception.pose_estimation.hr_pose_estimation.high_resolution_learne
 from opendr.perception.pose_estimation.lightweight_open_pose.utilities import draw, get_bbox
 
 __all__ = ['LightweightOpenPoseLearner', 'draw', 'get_bbox', 'HighResolutionPoseEstimationLearner']
-
-# Dummy code change
