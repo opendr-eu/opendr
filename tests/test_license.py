818aa2ee379c858e28b23ada482908c8170b8598
Nikolaos Passalis <passalis@users.noreply.github.com>
2025-01-29 21:47:32 +0200

    Regex license test

--- tests/test_license.py
+++ tests/test_license.py
@@ -20,28 +20,33 @@ import unittest
 import os
 import fnmatch
 import datetime
+import re
 
 from io import open
 
-APACHE2_LICENSE_C = """/*
- * Copyright 2020-20XX OpenDR European Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */""".replace('20XX', str(datetime.datetime.now().year))
+# Dynamic regex pattern to match any end year (e.g., "2020-2025", "2020-2030", etc.)
+YEAR_PATTERN = r"2020-\d{4}"
 
-APACHE2_LICENSE_CPP = """// Copyright 2020-20XX OpenDR European Project
+# License templates with YEAR_PATTERN instead of fixed years
+APACHE2_LICENSE_C_REGEX = re.compile(rf"""/\*
+ \* Copyright {YEAR_PATTERN} OpenDR European Project
+ \*
+ \* Licensed under the Apache License, Version 2.0 \(the "License"\);
+ \* you may not use this file except in compliance with the License.
+ \* You may obtain a copy of the License at
+ \*
+ \*     http://www.apache.org/licenses/LICENSE-2.0
+ \*
+ \* Unless required by applicable law or agreed to in writing, software
+ \* distributed under the License is distributed on an "AS IS" BASIS,
+ \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ \* See the License for the specific language governing permissions and
+ \* limitations under the License.
+ \*/""", re.MULTILINE)
+
+APACHE2_LICENSE_CPP_REGEX = re.compile(rf"""// Copyright {YEAR_PATTERN} OpenDR European Project
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 \(the "License"\);
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -51,11 +56,11 @@ APACHE2_LICENSE_CPP = """// Copyright 2020-20XX OpenDR European Project
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.""".replace('20XX', str(datetime.datetime.now().year))
+// limitations under the License.""", re.MULTILINE)
 
-APACHE2_LICENSE_PYTHON = """# Copyright 2020-20XX OpenDR European Project
+APACHE2_LICENSE_PYTHON_REGEX = re.compile(rf"""# Copyright {YEAR_PATTERN} OpenDR European Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 \(the "License"\);
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -65,7 +70,7 @@ APACHE2_LICENSE_PYTHON = """# Copyright 2020-20XX OpenDR European Project
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.""".replace('20XX', str(datetime.datetime.now().year))
+# limitations under the License.""", re.MULTILINE)
 
 PYTHON_OPTIONAL_HEADERS = [
     '#!/usr/bin/env python2',
@@ -96,36 +101,6 @@ class TestLicense(unittest.TestCase):
             'src/opendr/perception/object_tracking_2d/siamrpn/data_utils',
             'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/backbones',
             'src/opendr/perception/heart_anomaly_detection/attention_neural_bag_of_feature/algorithm',
-            'src/opendr/simulation/human_model_generation/utilities/PIFu',
-            'src/opendr/perception/multimodal_human_centric/rgbd_hand_gesture_learner/algorithm/architectures',
-            'src/opendr/perception/multimodal_human_centric/intent_recognition_learner/algorithm/',
-            'src/opendr/perception/skeleton_based_action_recognition/algorithm',
-            'projects/python/simulation/synthetic_multi_view_facial_image_generation/algorithm',
-            'projects/opendr_ws/devel',
-            'src/opendr/perception/semantic_segmentation/bisenet/algorithm',
-            'src/opendr/perception/object_detection_2d/retinaface/algorithm',
-            'src/opendr/perception/object_detection_2d/gem/algorithm',
-            'src/opendr/perception/object_detection_2d/detr/algorithm',
-            'src/opendr/perception/object_detection_2d/nanodet/algorithm',
-            'src/opendr/perception/panoptic_segmentation/efficient_ps/algorithm/EfficientPS',
-            'src/opendr/perception/facial_expression_recognition/landmark_based_facial_expression_recognition',
-            'src/opendr/perception/object_tracking_3d/single_object_tracking/vpit/second_detector',
-            'src/opendr/perception/panoptic_segmentation/efficient_lps/algorithm/EfficientLPS',
-            'src/opendr/perception/continual_slam/algorithm',
-            'src/opendr/perception/facial_expression_recognition/landmark_based_facial_expression_recognition/algorithm',
-            'src/opendr/perception/facial_expression_recognition/image_based_facial_emotion_estimation/algorithm',
-            'src/opendr/perception/gesture_recognition/algorithm',
-            'projects/python/perception/facial_expression_recognition/image_based_facial_emotion_estimation',
-            'projects/opendr_ws_2/src/opendr_perception/test',
-            'projects/opendr_ws_2/src/opendr_ros2_bridge/test',
-            'projects/opendr_ws_2/src/vision_opencv',
-            'projects/opendr_ws_2/install',
-            'projects/opendr_ws_2/src/data_generation/test',
-            'projects/opendr_ws_2/src/opendr_planning/test',
-            'projects/opendr_ws_2/src/opendr_bridge/test',
-            'projects/opendr_ws_2/src/opendr_interface/test',
-            'projects/opendr_ws_2/src/opendr_data_generation/test',
-            'projects/opendr_ws_2/src/opendr_simulation/test',
         ]
 
         skippedFilePaths = [
@@ -133,16 +108,6 @@ class TestLicense(unittest.TestCase):
             'projects/python/perception/pose_estimation/lightweight_open_pose/jetbot/utils/pid.py',
             'src/opendr/perception/compressive_learning/multilinear_compressive_learning/algorithm/trainers.py',
             'src/opendr/perception/object_detection_2d/retinaface/Makefile',
-            'src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/efficientface_modulator.py',
-            'src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/efficientface_utils.py',
-            'src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/spatial_transforms.py',
-            'src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/transformer_timm.py',
-            'src/opendr/perception/multimodal_human_centric/audiovisual_emotion_learner/algorithm/utils.py',
-            'projects/opendr_ws_2/src/opendr_perception/setup.py',
-            'projects/opendr_ws_2/src/opendr_planning/setup.py',
-            'projects/opendr_ws_2/src/opendr_bridge/setup.py',
-            'projects/opendr_ws_2/src/data_generation/setup.py',
-            'projects/opendr_ws_2/src/opendr_simulation/setup.py',
         ]
 
         skippedDirectories = [
@@ -182,28 +147,25 @@ class TestLicense(unittest.TestCase):
             with open(source, 'r', encoding='utf-8') as content_file:
                 content = content_file.read()
                 if source.endswith('.c') or source.endswith('.h'):
-                    self.assertTrue(
-                        APACHE2_LICENSE_C in content,
-                        msg='Source file "%s" doesn\'t contain the correct Apache 2.0 License:\n%s' %
-                            (source, APACHE2_LICENSE_C)
+                    self.assertRegex(
+                        content,
+                        APACHE2_LICENSE_C_REGEX,
+                        msg=f'Source file "{source}" doesn\'t contain the correct Apache 2.0 License.'
                     )
                 elif source.endswith('.cpp') or source.endswith('.hpp') or source.endswith('.java'):
-                    self.assertTrue(
-                        APACHE2_LICENSE_CPP in content,
-                        msg='Source file "%s" doesn\'t contain the correct Apache 2.0 License:\n%s' %
-                            (source, APACHE2_LICENSE_CPP)
+                    self.assertRegex(
+                        content,
+                        APACHE2_LICENSE_CPP_REGEX,
+                        msg=f'Source file "{source}" doesn\'t contain the correct Apache 2.0 License.'
                     )
                 elif source.endswith('.py') or source.endswith('Makefile'):
-                    self.assertTrue(
-                        APACHE2_LICENSE_PYTHON in content,
-                        msg='Source file "%s" doesn\'t contain the correct Apache 2.0 License:\n%s' %
-                            (source, APACHE2_LICENSE_PYTHON)
+                    self.assertRegex(
+                        content,
+                        APACHE2_LICENSE_PYTHON_REGEX,
+                        msg=f'Source file "{source}" doesn\'t contain the correct Apache 2.0 License.'
                     )
                 else:
-                    self.assertTrue(
-                        False,
-                        msg='Unsupported file extension "%s".' % source
-                    )
+                    self.fail(f'Unsupported file extension "{source}".')
 
 
 if __name__ == '__main__':
